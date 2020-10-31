2fb6669cb189c5409ad7211274c4f51c57c9a4fc
ValerijaI <55547022+ValerijaI@users.noreply.github.com>
2020-10-31 14:34:37 +0200

    ProductListApplication

---
+++ team_no_name_no_problem/src/main/java/internet_store/application/ProductListApplication.java
@@ -0,0 +1,10 @@
+package internet_store.application;
+
+public class ProductListApplication {
+
+    public static void main(String[] args) {
+        InternetStore internetStore = new InternetStore();
+        internetStore.run();
+    }
+
+}
