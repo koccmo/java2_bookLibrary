4f1c64adeb8759a2363f2cf8c40af78b2a9aae96
Anvar331 <anvar.intezar@gmail.com>
2020-10-31 23:20:24 +0200

    internet_store/Delete by ID tests

--- team_no_name_no_problem/src/test/java/internet_store/ProductDatabaseImplTest.java
+++ team_no_name_no_problem/src/test/java/internet_store/ProductDatabaseImplTest.java
@@ -31,9 +31,30 @@ public class ProductDatabaseImplTest {
         Long secondId = secondProduct.getId();
         assertTrue(firstId == 1L);
         assertTrue(secondId == 2L);
-
-
     }
 
+    @Test
+    public void deleteByIdTest() {
+        ProductDatabaseImpl productDatabase = new ProductDatabaseImpl();
+
+        Product firstProduct = new Product("Laptop","Dell",400);
+        Product secondProduct = new Product("Refrigerator","Electrolux",300);
+        productDatabase.save(firstProduct);
+        productDatabase.save(secondProduct);
+        boolean deleteResult = productDatabase.deleteById(1L);
+        assertTrue(deleteResult);
+    }
+
+    @Test
+    public void deleteByIdTest2() {
+        ProductDatabaseImpl productDatabase = new ProductDatabaseImpl();
+
+        Product firstProduct = new Product("Laptop","Dell",400);
+        Product secondProduct = new Product("Refrigerator","Electrolux",300);
+        productDatabase.save(firstProduct);
+        productDatabase.save(secondProduct);
+        boolean deleteResult = productDatabase.deleteById(2L);
+        assertTrue(deleteResult);
+    }
 
 }
