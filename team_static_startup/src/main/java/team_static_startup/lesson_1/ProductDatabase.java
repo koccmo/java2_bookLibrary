7780deab8d5eedf7a776457ac200ea217c522824
Igors Mihejevs <mihejevs.igors@gmail.com>
2020-10-31 22:44:12 +0200

    lesson_1 refactoring

--- team_static_startup/src/main/java/team_static_startup/lesson_1/ProductDatabase.java
+++ team_static_startup/src/main/java/team_static_startup/lesson_1/ProductDatabase.java
@@ -10,28 +10,4 @@ public interface ProductDatabase {
 
     void deleteByProductName(String product);
 
-//    Optional<Book> findById(Long bookId);
-
-//    List<Book> findByAuthor(String author);
-
-//    List<Book> findByTitle(String title);
-
-//    int countAllBooks();
-
-//    void deleteByTitle(String title);
-
-//    List<Book> find(SearchCriteria searchCriteria);
-
-//    Set<String> findUniqueAuthors();
-
-//    Set<String> findUniqueTitles();
-
-//    Set<Book> findUniqueBooks();
-
-//    boolean contains(Book book);
-
-//    Map<String, List<Book>> getAuthorToBooksMap();
-
-//    Map<String, Integer> getEachAuthorBookCount();
-
 }
