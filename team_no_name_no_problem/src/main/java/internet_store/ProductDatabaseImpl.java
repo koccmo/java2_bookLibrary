0a127744489cf0a2992d7991b108c877b4ef53ec
ValerijaI <55547022+ValerijaI@users.noreply.github.com>
2020-10-31 16:04:41 +0200

    ProductDatabaseImpl + delete + print

--- team_no_name_no_problem/src/main/java/internet_store/ProductDatabaseImpl.java
+++ team_no_name_no_problem/src/main/java/internet_store/ProductDatabaseImpl.java
@@ -24,5 +24,26 @@ public class ProductDatabaseImpl implements ProductDatabase{
         return true;
     }
 
+    @Override
+    public boolean deleteById(long id) {
+        for (int i = 0; i< productList.size(); i++){
+            if (productList.get(i).getId() == id){
+                productList.remove(i);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public void printProducts() {
+        if (productList.size()>0) {
+            System.out.println("Products in database: ");
+            productList.forEach(System.out::println);
+        }else{
+            System.out.println("Database is empty");
+        }
+    }
+
 
 }
