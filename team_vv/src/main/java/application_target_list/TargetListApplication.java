27153da5a35738eccd8f42ad552f1c359737913c
VadimsVladisevs <vadims.vladisevs@gmail.com>
2020-11-01 15:19:01 +0200

    Target list, team_vv, Lesson 1 added.

---
+++ team_vv/src/main/java/application_target_list/TargetListApplication.java
@@ -0,0 +1,9 @@
+package application_target_list;
+
+ class TargetListApplication {
+
+     public static void main(String[] args) {
+        Menu menu = new Menu();
+        menu.start();
+     }
+}
