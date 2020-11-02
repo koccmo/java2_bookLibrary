d9540f55fe8f621b56a8e63853d7585eac6ac1be
ValerijaI <55547022+ValerijaI@users.noreply.github.com>
2020-11-02 23:09:17 +0200

    AddPatientUIAction

---
+++ team_healthy_tooth/src/main/java/dental_clinic/UI/AddPatientUIAction.java
@@ -0,0 +1,33 @@
+package dental_clinic.UI;
+
+import dental_clinic.CardDatabase;
+import dental_clinic.domain.Patient;
+
+class AddPatientUIAction implements UIAction {
+
+    private CardDatabase cardDatabase;
+    InputCheckUtility inputCheckUtility = new InputCheckUtility();
+
+    public AddPatientUIAction(CardDatabase cardDatabase){
+        this.cardDatabase = cardDatabase;
+    }
+
+    public void execute(){
+        String name = inputCheckUtility.inputValidString("Please enter name");
+        String surname = inputCheckUtility.inputValidString("Please enter surname");
+        String phone = inputCheckUtility.inputValidString("Please enter phone");
+        String personalCode = inputCheckUtility.inputValidString("Please enter personal code");
+        String doctor = inputCheckUtility.inputValidString("Please enter doctor's surname");
+
+        Patient patient = new Patient(name, surname, phone, personalCode, doctor);
+
+        if (cardDatabase.addPatient(patient)){
+            System.out.println("Patient added");
+        }else{
+            System.out.println("The same patient is in database");
+        }
+
+    }
+
+}
+
