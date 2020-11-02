666cf9c316c4a2ea94c43fc58123a5f7551dff65
ValerijaI <55547022+ValerijaI@users.noreply.github.com>
2020-11-02 23:08:54 +0200

    DentalClinic

---
+++ team_healthy_tooth/src/main/java/dental_clinic/UI/DentalClinic.java
@@ -0,0 +1,64 @@
+package dental_clinic.UI;
+
+import dental_clinic.CardDatabase;
+import dental_clinic.CardDatabaseImpl;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class DentalClinic {
+
+    private Map<Integer, UIAction> menuNumberToAction;
+
+    public DentalClinic() {
+        CardDatabase cardDatabase = new CardDatabaseImpl();
+
+        menuNumberToAction = new HashMap();
+
+        menuNumberToAction.put(1, new AddPatientUIAction(cardDatabase));
+        menuNumberToAction.put(2, new DeletePatientUIAction(cardDatabase));
+    }
+
+    public void run(){
+
+        while(true) {
+
+            System.out.println("\nMenu\n" +
+                    "1   Add patient\n" +
+                    "2   Delete by id\n" +
+                    "0   Exit");
+
+            Scanner in = new Scanner(System.in);
+            int userSelectedMenuNumber;
+
+            while (true) {
+                try {
+                    System.out.println("Please enter menu number: ");
+                    userSelectedMenuNumber = Integer.parseInt(in.nextLine());
+                    break;
+                } catch (NumberFormatException e) {
+                    System.out.println("It's not valid number!");
+                }
+            }
+
+            if (userSelectedMenuNumber == 0) {
+                System.out.println(":) End of work day!");
+                break;
+            } else {
+                executeUIAction(userSelectedMenuNumber);
+            }
+
+        }
+    }
+
+    private void executeUIAction (int userSelectedMenuNumber) {
+        UIAction uiAction = menuNumberToAction.get(userSelectedMenuNumber);
+        if (uiAction != null) {
+            uiAction.execute();
+        } else {
+            System.out.println("Menu item does not exist: " + userSelectedMenuNumber);
+        }
+    }
+
+}
