5f4f7ff364af02eb12f8a28a6fea4a4fe622d311
binco <vladimirs.danilins@gmail.com>
2020-11-03 16:39:51 +0200

    Тесты к PatientDatabaseImpl версия 1.0

--- team_healthy_tooth/src/test/java/dental_clinic/PatientDatabaseImplTest.java
+++ team_healthy_tooth/src/test/java/dental_clinic/PatientDatabaseImplTest.java
@@ -1,9 +1,92 @@
 package dental_clinic;
 
-import static org.junit.Assert.*;
+import dental_clinic.domain.PatientPersonalData;
+
+import java.util.List;
 
 public class PatientDatabaseImplTest {
+    private CardDatabaseImpl impl = new CardDatabaseImpl();
+    private boolean isSomeTestFailed = false;
 
-    //Test come soon... by volodya
+    public static void main(String[] args) {
+        PatientDatabaseImplTest test = new PatientDatabaseImplTest();
+
+        test.testAddPatient();
+        test.testFindPatientBySurname();
+        test.testFindPatientByPersonalCode();
+        test.testDeletePatient();
+
+        test.finaliseTests();
+
+    }
+
+    //patient1 for tests 1, 2, 3
+    PatientPersonalData patient1 = new PatientPersonalData(
+            "name", "surname", "phone", "pCode"
+    );
+
+    //Test 1 : testing duplicate patient in .addPatient
+    private void testAddPatient() {
+        System.out.print("Testing .addPatient ... ");
+
+        printTestResult(impl.addPatient(patient1)
+        && !impl.addPatient(patient1));
+    }
+
+    //Test 2 : testing .findPatientBySurname
+    private void testFindPatientBySurname() {
+        System.out.print("Testing .findPatientBySurname ... ");
+
+        List<PatientPersonalData> resultPositive = //should find patient1
+                impl.findPatientBySurname("surname");
+
+        List<PatientPersonalData> resultNegative = //should find nothing
+                impl.findPatientBySurname("invalid surname");
+
+        printTestResult(resultPositive.contains(patient1)
+        && resultNegative.isEmpty());
+    }
+
+    //Test 3 : testing .findPatientByPersonalCode
+    private void testFindPatientByPersonalCode() {
+        System.out.print("Testing .findPatientByPersonalCode ... ");
+
+        List<PatientPersonalData> resultPositive = //should find patient1
+                impl.findPatientByPersonalCode("pCode");
+
+        List<PatientPersonalData> resultNegative = //should find nothing
+                impl.findPatientByPersonalCode("invalid pCode");
+
+        printTestResult(resultPositive.contains(patient1)
+        && resultNegative.isEmpty());
+    }
+
+    //Test 4 : testing invalid patient removal in .deletePatient
+    private void testDeletePatient() {
+        System.out.print("Testing .deletePatient ... ");
+
+        printTestResult(impl.deletePatient(1L)
+        && !impl.deletePatient(9999L)); //id 9999 is invalid
+    }
+
+
+    private void printTestResult(boolean successCondition) {
+        if (successCondition) {
+            System.out.println("Test OK");
+        }
+        else {
+            System.out.println("Test FAIL");
+            isSomeTestFailed = true;
+        }
+    }
+
+    private void finaliseTests() {
+        if (!isSomeTestFailed) {
+            System.out.println("All tests OK");
+        }
+        else {
+            System.out.println("Some tests FAILED");
+        }
+    }
 
 }
