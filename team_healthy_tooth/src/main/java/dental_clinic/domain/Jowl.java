cf20561c744c271b31c5756c6e4f4c7217f4a4f7
ValerijaI <55547022+ValerijaI@users.noreply.github.com>
2020-11-04 13:22:50 +0200

    Refactoring + Jowl

---
+++ team_healthy_tooth/src/main/java/dental_clinic/domain/Jowl.java
@@ -0,0 +1,61 @@
+package dental_clinic.domain;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
+public class Jowl {
+
+    private Map<Integer, ToothInfo> jowl;
+
+    Jowl (){
+        jowl = createNewJowl();
+    }
+
+
+    public Map<Integer, ToothInfo> getJowl() {
+        return jowl;
+    }
+
+    private Map <Integer, ToothInfo> createNewJowl(){
+        Map <Integer, ToothInfo> newJowl = new HashMap<>();
+        for (int i = 18; i>10; i--) {
+            newJowl.put(i, new ToothInfo(Optional.empty(), ToothStatus.HEALTHY));
+        }
+        for (int i = 21; i<29; i++) {
+            newJowl.put(i, new ToothInfo(Optional.empty(), ToothStatus.HEALTHY));
+        }
+        for (int i = 55; i>50; i--) {
+            newJowl.put(i, new ToothInfo(Optional.empty(), ToothStatus.HEALTHY));
+        }
+        for (int i = 61; i<66; i++) {
+            newJowl.put(i, new ToothInfo(Optional.empty(), ToothStatus.HEALTHY));
+        }
+
+        for (int i = 48; i>40; i--) {
+            newJowl.put(i, new ToothInfo(Optional.empty(), ToothStatus.HEALTHY));
+        }
+        for (int i = 31; i<39; i++) {
+            newJowl.put(i, new ToothInfo(Optional.empty(), ToothStatus.HEALTHY));
+        }
+        for (int i = 85; i>80; i--) {
+            newJowl.put(i, new ToothInfo(Optional.empty(), ToothStatus.HEALTHY));
+        }
+        for (int i = 71; i<76; i++) {
+            newJowl.put(i, new ToothInfo(Optional.empty(), ToothStatus.HEALTHY));
+        }
+        return newJowl;
+    }
+
+    public boolean updateJowl(int toothNumber, Optional <String> comment, ToothStatus toothStatus) {
+        if (jowl.containsKey(toothNumber)) {
+            jowl.get(toothNumber).addComment(comment);
+            jowl.get(toothNumber).addStatus(toothStatus);
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+
+}
