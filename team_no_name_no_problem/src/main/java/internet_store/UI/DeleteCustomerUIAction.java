f663477061346cc76d9c97b367221d1822e776c2
Jaroslav <brutan.jaroslav@gmail.com>
2020-11-04 23:17:49 +0200

    Customer update

---
+++ team_no_name_no_problem/src/main/java/internet_store/UI/DeleteCustomerUIAction.java
@@ -0,0 +1,25 @@
+package internet_store.UI;
+
+import internet_store.CustomerDatabase;
+
+public class DeleteCustomerUIAction implements UIAction{
+
+    InputCheckUtility inputCheckUtility = new InputCheckUtility();
+
+    private CustomerDatabase customerDatabase;
+
+    public DeleteCustomerUIAction(CustomerDatabase personDatabase){
+        this.customerDatabase = personDatabase;
+    }
+
+    public void execute(){
+
+        long id = inputCheckUtility.inputValidLong("Please enter customer's id to delete");
+
+        if (customerDatabase.deleteCustomer(id)){
+            System.out.println("Customer is deleted");
+        } else {
+            System.out.println("There's no such id " + id + " in database");
+        }
+    }
+}
