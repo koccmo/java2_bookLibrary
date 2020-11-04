f090a27ea91322d248c43a79ff8d91f9abd1239d
Jaroslav <brutan.jaroslav@gmail.com>
2020-11-04 23:18:47 +0200

    Customer update

---
+++ team_no_name_no_problem/src/main/java/internet_store/UI/FindCustomersByNameAndSurnameUIAction.java
@@ -0,0 +1,23 @@
+package internet_store.UI;
+
+import internet_store.CustomerDatabase;
+
+public class FindCustomersByNameAndSurnameUIAction implements UIAction{
+
+    private CustomerDatabase customerDatabase;
+    InputCheckUtility inputCheckUtility = new InputCheckUtility();
+
+    public FindCustomersByNameAndSurnameUIAction(CustomerDatabase personDatabase){
+        this.customerDatabase = personDatabase;
+    }
+
+    public void execute(){
+        String name = inputCheckUtility.inputValidString("Please enter customers name for search: ");
+        String surname = inputCheckUtility.inputValidString("Please enter customers surname for search: ");
+        if (customerDatabase.findCustomersByNameAndSurname(name, surname).isPresent()){
+            System.out.println(customerDatabase.findCustomersByNameAndSurname(name,surname).get());
+        } else {
+            System.out.println("Customer with " + " name and surname isn't presented in database!");
+        }
+    }
+}
