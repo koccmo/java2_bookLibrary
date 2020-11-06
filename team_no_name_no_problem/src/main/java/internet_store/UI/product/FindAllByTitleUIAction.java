fa4c5bc2ac6d1c0cd62cb5c67f07ebf217954a14
ValerijaI <55547022+ValerijaI@users.noreply.github.com>
2020-11-06 15:01:51 +0200

    FindAllByTitleService added

--- team_no_name_no_problem/src/main/java/internet_store/UI/product/FindAllByTitleUIAction.java
+++ team_no_name_no_problem/src/main/java/internet_store/UI/product/FindAllByTitleUIAction.java
@@ -4,22 +4,23 @@ import internet_store.UI.InputCheckUtility;
 import internet_store.UI.UIAction;
 import internet_store.database.product.ProductDatabase;
 import internet_store.domain.Product;
+import internet_store.services.product.FindAllByTitleService;
 
 import java.util.List;
 
 public class FindAllByTitleUIAction implements UIAction {
 
-    private ProductDatabase productDatabase;
+    private FindAllByTitleService findAllByTitle;
     InputCheckUtility inputCheckUtility = new InputCheckUtility();
 
-    public FindAllByTitleUIAction(ProductDatabase itemDatabase){
-        this.productDatabase = itemDatabase;
+    public FindAllByTitleUIAction(FindAllByTitleService findAllByTitle) {
+        this.findAllByTitle = findAllByTitle;
     }
 
     public void execute(){
         String title = inputCheckUtility.inputValidString("Please enter product's title for search: ");
 
-        List<Product> result = productDatabase.findAllByTitle(title);
+        List<Product> result = findAllByTitle.execute(title);
         if (result.size() == 0){
             System.out.println("Product with title " + title +" isn't present in database");
         }else{
