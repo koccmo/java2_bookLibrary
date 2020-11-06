36970f41f1f1717131208854f0aa409439eedc06
ValerijaI <55547022+ValerijaI@users.noreply.github.com>
2020-11-06 10:31:16 +0200

    Tests refactoring

---
+++ team_healthy_tooth/src/test/java/dental_clinic/database/PatientDatabaseImplTest.java
@@ -0,0 +1,56 @@
+package dental_clinic.database;
+
+import dental_clinic.domain.Patient;
+import dental_clinic.domain.PersonalData;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class PatientDatabaseImplTest {
+    PersonalData personalData1 = new PersonalData("Bob", "Bobbins", "12345678", "12345678900");
+    PersonalData personalData2 = new PersonalData("John", "Joninns", "12345670", "12345678999");
+    String doctor1 = "Doktor Zlo";
+    String doctor2 = "Doktor Haus";
+    PatientDatabase patientDatabase = new PatientDatabaseImpl();
+    @Before
+    public void init(){
+        patientDatabase.addPatient(personalData1);
+    }
+
+    @Test
+    public void testGetList(){
+        List<Patient> expectedList = new ArrayList<>();
+        Patient patient = new Patient(personalData1);
+        expectedList.add(patient);
+        assertTrue(patientDatabase.getPatientList().equals(expectedList));
+    }
+
+    @Test
+    public void testAddPatient(){
+        patientDatabase.addPatient(personalData2);
+        patientDatabase.getPatientList().contains(personalData2);
+    }
+
+    @Test
+    public void testIdGeneration(){
+        assertTrue(patientDatabase.getPatientList().get(0).getPersonalData().getId() == 1);
+        patientDatabase.addPatient(personalData2);
+        assertTrue(patientDatabase.getPatientList().get(1).getPersonalData().getId() == 2);
+    }
+
+    @Test
+    public void testDeletePatient(){
+        assertTrue(patientDatabase.getPatientList().size() == 1);
+        patientDatabase.deletePatient(1);
+        assertTrue(patientDatabase.getPatientList().size() == 0);
+    }
+
+
+
+
+
+}
