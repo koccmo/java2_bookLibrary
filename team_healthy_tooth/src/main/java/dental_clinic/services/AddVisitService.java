b9510103481eb8f5d83f56a50422b3f75c795b23
ValerijaI <55547022+ValerijaI@users.noreply.github.com>
2020-11-07 22:48:36 +0200

    AddVisitService corrected

--- team_healthy_tooth/src/main/java/dental_clinic/services/AddVisitService.java
+++ team_healthy_tooth/src/main/java/dental_clinic/services/AddVisitService.java
@@ -2,6 +2,7 @@ package dental_clinic.services;
 
 import dental_clinic.database.PatientDatabase;
 import dental_clinic.domain.ToothStatus;
+import dental_clinic.domain.Visit;
 
 import java.util.Optional;
 
@@ -13,13 +14,19 @@ public class AddVisitService {
         this.patientDatabase = patientDatabase;
     }
 
-    public boolean execute(long id, int toothNumber, Optional<String> comment, ToothStatus toothStatus){
+    public boolean execute(long id, int toothNumber, Optional<String> comment, ToothStatus toothStatus, String doctor){
         for (int i = 0; i < patientDatabase.getPatients().size(); i++){
-            if (patientDatabase.getPatients().get(i).getPersonalData().getId() == id){
-                patientDatabase.addVisit(id, toothNumber, comment, toothStatus);
+            if (isSpecificPatient(i, id)){
+                Visit visit = new Visit(toothNumber, comment, toothStatus, doctor);
+                patientDatabase.getPatients().get(i).addVisit(visit);
+                patientDatabase.getPatients().get(i).updateJowl(toothNumber, toothStatus);
                 return true;
             }
         }
         return false;
+    }
+
+    private boolean isSpecificPatient (int index, long id) {
+        return patientDatabase.getPatients().get(index).getPersonalData().getId() == id;
     }
 }
