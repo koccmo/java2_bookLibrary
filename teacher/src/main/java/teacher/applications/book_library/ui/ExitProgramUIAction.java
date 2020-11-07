5cbc5ad6eca217267f54e3b317b2aafc976ec343
vsavonins <viktors.savonins@proofit.lv>
2020-11-07 09:49:00 +0200

    Lesson 1, 2, 3, 4

--- teacher/src/main/java/teacher/applications/book_library/ui/ExitProgramUIAction.java
+++
@@ -1,9 +0,0 @@
-package teacher.applications.book_library.ui;
-
-public class ExitProgramUIAction implements UIAction {
-	@Override
-	public void execute() {
-		System.out.println("Good by!");
-		System.exit(0);
-	}
-}
