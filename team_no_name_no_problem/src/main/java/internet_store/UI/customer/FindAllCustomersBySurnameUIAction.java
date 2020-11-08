50142dbeadabe2ef598c678596766e106a4e4231
Jaroslav <brutan.jaroslav@gmail.com>
2020-11-08 23:37:17 +0200

    Customer business logic

--- team_no_name_no_problem/src/main/java/internet_store/UI/customer/FindAllCustomersBySurnameUIAction.java
+++ team_no_name_no_problem/src/main/java/internet_store/UI/customer/FindAllCustomersBySurnameUIAction.java
@@ -4,22 +4,23 @@ import internet_store.UI.InputCheckUtility;
 import internet_store.UI.UIAction;
 import internet_store.database.customer.CustomerDatabase;
 import internet_store.domain.Customer;
+import internet_store.services.customer.FindAllCustomersBySurnameService;
 
 import java.util.List;
 
 public class FindAllCustomersBySurnameUIAction implements UIAction {
 
-    private CustomerDatabase customerDatabase;
+    private FindAllCustomersBySurnameService findAllCustomersBySurname;
     InputCheckUtility inputCheckUtility = new InputCheckUtility();
 
-    public FindAllCustomersBySurnameUIAction(CustomerDatabase customerDatabase) {
-        this.customerDatabase = customerDatabase;
+    public FindAllCustomersBySurnameUIAction(FindAllCustomersBySurnameService findAllCustomersBySurname) {
+        this.findAllCustomersBySurname = findAllCustomersBySurname;
     }
 
     public void execute(){
         String surname = inputCheckUtility.inputValidString("Please enter customer's surname for search: ");
 
-        List<Customer> result = customerDatabase.findAllCustomersBySurname(surname);
+        List<Customer> result = findAllCustomersBySurname.execute(surname);
         if (result.size() == 0){
             System.out.println("Customer's with surname " + surname + " isn't presented in database!");
         } else {
