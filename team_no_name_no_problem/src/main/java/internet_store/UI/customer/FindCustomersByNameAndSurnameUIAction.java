73496c5ec4b5b6905202f7f1fde9aa5ef9ce02a1
Jaroslav <brutan.jaroslav@gmail.com>
2020-11-08 23:37:38 +0200

    Customer business logic

--- team_no_name_no_problem/src/main/java/internet_store/UI/customer/FindCustomersByNameAndSurnameUIAction.java
+++ team_no_name_no_problem/src/main/java/internet_store/UI/customer/FindCustomersByNameAndSurnameUIAction.java
@@ -3,21 +3,23 @@ package internet_store.UI.customer;
 import internet_store.UI.InputCheckUtility;
 import internet_store.UI.UIAction;
 import internet_store.database.customer.CustomerDatabase;
+import internet_store.services.customer.FindAllCustomersByNameAndSurnameService;
 
 public class FindCustomersByNameAndSurnameUIAction implements UIAction {
 
-    private CustomerDatabase customerDatabase;
+    private FindAllCustomersByNameAndSurnameService findAllCustomersByNameAndSurnameService;
     InputCheckUtility inputCheckUtility = new InputCheckUtility();
 
-    public FindCustomersByNameAndSurnameUIAction(CustomerDatabase personDatabase){
-        this.customerDatabase = personDatabase;
+    public FindCustomersByNameAndSurnameUIAction(FindAllCustomersByNameAndSurnameService
+                                                         findAllCustomersByNameAndSurnameService){
+        this.findAllCustomersByNameAndSurnameService = findAllCustomersByNameAndSurnameService;
     }
 
     public void execute(){
         String name = inputCheckUtility.inputValidString("Please enter customers name for search: ");
         String surname = inputCheckUtility.inputValidString("Please enter customers surname for search: ");
-        if (customerDatabase.findCustomersByNameAndSurname(name, surname).isPresent()){
-            System.out.println(customerDatabase.findCustomersByNameAndSurname(name,surname).get());
+        if (findAllCustomersByNameAndSurnameService.execute(name, surname).isPresent()){
+            System.out.println(findAllCustomersByNameAndSurnameService.execute(name,surname).get());
         } else {
             System.out.println("Customer with " + " name and surname isn't presented in database!");
         }
