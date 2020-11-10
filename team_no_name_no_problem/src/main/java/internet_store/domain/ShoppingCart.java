7c3095ebeb075789cdf8f0a20f6dec4672f3ca68
ValerijaI <55547022+ValerijaI@users.noreply.github.com>
2020-11-10 21:59:23 +0200

    ShoppingCart ideas for Jaroslav

--- team_no_name_no_problem/src/main/java/internet_store/domain/ShoppingCart.java
+++ team_no_name_no_problem/src/main/java/internet_store/domain/ShoppingCart.java
@@ -5,12 +5,24 @@ import java.util.Map;
 
 public class ShoppingCart {
 
-
     Product product;
-    Integer price;
+    Integer quantity;
 
     Map <Product, Integer> shoppingCart = new HashMap <>();
 
+    void addProduct(Product product, Integer quantity){
+        shoppingCart.put(product, quantity);
+    }
+
+    void deleteProduct(Product product){
+        if (shoppingCart.keySet().contains(product)){
+            shoppingCart.remove(product);
+        }
+    }
+
+    void changeProductQuantity(Product product, int quantity){
+        shoppingCart.put(product, quantity);
+    }
 
 
     /*
