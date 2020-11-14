b59133636f40f228fe47c91f5b72b80cefa6525b
ValerijaI <55547022+ValerijaI@users.noreply.github.com>
2020-11-14 21:30:46 +0200

    GetAllProductsUIAction

--- team_no_name_no_problem/src/main/java/internet_store/console_ui/product/GetAllProductsUIAction.java
+++ team_no_name_no_problem/src/main/java/internet_store/console_ui/product/GetAllProductsUIAction.java
@@ -1,6 +1,8 @@
 package internet_store.console_ui.product;
 
 import internet_store.console_ui.UIAction;
+import internet_store.core.requests.product.GetProductsRequest;
+import internet_store.core.response.product.GetProductsResponse;
 import internet_store.core.services.product.GetAllProductsService;
 
 public class GetAllProductsUIAction implements UIAction {
@@ -12,10 +14,14 @@ public class GetAllProductsUIAction implements UIAction {
     }
 
     public void execute(){
-        if (getAllProductsService.execute().isEmpty()){
-            System.out.println("Database is empty");
+
+        GetProductsRequest getProductsRequest = new GetProductsRequest();
+        GetProductsResponse getProductsResponse = getAllProductsService.execute(getProductsRequest);
+
+        if (getProductsResponse.hasErrors()){
+            getProductsResponse.getErrors().forEach(System.out::println);
         }else
-            getAllProductsService.execute().forEach(System.out::println);
+            getProductsResponse.getProducts().forEach(System.out::println);
     }
 
 }
