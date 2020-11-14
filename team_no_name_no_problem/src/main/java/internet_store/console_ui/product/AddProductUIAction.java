16ad47843aaa643caac58f91d1b4ab318c378b30
ValerijaI <55547022+ValerijaI@users.noreply.github.com>
2020-11-14 21:56:49 +0200

    AddProductUIAction

--- team_no_name_no_problem/src/main/java/internet_store/console_ui/product/AddProductUIAction.java
+++ team_no_name_no_problem/src/main/java/internet_store/console_ui/product/AddProductUIAction.java
@@ -3,26 +3,42 @@ package internet_store.console_ui.product;
 import internet_store.console_ui.InputCheckUtility;
 import internet_store.console_ui.UIAction;
 import internet_store.core.domain.Product;
+import internet_store.core.requests.product.AddProductRequest;
+import internet_store.core.response.product.AddProductResponse;
 import internet_store.core.services.product.AddProductService;
+
+import java.util.Scanner;
 
 public class AddProductUIAction implements UIAction {
 
     private AddProductService addProductService;
-    InputCheckUtility inputCheckUtility = new InputCheckUtility();
 
     public AddProductUIAction(AddProductService addProductService) {
         this.addProductService = addProductService;
     }
 
     public void execute(){
-        String title = inputCheckUtility.inputValidString("Please enter product's title: ");
 
-        String description = inputCheckUtility.inputValidString("Please enter product's description");
+        Scanner in = new Scanner(System.in);
 
-        int price = inputCheckUtility.inputValidInteger("Please enter product's price");
+        System.out.println("Please enter product's title: ");
+        String title = in.nextLine();
+
+        System.out.println("Please enter product's description");
+        String description = in.nextLine();
+
+        System.out.println("Please enter product's price");
+        int price = in.nextInt();
 
         Product newProduct = new Product(title, description, price);
-        addProductService.execute(newProduct);
+
+        AddProductRequest addProductRequest = new AddProductRequest(newProduct);
+        AddProductResponse addProductResponse = addProductService.execute(addProductRequest);
+        if (addProductResponse.hasErrors()){
+            addProductResponse.getErrors().forEach(System.out::println);
+        }else {
+            System.out.println("Product was successfully added");
+        }
     }
 
 }
