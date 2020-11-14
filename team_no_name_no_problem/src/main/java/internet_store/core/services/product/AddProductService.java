85265c0e8f88700a0545721fb276e81d5a7a0e2b
ValerijaI <55547022+ValerijaI@users.noreply.github.com>
2020-11-14 21:57:32 +0200

    AddProductService

--- team_no_name_no_problem/src/main/java/internet_store/core/services/product/AddProductService.java
+++ team_no_name_no_problem/src/main/java/internet_store/core/services/product/AddProductService.java
@@ -1,23 +1,37 @@
 package internet_store.core.services.product;
 
+import internet_store.core.requests.product.AddProductRequest;
+import internet_store.core.response.CoreError;
+import internet_store.core.response.product.AddProductResponse;
 import internet_store.database.product.ProductDatabase;
 import internet_store.core.domain.Product;
+
+import java.util.List;
 
 public class AddProductService {
 
     private final ProductDatabase productDatabase;
+    private final AddProductValidator addProductValidator;
 
-    public AddProductService(ProductDatabase productDatabase) {
+    public AddProductService(ProductDatabase productDatabase, AddProductValidator addProductValidator) {
         this.productDatabase = productDatabase;
+        this.addProductValidator = addProductValidator;
     }
 
-    public boolean execute(Product product){
-        if (productDatabase.getProducts().contains(product)){
-            return false;
-        }else{
-            productDatabase.add(product);
-            return true;
+    public AddProductResponse execute(AddProductRequest addProductRequest){
+
+        List<CoreError>errors = addProductValidator.validate(addProductRequest);
+        if (!errors.isEmpty()){
+            return new AddProductResponse(errors);
         }
+
+        if (productDatabase.getProducts().contains(addProductRequest.getProduct())){
+            errors.add(new CoreError("database", "Database contains the same product"));
+            return new AddProductResponse(errors);
+        }
+
+        productDatabase.add(addProductRequest.getProduct());
+        return new AddProductResponse(addProductRequest.getProduct());
     }
 
 }
