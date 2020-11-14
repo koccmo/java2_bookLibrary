86e2e37d4fc9433e094cc35bc888a6f40bb1077b
Artur M <kpackalv@yahoo.com>
2020-11-14 12:56:06 +0200

    StaticStartup L3 DeleteByProductName Request/Response/Validation

--- team_static_startup/src/main/java/internet_store/application/core/services/DeleteProductService.java
+++ team_static_startup/src/main/java/internet_store/application/core/services/DeleteProductService.java
@@ -1,18 +1,32 @@
 package internet_store.application.core.services;
 
-import internet_store.application.database.Database;
 import internet_store.application.core.domain.Product;
+import internet_store.application.core.requests.DeleteByProductNameRequest;
+import internet_store.application.core.responses.CoreError;
+import internet_store.application.core.responses.DeleteByProductNameResponse;
+import internet_store.application.database.Database;
+
+import java.util.List;
 
 public class DeleteProductService {
 
-private final Database database;
+    private final Database database;
+    private DeleteByProductNameValidator validator;
 
-    public DeleteProductService(Database database) {
+    public DeleteProductService(Database database, DeleteByProductNameValidator validator) {
         this.database = database;
+        this.validator = validator;
     }
 
-    public boolean deleteByProductName(String productName) {
-        return database.deleteByProductName(productName);
+    public DeleteByProductNameResponse deleteByProductName(DeleteByProductNameRequest productNameRequest) {
+        List<CoreError> errors = validator.validate(productNameRequest);
+        if (!errors.isEmpty()){
+            return new DeleteByProductNameResponse(errors);
+        }
+
+        boolean isRemoved = database.deleteByProductName(productNameRequest.getProductName());
+
+        return new DeleteByProductNameResponse(isRemoved);
     }
 
     public boolean delete(Product product) {
