46a7d71c60dd18f7aadae20651843defce7cc419
ValerijaI <55547022+ValerijaI@users.noreply.github.com>
2020-11-14 13:58:43 +0200

    DeletePatientServices

--- team_healthy_tooth/src/main/java/dental_clinic/core/services/DeletePatientService.java
+++ team_healthy_tooth/src/main/java/dental_clinic/core/services/DeletePatientService.java
@@ -1,24 +1,39 @@
 package dental_clinic.core.services;
 
+import dental_clinic.core.requests.DeletePatientRequest;
+import dental_clinic.core.responses.CoreError;
+import dental_clinic.core.responses.DeletePatientResponse;
 import dental_clinic.database.PatientDatabase;
 import dental_clinic.core.domain.PersonalData;
+import java.util.List;
 
 public class DeletePatientService {
 
     private final PatientDatabase patientDatabase;
+    private DeletePatientValidator deletePatientValidator;
 
-    public DeletePatientService(PatientDatabase patientDatabase) {
+    public DeletePatientService(PatientDatabase patientDatabase, DeletePatientValidator deletePatientValidator) {
         this.patientDatabase = patientDatabase;
+        this.deletePatientValidator = deletePatientValidator;
     }
 
-    public boolean deletePatient(long id){
+    public DeletePatientResponse execute(DeletePatientRequest deletePatientRequest){
+
+        List<CoreError> errors = deletePatientValidator.validate(deletePatientRequest);
+
+        if (!errors.isEmpty()){
+            return new DeletePatientResponse(errors);
+        }
+
         for (int i = 0; i < patientDatabase.getPatients().size(); i++){
-            if (getCurrentPatientPersonalData(i).getId() == id){
-                patientDatabase.deletePatient(id);
-                return true;
+            if (getCurrentPatientPersonalData(i).getId() == deletePatientRequest.getId()){
+                patientDatabase.deletePatient(deletePatientRequest.getId());
+                return new DeletePatientResponse(deletePatientRequest.getId());
             }
         }
-        return false;
+
+        errors.add(new CoreError("id", "Database doesn't contain patient with id"));
+        return new DeletePatientResponse(errors);
     }
 
     private PersonalData getCurrentPatientPersonalData(int index){
