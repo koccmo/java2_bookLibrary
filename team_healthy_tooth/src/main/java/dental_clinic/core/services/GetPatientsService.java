e489a9d00631fef0942d9cfba0852f10fb042126
ValerijaI <55547022+ValerijaI@users.noreply.github.com>
2020-11-14 15:23:56 +0200

    GetPatientsService

--- team_healthy_tooth/src/main/java/dental_clinic/core/services/GetPatientsService.java
+++ team_healthy_tooth/src/main/java/dental_clinic/core/services/GetPatientsService.java
@@ -1,9 +1,14 @@
 package dental_clinic.core.services;
 
-import dental_clinic.database.PatientDatabase;
 import dental_clinic.core.domain.Patient;
+import dental_clinic.core.requests.GetPatientRequest;
+import dental_clinic.core.responses.CoreError;
+import dental_clinic.core.responses.GetPatientsResponse;
+import dental_clinic.database.PatientDatabase;
 
+import java.util.ArrayList;
 import java.util.List;
+
 
 public class GetPatientsService {
 
@@ -13,7 +18,15 @@ public class GetPatientsService {
         this.patientDatabase = patientDatabase;
     }
 
-    public List<Patient>execute(){
-        return  patientDatabase.getPatients();
+    public GetPatientsResponse execute(GetPatientRequest getPatientRequest){
+        List <CoreError> errors = new ArrayList<>();
+
+        if (patientDatabase.getPatients().isEmpty()){
+            errors.add(new CoreError("database", "Database is empty"));
+            return new GetPatientsResponse(errors, new ArrayList<>());
+        }
+
+        List<Patient>patients = patientDatabase.getPatients();
+        return new GetPatientsResponse(patients);
     }
 }
