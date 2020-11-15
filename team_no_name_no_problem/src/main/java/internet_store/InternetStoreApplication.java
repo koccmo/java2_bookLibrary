80fa578b311f2be6d49ba6ea4e0b6cd3bd014361
ValerijaI <55547022+ValerijaI@users.noreply.github.com>
2020-11-15 22:33:18 +0200

    InternetStoreApplication rename

--- team_no_name_no_problem/src/main/java/internet_store/InternetStoreApplication.java
+++ team_no_name_no_problem/src/main/java/internet_store/InternetStoreApplication.java
@@ -2,7 +2,7 @@ package internet_store;
 
 import internet_store.console_ui.InternetStore;
 
-public class ProductListApplication {
+public class InternetStoreApplication {
 
     public static void main(String[] args) {
         InternetStore internetStore = new InternetStore();
