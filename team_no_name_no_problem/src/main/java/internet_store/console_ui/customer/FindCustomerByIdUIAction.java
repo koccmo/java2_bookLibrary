51914f3593584ade5849cd98a32063af5dc023ec
Jaroslav <brutan.jaroslav@gmail.com>
2020-11-18 00:44:21 +0200

    Find customer by id ui action

---
+++ team_no_name_no_problem/src/main/java/internet_store/console_ui/customer/FindCustomerByIdUIAction.java
@@ -0,0 +1,36 @@
+package internet_store.console_ui.customer;
+
+
+import internet_store.console_ui.UIAction;
+import internet_store.core.requests.customer.FindCustomerByIdRequest;
+import internet_store.core.response.customer.FindCustomerByIdResponse;
+import internet_store.core.services.customer.FindCustomerByIdService;
+
+import java.util.Scanner;
+
+public class FindCustomerByIdUIAction implements UIAction {
+
+    private FindCustomerByIdService findCustomerByIdService;
+
+    public FindCustomerByIdUIAction(FindCustomerByIdService findCustomerByIdService){
+        this.findCustomerByIdService = findCustomerByIdService;
+    }
+
+    public void execute(){
+
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Please enter customers id");
+        long id = in.nextLong();
+
+        FindCustomerByIdRequest findCustomersByIdRequest = new FindCustomerByIdRequest(id);
+        FindCustomerByIdResponse findCUstomerByIdResponse = findCustomerByIdService.execute(findCustomersByIdRequest);
+
+        if (findCUstomerByIdResponse.hasErrors()){
+            findCUstomerByIdResponse.getErrors().forEach(System.out::println);
+        } else {
+            System.out.println("Customer with id: " + id);
+            System.out.println(findCUstomerByIdResponse.getCustomer());
+        }
+    }
+}
