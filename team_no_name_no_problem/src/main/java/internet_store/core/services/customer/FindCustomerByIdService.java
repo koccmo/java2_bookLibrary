ceee4c4c7e1fc3806955963c4b0c6b651ae86b11
Jaroslav <brutan.jaroslav@gmail.com>
2020-11-18 00:44:08 +0200

    Find customer by id service

---
+++ team_no_name_no_problem/src/main/java/internet_store/core/services/customer/FindCustomerByIdService.java
@@ -0,0 +1,38 @@
+package internet_store.core.services.customer;
+
+import internet_store.core.domain.Customer;
+import internet_store.core.requests.customer.FindCustomerByIdRequest;
+import internet_store.core.response.CoreError;
+import internet_store.core.response.customer.FindCustomerByIdResponse;
+import internet_store.database.customer.CustomerDatabase;
+
+import java.util.List;
+import java.util.Optional;
+
+public class FindCustomerByIdService {
+
+    private final CustomerDatabase customerDatabase;
+    private final FindCustomerByIdRequestValidator findCustomerByIdRequestValidator;
+
+    public FindCustomerByIdService(CustomerDatabase customerDatabase, FindCustomerByIdRequestValidator findCustomerByIdRequestValidator) {
+        this.customerDatabase = customerDatabase;
+        this.findCustomerByIdRequestValidator = findCustomerByIdRequestValidator;
+    }
+
+    public FindCustomerByIdResponse execute(FindCustomerByIdRequest findCustomerByIdRequest){
+
+        List<CoreError> errors = findCustomerByIdRequestValidator.validate(findCustomerByIdRequest);
+
+        if (!errors.isEmpty()){
+            return new FindCustomerByIdResponse(errors);
+        }
+        Optional<Customer> expectedCustomer = customerDatabase.findById(findCustomerByIdRequest.getId());
+
+        if (expectedCustomer.isEmpty()){
+            errors.add(new CoreError("database", "Database doesn't contain customer with this id "
+            + findCustomerByIdRequest.getId()));
+            return new FindCustomerByIdResponse(errors);
+        }
+        return new FindCustomerByIdResponse(customerDatabase.findById(findCustomerByIdRequest.getId()).get());
+    }
+}
