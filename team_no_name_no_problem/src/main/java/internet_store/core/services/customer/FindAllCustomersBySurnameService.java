f8c90377a74951e5afe916f3886fd5a925b0dc6e
Jaroslav <brutan.jaroslav@gmail.com>
2020-11-18 00:42:36 +0200

    Find all customers by s service update

--- team_no_name_no_problem/src/main/java/internet_store/core/services/customer/FindAllCustomersBySurnameService.java
+++ team_no_name_no_problem/src/main/java/internet_store/core/services/customer/FindAllCustomersBySurnameService.java
@@ -1,10 +1,9 @@
 package internet_store.core.services.customer;
 
-import internet_store.core.requests.customer.FindCustomerBySurnameRequest;
+import internet_store.core.requests.customer.FindAllCustomersBySurnameRequest;
 import internet_store.core.response.CoreError;
-import internet_store.core.response.customer.FindCustomerBySurnameResponse;
+import internet_store.core.response.customer.FindAllCustomersBySurnameResponse;
 import internet_store.database.customer.CustomerDatabase;
-import internet_store.core.domain.Customer;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -12,25 +11,25 @@ import java.util.List;
 public class FindAllCustomersBySurnameService {
 
     private final CustomerDatabase customerDatabase;
-    private final FindCustomerBySurnameValidator findCustomerBySurnameValidator;
+    private final FindAllCustomersBySurnameValidator findAllCustomersBySurnameValidator;
 
-    public FindAllCustomersBySurnameService(CustomerDatabase customerDatabase, FindCustomerBySurnameValidator findCustomerBySurnameValidator) {
+    public FindAllCustomersBySurnameService(CustomerDatabase customerDatabase, FindAllCustomersBySurnameValidator findAllCustomersBySurnameValidator) {
         this.customerDatabase = customerDatabase;
-        this.findCustomerBySurnameValidator = findCustomerBySurnameValidator;
+        this.findAllCustomersBySurnameValidator = findAllCustomersBySurnameValidator;
     }
 
-    public FindCustomerBySurnameResponse execute(FindCustomerBySurnameRequest findCustomerBySurnameRequest){
-        List<CoreError> errors = findCustomerBySurnameValidator.validate(findCustomerBySurnameRequest);
+    public FindAllCustomersBySurnameResponse execute(FindAllCustomersBySurnameRequest findAllCustomersBySurnameRequest){
+        List<CoreError> errors = findAllCustomersBySurnameValidator.validate(findAllCustomersBySurnameRequest);
         if (!errors.isEmpty()){
-            return new FindCustomerBySurnameResponse(errors, new ArrayList<>());
+            return new FindAllCustomersBySurnameResponse(errors, new ArrayList<>());
         }
 
-        if (customerDatabase.findAllCustomersBySurname(findCustomerBySurnameRequest.getSurname()).isEmpty()){
+        if (customerDatabase.findAllCustomersBySurname(findAllCustomersBySurnameRequest.getSurname()).isEmpty()){
             errors.add(new CoreError("database", "Database doesn't contain such customer surname"
-            + findCustomerBySurnameRequest.getSurname()));
-        return new FindCustomerBySurnameResponse(errors, new ArrayList<>());
+            + findAllCustomersBySurnameRequest.getSurname()));
+        return new FindAllCustomersBySurnameResponse(errors, new ArrayList<>());
     }
-        return new FindCustomerBySurnameResponse(customerDatabase.findAllCustomersBySurname
-                (findCustomerBySurnameRequest.getSurname()));
+        return new FindAllCustomersBySurnameResponse(customerDatabase.findAllCustomersBySurname
+                (findAllCustomersBySurnameRequest.getSurname()));
     }
 }
