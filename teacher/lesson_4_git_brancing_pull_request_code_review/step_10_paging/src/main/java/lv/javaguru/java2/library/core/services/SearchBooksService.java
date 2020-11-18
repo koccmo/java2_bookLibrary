5d0d420806d7e3e5eed1210d2f6961eaa93887f7
vsavonins <viktors.savonins@proofit.lv>
2020-11-18 12:59:38 +0200

    Lesson 4 ordering fix

--- teacher/lesson_4_git_brancing_pull_request_code_review/step_10_paging/src/main/java/lv/javaguru/java2/library/core/services/SearchBooksService.java
+++ teacher/lesson_4_git_brancing_pull_request_code_review/step_10_paging/src/main/java/lv/javaguru/java2/library/core/services/SearchBooksService.java
@@ -2,10 +2,10 @@ package lv.javaguru.java2.library.core.services;
 
 import java.util.Comparator;
 import java.util.List;
-import java.util.stream.Collectors;
 
 import lv.javaguru.java2.library.Book;
 import lv.javaguru.java2.library.core.database.Database;
+import lv.javaguru.java2.library.core.requests.Ordering;
 import lv.javaguru.java2.library.core.requests.SearchBooksRequest;
 import lv.javaguru.java2.library.core.responses.CoreError;
 import lv.javaguru.java2.library.core.responses.SearchBooksResponse;
@@ -29,23 +29,24 @@ public class SearchBooksService {
 
 		List<Book> books = null;
 		if (request.isTitleProvided() && !request.isAuthorProvided()) {
-			books = database.findByTitle(request.getTitle())
-					.stream()
-					.sorted(Comparator.comparing(Book::getTitle))
-					.collect(Collectors.toList());
+			books = database.findByTitle(request.getTitle());
 		}
 		if (!request.isTitleProvided() && request.isAuthorProvided()) {
-			books = database.findByAuthor(request.getAuthor())
-					.stream()
-					.sorted(Comparator.comparing(Book::getAuthor))
-					.collect(Collectors.toList());
+			books = database.findByAuthor(request.getAuthor());
 		}
 		if (request.isTitleProvided() && request.isAuthorProvided()) {
-			books = database.findByTitleAndAuthor(request.getTitle(), request.getAuthor())
-					.stream()
-					.sorted(Comparator.comparing(Book::getTitle)
-									.thenComparing(Book::getAuthor))
-					.collect(Collectors.toList());
+			books = database.findByTitleAndAuthor(request.getTitle(), request.getAuthor());
+		}
+
+		if (request.getOrdering() != null) {
+			Ordering ordering = request.getOrdering();
+			Comparator<Book> comparator = ordering.getOrderBy().equals("title")
+					? Comparator.comparing(Book::getTitle)
+					: Comparator.comparing(Book::getAuthor);
+			if (ordering.getOrderDirection().equals("DESCENDING")) {
+				comparator = comparator.reversed();
+			}
+			books.sort(comparator);
 		}
 
 		return new SearchBooksResponse(books, null);
