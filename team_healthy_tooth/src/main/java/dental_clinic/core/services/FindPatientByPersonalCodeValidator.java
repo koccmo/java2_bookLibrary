820adb8d215d582284903245b2fc0b74ab8f18a7
ValerijaI <55547022+ValerijaI@users.noreply.github.com>
2020-11-18 12:40:16 +0200

    DC refactoring

--- team_healthy_tooth/src/main/java/dental_clinic/core/services/FindPatientByPersonalCodeValidator.java
+++ team_healthy_tooth/src/main/java/dental_clinic/core/services/FindPatientByPersonalCodeValidator.java
@@ -1,11 +1,9 @@
 package dental_clinic.core.services;
 
-import dental_clinic.core.domain.PersonalData;
 import dental_clinic.core.requests.FindPatientByPersonalCodeRequest;
 import dental_clinic.core.responses.CoreError;
 
 import java.util.ArrayList;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.regex.Pattern;
 
