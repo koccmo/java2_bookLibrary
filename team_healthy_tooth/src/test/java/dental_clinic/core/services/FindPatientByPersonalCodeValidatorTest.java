3993a783a2336c838cf38e8f587550384dc8e2e2
binco <vladimirs.danilins@gmail.com>
2020-11-19 03:08:47 +0200

    FindPatientByPersonalCode validator tests v2 (regex only)

--- team_healthy_tooth/src/test/java/dental_clinic/core/services/FindPatientByPersonalCodeValidatorTest.java
+++ team_healthy_tooth/src/test/java/dental_clinic/core/services/FindPatientByPersonalCodeValidatorTest.java
@@ -3,47 +3,53 @@ package dental_clinic.core.services;
 import dental_clinic.core.requests.FindPatientByPersonalCodeRequest;
 import dental_clinic.core.responses.CoreError;
 import junit.framework.TestCase;
-
-import java.util.ArrayList;
 import java.util.List;
 
 public class FindPatientByPersonalCodeValidatorTest extends TestCase {
 
+    FindPatientByPersonalCodeValidator validator;
+    List<CoreError> testErrorList;
+
     public void testValidate() {
-        FindPatientByPersonalCodeValidator validator = new FindPatientByPersonalCodeValidator();
-        List<CoreError> testErrorList;
+        this.validator = new FindPatientByPersonalCodeValidator();
+        FindPatientByPersonalCodeValidatorTest test = new FindPatientByPersonalCodeValidatorTest();
 
-        List<CoreError> emptyErrorList = new ArrayList<>(); //for good requests
+        test.testGoodRequest1();
+        test.testGoodRequest2();
 
-        FindPatientByPersonalCodeRequest requestGood1 =
+        test.testBadRequest1();
+        test.testBadRequest2();
+    }
+
+    private boolean isListEmpty(List<CoreError> testErrorList) {
+        return testErrorList.isEmpty();
+    }
+
+    public void testGoodRequest1() {
+        FindPatientByPersonalCodeRequest goodRequest =
                 new FindPatientByPersonalCodeRequest("123456-12345");
-        testErrorList = validator.validate(requestGood1);
-        //assertEquals(testErrorList, emptyErrorList);
+        testErrorList = validator.validate(goodRequest);
+        assertTrue(isListEmpty(testErrorList));
+    }
 
-        testErrorList.clear();
-
-        FindPatientByPersonalCodeRequest requestGood2 =
+    public void testGoodRequest2() {
+        FindPatientByPersonalCodeRequest goodRequest =
                 new FindPatientByPersonalCodeRequest("12345612345");
-        testErrorList = validator.validate(requestGood2);
-        //assertEquals(testErrorList, emptyErrorList);
+        testErrorList = validator.validate(goodRequest);
+        assertTrue(isListEmpty(testErrorList));
+    }
 
-        testErrorList.clear();
+    public void testBadRequest1() {
+        FindPatientByPersonalCodeRequest badRequest =
+                new FindPatientByPersonalCodeRequest("x23456-12345");
+        testErrorList = validator.validate(badRequest);
+        assertFalse(isListEmpty(testErrorList));
+    }
 
-        List<CoreError> personalCodeErrorInList = new ArrayList<>(); //for bad requests
-        personalCodeErrorInList.add(new CoreError("Personal data : personal code",
-                        "Invalid input for personal code!"));
-
-        FindPatientByPersonalCodeRequest requestBad1 =
-                new FindPatientByPersonalCodeRequest("personalnij kod");
-        testErrorList = validator.validate(requestBad1);
-        assertEquals(testErrorList, personalCodeErrorInList);
-
-        testErrorList.clear();
-
-        FindPatientByPersonalCodeRequest requestBad2 =
+    public void testBadRequest2() {
+        FindPatientByPersonalCodeRequest badRequest =
                 new FindPatientByPersonalCodeRequest("123456-123456");
-        testErrorList = validator.validate(requestBad2);
-        assertEquals(testErrorList, personalCodeErrorInList);
-
+        testErrorList = validator.validate(badRequest);
+        assertFalse(isListEmpty(testErrorList));
     }
 }
