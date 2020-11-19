fa754272a5be65cba9d55f8d0d421032af4df5a7
AlexBogachenkov <aleksbogac@gmail.com>
2020-11-19 19:00:41 +0200

    StaticStartup deleteByProduct request/response/validation/UIAction service added

--- team_static_startup/src/main/java/internet_store/application/core/services/DeleteProductService.java
+++ team_static_startup/src/main/java/internet_store/application/core/services/DeleteProductService.java
@@ -10,22 +10,39 @@ import java.util.List;
 public class DeleteProductService {
 
     private final Database database;
-    private final DeleteByProductNameValidator validator;
+    private DeleteByProductNameValidator deleteByNameValidator;
+    private DeleteByProductValidator deleteByProductValidator;
 
     public DeleteProductService(Database database, DeleteByProductNameValidator validator) {
         this.database = database;
-        this.validator = validator;
+        this.deleteByNameValidator = validator;
+    }
+
+    public DeleteProductService(Database database, DeleteByProductValidator validator) {
+        this.database = database;
+        this.deleteByProductValidator = validator;
     }
 
     public DeleteByProductNameResponse deleteByProductName(DeleteByProductNameRequest productNameRequest) {
-        List<CoreError> errors = validator.validate(productNameRequest);
-        if (!errors.isEmpty()){
+        List<CoreError> errors = deleteByNameValidator.validate(productNameRequest);
+        if (!errors.isEmpty()) {
             return new DeleteByProductNameResponse(errors);
         }
 
         boolean isRemoved = database.deleteByProductName(productNameRequest.getProductName());
 
         return new DeleteByProductNameResponse(isRemoved);
+    }
+
+    public DeleteByProductResponse deleteByProduct(DeleteByProductRequest request) {
+        List<CoreError> errors = deleteByProductValidator.validate(request);
+        if (!errors.isEmpty()) {
+            return new DeleteByProductResponse(errors);
+        }
+
+        Product productToDelete = new Product(request.getProductName(), request.getProductDescription(), request.getProductPrice());
+        database.delete(productToDelete);
+        return new DeleteByProductResponse(productToDelete);
     }
 
     public boolean delete(Product product) {
