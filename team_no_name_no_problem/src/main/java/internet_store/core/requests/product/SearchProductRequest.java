12c36a1a059f73f6dafddba58be9b98f91af59ab
ValerijaI <55547022+ValerijaI@users.noreply.github.com>
2020-11-22 16:50:16 +0200

    SearchProductRequest + ordering & paging

--- team_no_name_no_problem/src/main/java/internet_store/core/requests/product/SearchProductRequest.java
+++ team_no_name_no_problem/src/main/java/internet_store/core/requests/product/SearchProductRequest.java
@@ -1,13 +1,20 @@
 package internet_store.core.requests.product;
 
+import internet_store.core.requests.Ordering;
+import internet_store.core.requests.Paging;
+
 public class SearchProductRequest {
 
     private String title;
     private String description;
+    private Ordering ordering;
+    private Paging paging;
 
-    public SearchProductRequest(String title, String description){
+    public SearchProductRequest(String title, String description, Ordering ordering, Paging paging){
         this.title = title;
         this.description = description;
+        this.ordering = ordering;
+        this.paging = paging;
     }
 
     public String getTitle(){
@@ -16,5 +23,13 @@ public class SearchProductRequest {
 
     public String getDescription(){
         return description;
+    }
+
+    public Ordering getOrdering(){
+        return ordering;
+    }
+
+    public Paging getPaging(){
+        return paging;
     }
 }
