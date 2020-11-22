e13e1245131111b7c8efd89feb79ae581a5cf514
ValerijaI <55547022+ValerijaI@users.noreply.github.com>
2020-11-22 18:14:44 +0200

    SearchProductService + ordering + paging

--- team_no_name_no_problem/src/main/java/internet_store/core/services/product/SearchProductService.java
+++ team_no_name_no_problem/src/main/java/internet_store/core/services/product/SearchProductService.java
@@ -1,12 +1,17 @@
 package internet_store.core.services.product;
 
+import internet_store.core.domain.Product;
+import internet_store.core.requests.Ordering;
+import internet_store.core.requests.Paging;
 import internet_store.core.requests.product.SearchProductRequest;
 import internet_store.core.response.CoreError;
 import internet_store.core.response.product.SearchProductResponse;
 import internet_store.database.product.ProductDatabase;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class SearchProductService {
 
@@ -20,64 +25,114 @@ public class SearchProductService {
 
     public SearchProductResponse execute (SearchProductRequest searchProductRequest){
         List<CoreError> errors = searchProductRequestValidator.validate(searchProductRequest);
-
         if (!errors.isEmpty()){
             return new SearchProductResponse(errors, new ArrayList<>());
         }
+        return provideSearchResultAccordingToRequest(searchProductRequest);
+    }
 
+    private SearchProductResponse provideSearchResultAccordingToRequest(SearchProductRequest searchProductRequest){
         if (isTitleAndDescriptionNotEmpty(searchProductRequest.getTitle(), searchProductRequest.getDescription())){
-            return searchByTitleAndDescriptionIsProvided(searchProductRequest.getTitle(), searchProductRequest.getDescription());
+            return searchByTitleAndDescriptionIsProvided(searchProductRequest);
         }
-
         if (isTitleFilled(searchProductRequest.getTitle())){
-            return searchByTitleIsProvided(searchProductRequest.getTitle());
+            return searchByTitleIsProvided(searchProductRequest);
         }
-
-        return searchByDescriptionIsProvided (searchProductRequest.getDescription());
-
+        return searchByDescriptionIsProvided (searchProductRequest);
     }
 
     private boolean isTitleAndDescriptionNotEmpty(String title, String description){
         return !title.isEmpty() && !description.isEmpty();
     }
 
-    private SearchProductResponse searchByTitleAndDescriptionIsProvided(String title, String description){
+    private SearchProductResponse searchByTitleAndDescriptionIsProvided(SearchProductRequest searchProductRequest){
         List <CoreError>errors = new ArrayList<>();
-        if (productDatabase.findAllByTitleAndDescription(title, description).isEmpty()){
+        List <Product> products = productDatabase.findAllByTitleAndDescription(searchProductRequest.getTitle(), searchProductRequest.getDescription());
+        if (products.isEmpty()){
             errors.add(new CoreError("database", "Database doesn't contain product with title: " +
-                    title + " and description: " + description));
-        }
-        if (!errors.isEmpty()){
+                    searchProductRequest.getTitle() + " and description: " + searchProductRequest.getDescription()));
             return new SearchProductResponse(errors, new ArrayList<>());
         }
-        return new SearchProductResponse(productDatabase.findAllByTitleAndDescription(title, description));
+        products = order(products, searchProductRequest.getOrdering());
+        products = paging(products, searchProductRequest.getPaging());
+        return new SearchProductResponse(products);
     }
 
     private boolean isTitleFilled(String title){
         return !title.isEmpty();
     }
 
-    private SearchProductResponse searchByTitleIsProvided(String title){
+    private SearchProductResponse searchByTitleIsProvided(SearchProductRequest searchProductRequest){
         List <CoreError>errors = new ArrayList<>();
-        if (productDatabase.findAllByTitle(title).isEmpty()){
+        List<Product> products = productDatabase.findAllByTitle(searchProductRequest.getTitle());
+        if (products.isEmpty()){
             errors.add(new CoreError("database", "Database doesn't contain products wits title: " +
-                    title));
-        }
-        if (!errors.isEmpty()){
+                    searchProductRequest.getTitle()));
             return new SearchProductResponse(errors, new ArrayList<>());
         }
-        return new SearchProductResponse(productDatabase.findAllByTitle(title));
+        products = order(products, searchProductRequest.getOrdering());
+        products = paging(products, searchProductRequest.getPaging());
+        return new SearchProductResponse(products);
     }
 
-    private SearchProductResponse searchByDescriptionIsProvided(String description){
+    private SearchProductResponse searchByDescriptionIsProvided(SearchProductRequest searchProductRequest){
         List <CoreError>errors = new ArrayList<>();
-        if (productDatabase.findAllByDescription(description).isEmpty()){
+        List<Product>products = productDatabase.findAllByDescription(searchProductRequest.getDescription());
+        if (products.isEmpty()){
             errors.add(new CoreError("database", "Database doesn't contain products wits description: " +
-                    description));
-        }
-        if (!errors.isEmpty()){
+                    searchProductRequest.getDescription()));
             return new SearchProductResponse(errors, new ArrayList<>());
         }
-        return new SearchProductResponse(productDatabase.findAllByDescription(description));
+        products = order(products, searchProductRequest.getOrdering());
+        products = paging(products, searchProductRequest.getPaging());
+        return new SearchProductResponse(products);
+    }
+
+    private List<Product> order(List<Product> products, Ordering ordering) {
+        if (ordering.filledBoth()){
+            if (ordering.getOrderBy().equals("name")){
+                return sortByTitle(products, ordering);
+            }else{
+                return sortByDescription(products, ordering);
+            }
+        }else{
+            return products;
+        }
+    }
+
+    private List<Product> sortByTitle(List<Product> products, Ordering ordering){
+        if (ordering.getOrderDirection().equals("ASC")){
+            return products.stream()
+                    .sorted(Comparator.comparing(product -> product.getTitle()))
+                    .collect(Collectors.toList());
+        }else{
+            return products.stream()
+                    .sorted((o1,o2) -> o2.getTitle().compareTo(o1.getTitle()))
+                    .collect(Collectors.toList());
+        }
+    }
+
+    private List<Product> sortByDescription(List<Product> products, Ordering ordering){
+        if (ordering.getOrderDirection().equals("ASC")){
+            return products.stream()
+                    .sorted(Comparator.comparing(product -> product.getDescription()))
+                    .collect(Collectors.toList());
+        }else{
+            return products.stream()
+                    .sorted((o1,o2) -> o2.getDescription().compareTo(o1.getDescription()))
+                    .collect(Collectors.toList());
+        }
+    }
+
+    private List<Product> paging(List<Product> products, Paging paging) {
+        if (paging != null) {
+            int skip = (paging.getPageNumber() - 1) * paging.getPageSize();
+            return products.stream()
+                    .skip(skip)
+                    .limit(paging.getPageSize())
+                    .collect(Collectors.toList());
+        } else {
+            return products;
+        }
     }
 }
