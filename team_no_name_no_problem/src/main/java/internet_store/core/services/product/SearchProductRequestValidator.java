3c220a9aa4f30c7a64d19ac1ca70600a24683f32
ValerijaI <55547022+ValerijaI@users.noreply.github.com>
2020-11-22 17:37:24 +0200

    SearchProductRequestValidator + ordering

--- team_no_name_no_problem/src/main/java/internet_store/core/services/product/SearchProductRequestValidator.java
+++ team_no_name_no_problem/src/main/java/internet_store/core/services/product/SearchProductRequestValidator.java
@@ -16,10 +16,49 @@ public class SearchProductRequestValidator {
             errors.add(new CoreError("title & description", "Not valid input for search"));
         }
 
+        if (isNotValidInputForOrdering(searchProductRequest)){
+            errors.addAll(updateErrorsListForOrdering(searchProductRequest));
+        }
+
         return errors;
     }
 
     private boolean isTitleAndDescriptionEmpty(String title, String description){
         return (title == null || title.isEmpty()) && (description == null || description.isEmpty());
+    }
+
+    private boolean isNotValidInputForOrdering(SearchProductRequest searchProductRequest){
+        return (!searchProductRequest.getOrdering().filledBoth() &&
+                !searchProductRequest.getOrdering().emptyBothFields()) ||
+                (isNotValidInputForOrderBy(searchProductRequest)) ||
+                (isNotValidInputForOrderDirection(searchProductRequest));
+    }
+
+    private boolean isNotValidInputForOrderBy(SearchProductRequest searchProductRequest){
+        return !searchProductRequest.getOrdering().getOrderBy().equals("title") &&
+                !searchProductRequest.getOrdering().getOrderBy().equals("description");
+    }
+
+    private boolean isNotValidInputForOrderDirection(SearchProductRequest searchProductRequest){
+        return !searchProductRequest.getOrdering().getOrderDirection().equals("ASC") &&
+                !searchProductRequest.getOrdering().getOrderDirection().equals("DESC");
+    }
+
+    private List<CoreError> updateErrorsListForOrdering(SearchProductRequest searchProductRequest){
+
+        List<CoreError>errors = new ArrayList<>();
+
+        if (!searchProductRequest.getOrdering().filledBoth() &&
+                !searchProductRequest.getOrdering().emptyBothFields()) {
+            errors.add(new CoreError("search", "Not valid input for ordering parameters"));
+        }else {
+            if (isNotValidInputForOrderBy(searchProductRequest) && searchProductRequest.getOrdering().filledBoth()) {
+                errors.add(new CoreError("orderBy", "Not valid input for orderBy"));
+            }
+            if (isNotValidInputForOrderDirection(searchProductRequest) && searchProductRequest.getOrdering().filledBoth()) {
+                errors.add(new CoreError("orderDirection", "Not valid input for orderDirection"));
+            }
+        }
+        return  errors;
     }
 }
