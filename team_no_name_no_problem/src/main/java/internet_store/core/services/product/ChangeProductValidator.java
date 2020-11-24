ce5e9a0a3450092c18880e0096ee073a13cce945
ValerijaI <55547022+ValerijaI@users.noreply.github.com>
2020-11-24 20:54:16 +0200

    ChangeProductValidator

---
+++ team_no_name_no_problem/src/main/java/internet_store/core/services/product/ChangeProductValidator.java
@@ -0,0 +1,25 @@
+package internet_store.core.services.product;
+
+import internet_store.core.requests.product.ChangeProductRequest;
+import internet_store.core.response.CoreError;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ChangeProductValidator {
+
+    public List<CoreError> validate (ChangeProductRequest changeProductRequest){
+
+        List <CoreError> errors = new ArrayList<>();
+
+        if (changeProductRequest.getId() < 1){
+            errors.add(new CoreError("id", "Not valid input for id"));
+        }
+        if (changeProductRequest.getPrice() < 1) {
+            errors.add(new CoreError("price", "Not valid input for price"));
+        }
+
+        return errors;
+    }
+
+}
