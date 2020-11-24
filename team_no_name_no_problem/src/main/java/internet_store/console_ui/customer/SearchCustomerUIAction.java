bb6db1b4b5e24dd894de6f95005dfea2c0e94738
Jaroslav <brutan.jaroslav@gmail.com>
2020-11-24 23:03:42 +0200

    Search customer ui action lesson 4

--- team_no_name_no_problem/src/main/java/internet_store/console_ui/customer/SearchCustomerUIAction.java
+++ team_no_name_no_problem/src/main/java/internet_store/console_ui/customer/SearchCustomerUIAction.java
@@ -1,6 +1,8 @@
 package internet_store.console_ui.customer;
 
 import internet_store.console_ui.UIAction;
+import internet_store.core.requests.Ordering;
+import internet_store.core.requests.Paging;
 import internet_store.core.requests.customer.SearchCustomerRequest;
 import internet_store.core.response.customer.SearchCustomerResponse;
 import internet_store.core.services.customer.SearchCustomerService;
@@ -25,7 +27,21 @@ public class SearchCustomerUIAction implements UIAction {
         System.out.println("Please enter surname to search by surname");
         String surname = in.nextLine();
 
-        SearchCustomerRequest searchCustomerRequest = new SearchCustomerRequest(name, surname);
+        System.out.println("Please enter order by: name / surname");
+        String orderBy = in.nextLine();
+
+        System.out.println("Please enter order direction");
+        String orderDirection = in.nextLine();
+
+        System.out.println("Please enter page number");
+        Integer pageNumber = in.nextInt();
+
+        System.out.println("Please enter page size");
+        Integer pageSize = in.nextInt();
+
+        Ordering ordering = new Ordering(orderBy, orderDirection);
+        Paging paging = new Paging(pageNumber, pageSize);
+        SearchCustomerRequest searchCustomerRequest = new SearchCustomerRequest(name, surname, ordering, paging);
         SearchCustomerResponse searchCustomerResponse = searchCustomerService.execute(searchCustomerRequest);
 
         if (searchCustomerResponse.hasErrors()){
