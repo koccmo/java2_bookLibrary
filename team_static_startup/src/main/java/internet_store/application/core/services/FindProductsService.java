a89bc88447b46b1346fb0ff1156c21e7bfa0acc4
dimzulu <58876364+dimzulu@users.noreply.github.com>
2020-11-26 11:55:17 +0200

    static_startup_paging_tasks_and_tests

--- team_static_startup/src/main/java/internet_store/application/core/services/FindProductsService.java
+++ team_static_startup/src/main/java/internet_store/application/core/services/FindProductsService.java
@@ -4,11 +4,13 @@ import internet_store.application.core.database.Database;
 import internet_store.application.core.domain.Product;
 import internet_store.application.core.requests.FindProductsRequest;
 import internet_store.application.core.requests.Ordering;
+import internet_store.application.core.requests.Paging;
 import internet_store.application.core.responses.CoreError;
 import internet_store.application.core.responses.FindProductsResponse;
 import internet_store.application.core.services.validators.FindProductsRequestValidator;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class FindProductsService {
 
@@ -44,11 +46,26 @@ public class FindProductsService {
         if (request.isNameProvided() && request.isDescriptionProvided()) {
             products = database.findByNameAndDescription(request.getName(), request.getDescription());
         }
+
+        products = applyPaging(products, request.getPaging());
+
         return products;
     }
 
     private List<Product> order(List<Product> productList, Ordering ordering){
         return productList;
+    }
+
+    private List<Product> applyPaging(List<Product> products, Paging paging) {
+        if (paging != null) {
+            int numberOfProductsToSkip = paging.getPageSize() * (paging.getPageNumber() - 1);
+            return products.stream()
+                    .skip(numberOfProductsToSkip)
+                    .limit(paging.getPageSize())
+                    .collect(Collectors.toList());
+        } else {
+            return products;
+        }
     }
 
 }
