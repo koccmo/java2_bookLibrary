46ff56e499f7fcbfb07640ded7c05835ad62d969
ValerijaI <55547022+ValerijaI@users.noreply.github.com>
2020-11-28 17:24:27 +0200

    FindPatientByPersonalCodeService

--- team_healthy_tooth/src/main/java/dental_clinic/core/services/FindPatientsByPersonalCodeService.java
+++ team_healthy_tooth/src/main/java/dental_clinic/core/services/FindPatientsByPersonalCodeService.java
@@ -1,22 +1,39 @@
 package dental_clinic.core.services;
 
+import dental_clinic.core.requests.FindPatientByPersonalCodeRequest;
+import dental_clinic.core.responses.CoreError;
+import dental_clinic.core.responses.FindPatientByPersonalCodeResponse;
 import dental_clinic.database.PatientDatabase;
 import dental_clinic.core.domain.Patient;
 
 import java.util.List;
-import java.util.stream.Collectors;
+import java.util.Optional;
 
 public class FindPatientsByPersonalCodeService {
 
     private final PatientDatabase patientDatabase;
+    private final FindPatientByPersonalCodeValidator findPatientByPersonalCodeValidator;
 
-    public FindPatientsByPersonalCodeService(PatientDatabase patientDatabase) {
+    public FindPatientsByPersonalCodeService(PatientDatabase patientDatabase, FindPatientByPersonalCodeValidator findPatientByPersonalCodeValidator) {
         this.patientDatabase = patientDatabase;
+        this.findPatientByPersonalCodeValidator = findPatientByPersonalCodeValidator;
     }
 
-    public List<Patient> execute(String personalCode){
-        return patientDatabase.getPatients().stream()
-                .filter(patient -> patient.getPersonalData().getPersonalCode().equals(personalCode))
-                .collect(Collectors.toList());
+    public FindPatientByPersonalCodeResponse execute(FindPatientByPersonalCodeRequest findPatientByPersonalCodeRequest){
+        List <CoreError> errors = findPatientByPersonalCodeValidator.validate(findPatientByPersonalCodeRequest);
+
+        if (!errors.isEmpty()){
+            return new FindPatientByPersonalCodeResponse(errors);
+        }
+
+        Optional<Patient> specificPatient = patientDatabase.findPatientsByPersonalCode(findPatientByPersonalCodeRequest.getPersonalCodeToSearch());
+
+        if (specificPatient.isPresent()){
+            return new FindPatientByPersonalCodeResponse(specificPatient.get());
+        }
+
+        errors.add(new CoreError("database", "Database doesn't contain patient with personal code " +
+                findPatientByPersonalCodeRequest.getPersonalCodeToSearch()));
+        return new FindPatientByPersonalCodeResponse(errors);
     }
 }
