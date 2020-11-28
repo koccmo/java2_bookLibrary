02ad82a99256a603eb48cd2ed598991522f43e7c
Anvar331 <anvar.intezar@gmail.com>
2020-11-28 21:54:02 +0200

    internet_store/AddOrderRequest(Customer argument added),LogIn response + request

---
+++ team_no_name_no_problem/src/main/java/internet_store/core/services/customer/LogInExistingCustomerService.java
@@ -0,0 +1,27 @@
+package internet_store.core.services.customer;
+/*
+import internet_store.database.customer.CustomerDatabase;
+
+public class LogInExistingCustomerService {
+
+    private final CustomerDatabase customerDatabase;
+    private final LogInExistingCustomerValidator logInExistingCustomerValidator;
+
+    public LogInExistingCustomerService(CustomerDatabase customerDatabase,
+                                        LogInExistingCustomerValidator logInExistingCustomerValidator) {
+        this.customerDatabase = customerDatabase;
+        this.logInExistingCustomerValidator = logInExistingCustomerValidator;
+
+    }
+
+    public LoginExistingCustomerResponse execute(LogInCustomerRequest logInCustomerRequest) {
+
+        List<CoreError> errors = logInExistingCustomerValidator.validate(logInCustomerRequest);
+        if (!errors.isEmpty()) {
+            return new LoginExistingCustomerResponse(errors);
+        }
+        return new LoginExistingCustomerResponse(LogInCustomerRequest.getExistingCustomer);
+    }
+}
+*/
+
