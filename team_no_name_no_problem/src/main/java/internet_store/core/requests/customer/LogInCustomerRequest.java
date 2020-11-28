0bcdd4b72251699937d5b806ccaeb7f5fabdf7a1
Anvar331 <anvar.intezar@gmail.com>
2020-11-28 21:36:10 +0200

    internet_store/Log In Customer Request

---
+++ team_no_name_no_problem/src/main/java/internet_store/core/requests/customer/LogInCustomerRequest.java
@@ -0,0 +1,19 @@
+package internet_store.core.requests.customer;
+/*
+import internet_store.core.domain.Customer;
+
+public class LogInCustomerRequest {
+
+    private Customer customer;
+
+    public LogInCustomerRequest(Customer customer){
+        this.customer = customer;
+    }
+
+    public Customer getExistingCustomer(){
+        return customer;
+    }
+}
+
+ */
+
