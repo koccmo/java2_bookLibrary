4475c752a3fb23dac94793a1e2076a38559c6d70
ValerijaI <55547022+ValerijaI@users.noreply.github.com>
2020-11-29 15:20:12 +0200

    MainMenuApplication

--- team_no_name_no_problem/src/main/java/internet_store/MainMenuApplication.java
+++ team_no_name_no_problem/src/main/java/internet_store/MainMenuApplication.java
@@ -13,8 +13,8 @@ public class MainMenuApplication {
         InternetStoreCustomerSide internetStoreCustomerSide = new InternetStoreCustomerSide();
 
         Scanner in = new Scanner(System.in);
-        System.out.println("Welcome to the No Name No Problem Internet Store  ¯\\_(ツ)_/¯");
-        System.out.println("In order to proceed further, please select your path from the list below: ");
+        System.out.println("\nWelcome to the No Name No Problem Internet Store  ¯\\_(ツ)_/¯");
+        System.out.println("\nIn order to proceed further, please select your path from the list below: ");
         System.out.println();
 
         System.out.println("1 - Administrator path");
