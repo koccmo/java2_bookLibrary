0f430ae78b75b12685bf891c7fb5e69be75f4c59
Anvar331 <anvar.intezar@gmail.com>
2020-11-30 20:13:06 +0200

    internet_store/AddToProductService Mock Test

--- team_no_name_no_problem/src/test/java/internet_store/services/product/AddProductServiceTest.java
+++ team_no_name_no_problem/src/test/java/internet_store/services/product/AddProductServiceTest.java
@@ -1,9 +1,44 @@
 package internet_store.services.product;
 
+import internet_store.core.domain.Product;
+import internet_store.core.requests.product.AddProductRequest;
+import internet_store.core.response.CoreError;
+import internet_store.core.response.product.AddProductResponse;
+import internet_store.core.services.product.AddProductRequestValidator;
+import internet_store.core.services.product.AddProductService;
+import internet_store.database.product.ProductDatabase;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.Assert.*;
+@RunWith(MockitoJUnitRunner.class)
 
 public class AddProductServiceTest {
 
-    //There will be Mockito tests.. Right?
+    @Mock private ProductDatabase productDatabase;
+    @Mock private AddProductRequestValidator addProductRequestValidator;
+    @InjectMocks AddProductService addProductService;
 
+    @Test
+    public void noTitleAddedToProductRequestTest() {
+
+        Product laptop = new Product(null,"Samsung",400);
+        AddProductRequest request1 = new AddProductRequest(laptop);
+
+        List<CoreError> errors1 = new ArrayList<>();
+        errors1.add(new CoreError("title","Should not be empty!"));
+        Mockito.when(addProductRequestValidator.validate(request1)).thenReturn(errors1);
+
+        AddProductResponse response = addProductService.execute(request1);
+        assertEquals(response.hasErrors(),true);
+        assertEquals(response.getErrors().size(),1);
+        assertEquals(response.getErrors().get(0).getField(),"title");
+    }
 }
---
+++ team_no_name_no_problem/src/test/java/internet_store/services/product/SearchProductServiceTest.java
@@ -0,0 +1,10 @@
+package internet_store.services.product;
+
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import static org.junit.Assert.*;
+@RunWith(MockitoJUnitRunner.class)
+
+public class SearchProductServiceTest {
+}
