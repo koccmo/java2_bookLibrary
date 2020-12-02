c9d0eb8ef01cbe4f860f2ca284beff4befef36e8
Igors Mihejevs <mihejevs.igors@gmail.com>
2020-12-02 21:12:24 +0200

    DeleteByProductNameServiceTest refactoring

--- team_static_startup/src/test/java/internet_store/application/core/services/DeleteByProductNameServiceTest.java
+++ team_static_startup/src/test/java/internet_store/application/core/services/DeleteByProductNameServiceTest.java
@@ -1,27 +1,28 @@
 package internet_store.application.core.services;
 
+import internet_store.application.core.database.Database;
 import internet_store.application.core.requests.DeleteByProductNameRequest;
 import internet_store.application.core.responses.*;
 import internet_store.application.core.services.validators.DeleteByProductNameValidator;
-import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.Assert.*;
+import static org.mockito.ArgumentMatchers.any;
 
+@RunWith(MockitoJUnitRunner.class)
 public class DeleteByProductNameServiceTest {
 
-    private DeleteByProductNameValidator validator;
-    private DeleteByProductNameService service;
-
-    @Before
-    public void setup() {
-        validator = Mockito.mock(DeleteByProductNameValidator.class);
-        service = new DeleteByProductNameService(null, validator);
-    }
+    @Mock private Database database;
+    @Mock private DeleteByProductNameValidator validator;
+    @InjectMocks private DeleteByProductNameService service;
 
     @Test
     public void shouldReturnResponseWithErrorsWhenValidatorFails() {
@@ -34,6 +35,33 @@ public class DeleteByProductNameServiceTest {
         assertTrue(response.hasErrors());
         assertEquals(response.getErrors().size(), 1);
         assertEquals(response.getErrors().get(0).getField(), "Product Name");
+
+        Mockito.verify(validator).validate(request); // better use for addProduct, checking that method was called
+        Mockito.verify(validator).validate(any());
+        Mockito.verifyNoInteractions(database);
+    }
+
+    @Test
+    public void shouldReturnErrorWhenProductNameNotProvided() {
+        DeleteByProductNameRequest request = new DeleteByProductNameRequest(null);
+        List<CoreError> errors = new ArrayList<>();
+        errors.add(new CoreError("ProductName", "must not be empty"));
+        Mockito.when(validator.validate(request)).thenReturn(errors);
+        DeleteByProductNameResponse response = service.execute(request);
+        assertTrue(response.hasErrors());
+        assertEquals(response.getErrors().size(), 1);
+        assertEquals(response.getErrors().get(0).getField(), "ProductName");
+        assertEquals(response.getErrors().get(0).getMessage(), "must not be empty");
+    }
+
+    @Test
+    public void shouldDeleteProductWithProductNameFromDatabase() {
+        Mockito.when(validator.validate(any())).thenReturn(new ArrayList<>());
+        Mockito.when(database.deleteByProductName("Receiver")).thenReturn(true);
+        DeleteByProductNameRequest request = new DeleteByProductNameRequest("Receiver");
+        DeleteByProductNameResponse response = service.execute(request);
+        assertFalse(response.hasErrors());
+        assertTrue(response.isProductRemoved());
     }
 
 }
