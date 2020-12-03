dd73c6f2ff721bf511a90f571ea25922b19c66a5
ValerijaI <55547022+ValerijaI@users.noreply.github.com>
2020-12-03 17:58:10 +0200

    AcceptanceTest2 corrected

--- team_no_name_no_problem/src/test/java/internet_store/acceptance_test/AcceptanceTest2.java
+++ team_no_name_no_problem/src/test/java/internet_store/acceptance_test/AcceptanceTest2.java
@@ -2,13 +2,17 @@ package internet_store.acceptance_test;
 
 import internet_store.ApplicationContext;
 import internet_store.core.domain.Product;
+import internet_store.core.requests.product.AddProductRequest;
 import internet_store.core.requests.product.FindByIdRequest;
 import internet_store.core.requests.product.GetProductsRequest;
+import internet_store.core.response.product.FindByIdResponse;
 import internet_store.core.response.product.GetProductsResponse;
+import internet_store.core.services.product.AddProductService;
 import internet_store.core.services.product.FindProductByIdService;
 import internet_store.core.services.product.GetAllProductsService;
 import org.junit.Test;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 public class AcceptanceTest2 {
@@ -18,15 +22,23 @@ public class AcceptanceTest2 {
     @Test
     public void test(){
         Product product = new Product("Mobile phone", "Nokia", 50);
-        product.setId(5L);
+
+        AddProductRequest addProductRequest = new AddProductRequest(product);
+        addProductService().execute(addProductRequest);
+
         FindByIdRequest findByIdRequest = new FindByIdRequest(5L);
-        findProductByIdService().execute(findByIdRequest);
-        findProductByIdService().execute(findByIdRequest);
+        FindByIdResponse findByIdResponse = findProductByIdService().execute(findByIdRequest);
 
         GetProductsRequest getProductsRequest = new GetProductsRequest();
         GetProductsResponse getProductsResponse = getAllProductsService().execute(getProductsRequest);
 
-        assertTrue(getProductsResponse.getProducts().equals(product));
+        assertFalse(getProductsResponse.hasErrors());
+        assertTrue(getProductsResponse.getProducts().size() == 1);
+        assertTrue(findByIdResponse.hasErrors());
+    }
+
+    private AddProductService addProductService() {
+        return applicationContext.getBean(AddProductService.class);
     }
 
     private FindProductByIdService findProductByIdService() {
