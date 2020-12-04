6512750e2c42553444e849acbf335e085c32b736
ValerijaI <55547022+ValerijaI@users.noreply.github.com>
2020-12-04 09:27:39 +0200

    ImportCorrected

--- team_no_name_no_problem/src/main/java/internet_store/dependency_injection/DIComponent.java
+++ team_no_name_no_problem/src/main/java/internet_store/dependency_injection/DIComponent.java
@@ -1,19 +1,10 @@
 package internet_store.dependency_injection;
 
-
-import internet_store.core.services.customer.*;
-import internet_store.core.services.product.*;
-import internet_store.database.customer.CustomerDatabase;
-import internet_store.database.customer.CustomerDatabaseImpl;
-import internet_store.database.product.ProductDatabase;
-import internet_store.database.product.ProductDatabaseImpl;
-
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
-import java.util.HashMap;
-import java.util.Map;
+
 
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
