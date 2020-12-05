7c9ab1b2dd72f8bb114bc74b36aa324a518f2e44
ValerijaI <55547022+ValerijaI@users.noreply.github.com>
2020-12-05 11:07:14 +0200

    Dental clinic added main

---
+++ team_healthy_tooth/src/main/java/dental_clinic/DentalClinic.java
@@ -0,0 +1,88 @@
+package dental_clinic;
+
+import dental_clinic.console_ui.*;
+import dental_clinic.dependency_injection.ApplicationContext;
+import dental_clinic.dependency_injection.DIApplicationContextBuilder;
+
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class DentalClinic {
+
+    private static  ApplicationContext applicationContext =
+            new DIApplicationContextBuilder().build("dental_clinic");
+
+    public static void main(String [] args){
+        while(true) {
+
+            printMenu();
+
+            int userSelectedMenuNumber = inputValidInteger();
+
+            executeUIAction(userSelectedMenuNumber);
+        }
+    }
+
+    private static void printMenu(){
+        System.out.println("\nMenu\n" +
+                "1   Add patient\n" +
+                "2   Delete by id\n" +
+                "3   Print patients database\n" +
+                "4   Print specific patient information\n" +
+                "5   Search by name / surname\n" +
+                "6   Find patient by personal code\n" +
+                "7   Add visit\n" +
+                "8   Print patients card\n" +
+                "9   Change patient's personal data\n" +
+                "0   Exit");
+    }
+
+    private static int inputValidInteger(){
+        Integer input;
+        Scanner in = new Scanner(System.in);
+        while (true){
+            System.out.println("Please enter menu number: ");
+            while (true){
+                try {
+                    input = Integer.parseInt(in.nextLine());
+                    break;
+                } catch (NumberFormatException e) {
+                    System.out.println("It's not valid number! Please input valid number!");
+                }
+            }
+            if (input >= 0){
+                break;
+            }else{
+                System.out.println("Please enter valid value!");
+            }
+        }
+        return input;
+    }
+
+    private static void executeUIAction (int userSelectedMenuNumber) {
+
+        Map<Integer, UIAction> menuNumberToAction;
+
+        menuNumberToAction = new HashMap();
+        menuNumberToAction.put(1, applicationContext.getBean(AddPatientUIAction.class));
+        menuNumberToAction.put(2, applicationContext.getBean(DeletePatientUIAction.class));
+        menuNumberToAction.put(3, applicationContext.getBean(GetAllPatientsUIAction.class));
+        menuNumberToAction.put(4, applicationContext.getBean(GetSpecificPatientHistoryUIAction.class));
+        menuNumberToAction.put(5, applicationContext.getBean(SearchPatientUIAction.class));
+        menuNumberToAction.put(6, applicationContext.getBean(SearchPatientByPersonalCodeUIAction.class));
+        menuNumberToAction.put(7, applicationContext.getBean(AddVisitUIAction.class));
+        menuNumberToAction.put(8, applicationContext.getBean(GetPatientCardUIAction.class));
+        menuNumberToAction.put(9, applicationContext.getBean(ChangePersonalDataUiAction.class));
+        menuNumberToAction.put(0, applicationContext.getBean(ExitUIAction.class));
+
+        UIAction uiAction = menuNumberToAction.get(userSelectedMenuNumber);
+        if (uiAction != null) {
+            uiAction.execute();
+        } else {
+            System.out.println("Menu item does not exist: " + userSelectedMenuNumber);
+        }
+    }
+
+}
