e02532198b95e616b2178876b0543b4c0e601709
ValerijaI <55547022+ValerijaI@users.noreply.github.com>
2020-12-05 11:06:16 +0200

    Application context removing

--- team_healthy_tooth/src/main/java/dental_clinic/dependency_injection/ApplicationContext.java
+++ team_healthy_tooth/src/main/java/dental_clinic/dependency_injection/ApplicationContext.java
@@ -1,10 +1,5 @@
 package dental_clinic.dependency_injection;
 
-import dental_clinic.core.services.*;
-import dental_clinic.core.services.validators.*;
-import dental_clinic.database.PatientDatabase;
-import dental_clinic.database.PatientDatabaseImpl;
-
 import java.util.HashMap;
 import java.util.Map;
 
@@ -12,49 +7,7 @@ public class ApplicationContext {
 
     private Map<Class, Object> beans = new HashMap<>();
 
-    public ApplicationContext() {
-        beans.put(PatientDatabase.class, new PatientDatabaseImpl());
-
-        beans.put(AddPatientRequestValidator.class, new AddPatientRequestValidator());
-        beans.put(AddPatientService.class,
-                new AddPatientService(getBean(PatientDatabase.class), getBean(AddPatientRequestValidator.class)));
-
-        beans.put(DeletePatientValidator.class, new DeletePatientValidator());
-        beans.put(DeletePatientService.class,
-                new DeletePatientService(getBean(PatientDatabase.class), getBean(DeletePatientValidator.class)));
-
-        beans.put(GetAllPatientsRequestValidator.class, new GetAllPatientsRequestValidator());
-        beans.put(GetAllPatientsService.class,
-                new GetAllPatientsService(getBean(PatientDatabase.class), getBean(GetAllPatientsRequestValidator.class)));
-
-        beans.put(GetSpecificPatientHistoryRequestValidator.class, new GetSpecificPatientHistoryRequestValidator());
-        beans.put(GetSpecificPatientHistoryService.class,
-                new GetSpecificPatientHistoryService(getBean(PatientDatabase.class), getBean(GetSpecificPatientHistoryRequestValidator.class)));
-
-        beans.put(SearchPatientByPersonalCodeRequestValidator.class, new SearchPatientByPersonalCodeRequestValidator());
-        beans.put(SearchPatientsByPersonalCodeService.class,
-                new SearchPatientsByPersonalCodeService(getBean(PatientDatabase.class), getBean(SearchPatientByPersonalCodeRequestValidator.class)));
-
-        beans.put(AddVisitValidator.class, new AddVisitValidator());
-        beans.put(AddVisitService.class,
-                new AddVisitService(getBean(PatientDatabase.class), getBean(AddVisitValidator.class)));
-
-        beans.put(ContainsDatabaseIdValidator.class, new ContainsDatabaseIdValidator());
-        beans.put(ContainsDatabaseIdService.class,
-                new ContainsDatabaseIdService(getBean(PatientDatabase.class), getBean(ContainsDatabaseIdValidator.class)));
-
-        beans.put(SearchPatientRequestValidator.class, new SearchPatientRequestValidator());
-        beans.put(SearchPatientService.class,
-                new SearchPatientService(getBean(PatientDatabase.class), getBean(SearchPatientRequestValidator.class)));
-
-        beans.put(GetPatientCardRequestValidator.class, new GetPatientCardRequestValidator());
-        beans.put(GetPatientCardService.class,
-                new GetPatientCardService(getBean(PatientDatabase.class), getBean(GetPatientCardRequestValidator.class)));
-
-        beans.put(ChangePersonalDataValidator.class, new ChangePersonalDataValidator());
-        beans.put(ChangePersonalDataService.class,
-                new ChangePersonalDataService(getBean(PatientDatabase.class), getBean(ChangePersonalDataValidator.class)));
-    }
+    public ApplicationContext() {}
 
     public void addBean(Class beanClass, Object beanInstance) {
         beans.put(beanClass, beanInstance);
