84238f96576d6b198aa503a44e645b535f3b7067
ValerijaI <55547022+ValerijaI@users.noreply.github.com>
2020-12-05 11:08:03 +0200

    DIComponentCreatorJUnitTest

---
+++ team_healthy_tooth/src/test/java/dental_clinic_tests/dependency_injection/junit/DIComponentFilterJUnitTest.java
@@ -0,0 +1,36 @@
+package dental_clinic_tests.dependency_injection.junit;
+
+import dental_clinic.console_ui.AddPatientUIAction;
+import dental_clinic.console_ui.DeletePatientUIAction;
+import dental_clinic.core.requests.DeletePatientRequest;
+import dental_clinic.core.responses.DeletePatientResponse;
+import dental_clinic.dependency_injection.DIComponentFilter;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+
+public class DIComponentFilterJUnitTest {
+
+    private DIComponentFilter diComponentFilter = new DIComponentFilter();
+
+    @Test
+    public void test(){
+        List<Class> classList = new ArrayList<>();
+        classList.add(AddPatientUIAction.class);
+        classList.add(DeletePatientUIAction.class);
+        classList.add(DeletePatientRequest.class);
+        classList.add(DeletePatientResponse.class);
+
+        List<Class> expectedList = diComponentFilter.filter(classList);
+
+        assertTrue(expectedList.size() == 2);
+        assertTrue(expectedList.contains(AddPatientUIAction.class));
+        assertFalse(expectedList.contains(DeletePatientRequest.class));
+    }
+
+}
