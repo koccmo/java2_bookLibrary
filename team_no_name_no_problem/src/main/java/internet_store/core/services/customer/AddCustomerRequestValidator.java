3b5a0e8d951206a959279200a33f7da371c9615a
Jaroslav <brutan.jaroslav@gmail.com>
2020-12-06 22:43:59 +0200

    add customer request validator update

--- team_no_name_no_problem/src/main/java/internet_store/core/services/customer/AddCustomerRequestValidator.java
+++ team_no_name_no_problem/src/main/java/internet_store/core/services/customer/AddCustomerRequestValidator.java
@@ -1,11 +1,12 @@
 package internet_store.core.services.customer;
 
-import internet_store.core.domain.Customer;
 import internet_store.core.requests.customer.AddCustomerRequest;
 import internet_store.core.response.CoreError;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class AddCustomerRequestValidator {
 
@@ -16,13 +17,22 @@ public class AddCustomerRequestValidator {
                 .isEmpty()) {
             errors.add(new CoreError("name", "Not valid input for name"));
         }
-        if (addCustomerRequest.getCustomer().getName() == null || addCustomerRequest.getCustomer().getSurname()
+        if (!containsOnlyLetters(addCustomerRequest.getCustomer().getName())){
+            errors.add(new CoreError("name","Not valid input for name, should contain only letters"));
+        }
+        if (addCustomerRequest.getCustomer().getSurname() == null || addCustomerRequest.getCustomer().getSurname()
                 .isEmpty()) {
             errors.add(new CoreError("surname", "Not valid input for surname"));
+        }
+        if (!containsOnlyLetters(addCustomerRequest.getCustomer().getSurname())){
+            errors.add(new CoreError("surname", "Not valid input for surname, should contain only letters"));
         }
         if (addCustomerRequest.getCustomer().getPhoneNumber() == null || addCustomerRequest.getCustomer().getPhoneNumber()
                 .isEmpty()) {
             errors.add(new CoreError("phone number", "Not valid input for phone number"));
+        }
+        if (!numberContainsOnlyDigits(addCustomerRequest.getCustomer().getPhoneNumber())){
+            errors.add(new CoreError("phone number", "Not valid input for phone number, should contain only digits"));
         }
         if (addCustomerRequest.getCustomer().getAddress() == null || addCustomerRequest.getCustomer().getAddress()
                 .isEmpty()) {
@@ -33,6 +43,28 @@ public class AddCustomerRequestValidator {
             errors.add(new CoreError("e-mail", "Not valid input for e-mail"));
         }
         return errors;
+    }
+
+    private boolean numberContainsOnlyDigits(String input){
+        String regex = "[0-9]+";
+
+        Pattern pattern = Pattern.compile(regex);
+        if (input == null){
+            return false;
+        }
+        Matcher matcher = pattern.matcher(input);
+        return matcher.matches();
+    }
+
+    private boolean containsOnlyLetters(String input){
+        String regex = "[a-zA-Z]+";
+
+        Pattern pattern = Pattern.compile(regex);
+        if (input == null){
+            return false;
+        }
+        Matcher matcher = pattern.matcher(input);
+        return matcher.matches();
     }
 
 }
