b9855d12e37211ffb0cd1f7c41628bdf4800a6c6
Jaroslav <brutan.jaroslav@gmail.com>
2020-12-06 22:43:29 +0200

    Acceptance test search customer name update

--- team_no_name_no_problem/src/test/java/internet_store_tests/acceptance_test/customer/AcceptanceTestSearchCustomerByName.java
+++ team_no_name_no_problem/src/test/java/internet_store_tests/acceptance_test/customer/AcceptanceTestSearchCustomerByName.java
@@ -22,9 +22,9 @@ public class AcceptanceTestSearchCustomerByName {
 
     @Test
     public void test(){
-        Customer customer = new Customer("name", "surname", "number", "address",
+        Customer customer = new Customer("name", "surname", "82938172", "address",
                 "email");
-        Customer customer1 = new Customer("name1", "surname20","phone", "Matisa",
+        Customer customer1 = new Customer("name", "surname","29876472", "Matisa",
                 "tr3vis@Inbox.lv");
         Customer customer2 = new Customer("Valerija", "Lobanova","2781263",
                 "Ukraina", "privetpoka@tikto.lv");
