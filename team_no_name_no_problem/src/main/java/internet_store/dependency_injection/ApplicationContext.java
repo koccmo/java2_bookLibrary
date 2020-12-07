ca4b5427619228f94cbc21d330853a022df29775
Jaroslav <brutan.jaroslav@gmail.com>
2020-12-07 22:51:19 +0200

    update

--- team_no_name_no_problem/src/main/java/internet_store/dependency_injection/ApplicationContext.java
+++ team_no_name_no_problem/src/main/java/internet_store/dependency_injection/ApplicationContext.java
@@ -1,6 +1,7 @@
 package internet_store.dependency_injection;
 
 import internet_store.core.services.customer.*;
+import internet_store.core.services.customer.validators.*;
 import internet_store.core.services.product.*;
 import internet_store.core.services.product.validators.*;
 import internet_store.database.customer.CustomerDatabase;
@@ -68,7 +69,23 @@ public class ApplicationContext {
 
     }
 
+    public void addBean(Class beanClass, Object beanInstance) {
+        beans.put(beanClass, beanInstance);
+        Class[] instanceInterfaces = beanClass.getInterfaces();
+        for (int i = 0; i < instanceInterfaces.length; i++) {
+            Class instanceInterface = instanceInterfaces[i];
+            Object instance = getBean(instanceInterface);
+            if (instance == null) {
+                beans.put(instanceInterface, beanInstance);
+            }
+        }
+    }
+
     public <T extends Object> T getBean(Class c) {
         return (T) beans.get(c);
+    }
+
+    public Map<Class, Object> getBeans() {
+        return beans;
     }
 }
