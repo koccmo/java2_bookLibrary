20ad1a06b367557d2862dbd9c18814a4009d0924
Jaroslav <brutan.jaroslav@gmail.com>
2020-12-07 22:53:51 +0200

    update

--- team_no_name_no_problem/src/test/java/internet_store_tests/core/services/validators_tests/customer/FindCustomerByIdRequestValidatorTest.java
+++ team_no_name_no_problem/src/test/java/internet_store_tests/core/services/validators_tests/customer/FindCustomerByIdRequestValidatorTest.java
@@ -2,7 +2,7 @@ package internet_store_tests.core.services.validators_tests.customer;
 
 import internet_store.core.requests.customer.FindCustomerByIdRequest;
 import internet_store.core.response.CoreError;
-import internet_store.core.services.customer.FindCustomerByIdRequestValidator;
+import internet_store.core.services.customer.validators.FindCustomerByIdRequestValidator;
 import org.junit.Test;
 
 import java.util.List;
