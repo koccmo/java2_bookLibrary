b20f19e821f2af43d56f6c670597c0195e9e1097
ValerijaI <55547022+ValerijaI@users.noreply.github.com>
2020-12-09 18:56:58 +0200

    Order update & SC delete

--- team_no_name_no_problem/src/main/java/internet_store/core/domain/Order.java
+++ team_no_name_no_problem/src/main/java/internet_store/core/domain/Order.java
@@ -1,14 +1,17 @@
 package internet_store.core.domain;
 
+import java.util.Map;
+import java.util.Objects;
+
 public class Order {
 
     private Long id;
 
     private Customer customer;
 
-    private ShoppingCart shoppingCart;
+    private Map<Product, Integer> shoppingCart;
 
-    public Order (Customer customer, ShoppingCart shoppingCart){
+    public Order (Customer customer, Map <Product, Integer> shoppingCart){
         this.customer = customer;
         this.shoppingCart = shoppingCart;
     }
@@ -25,9 +28,28 @@ public class Order {
         return customer;
     }
 
-    public ShoppingCart getShoppingCart() {
+    public Map<Product, Integer> getShoppingCart() {
         return shoppingCart;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Order order = (Order) o;
+        return Objects.equals(id, order.id) && Objects.equals(customer, order.customer) && Objects.equals(shoppingCart, order.shoppingCart);
+    }
 
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, customer, shoppingCart);
+    }
+
+    @Override
+    public String toString() {
+        return  "Order: {" +
+                "\ncustomer=" + customer +
+                "\nshoppingCart=" + shoppingCart +
+                "}";
+    }
 }
