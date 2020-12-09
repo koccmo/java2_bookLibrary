f8105e40cb5e640c90577900eb02b9f279c86bd2
ValerijaI <55547022+ValerijaI@users.noreply.github.com>
2020-12-09 18:55:49 +0200

    OrderUI update

---
+++ team_no_name_no_problem/src/main/java/internet_store/console_ui/order/GetOrdersUIAction.java
@@ -0,0 +1,28 @@
+package internet_store.console_ui.order;
+
+import internet_store.console_ui.UIAction;
+import internet_store.core.requests.order.GetOrdersRequest;
+import internet_store.core.response.order.GetOrdersResponse;
+import internet_store.core.services.order.GetOrdersService;
+import internet_store.dependency_injection.DIComponent;
+import internet_store.dependency_injection.DIDependency;
+
+@DIComponent
+public class GetOrdersUIAction implements UIAction {
+
+    @DIDependency private GetOrdersService getOrdersService;
+
+    @Override
+    public void execute () {
+        System.out.println("Orders: ");
+
+        GetOrdersRequest getOrdersRequest = new GetOrdersRequest();
+        GetOrdersResponse getOrdersResponse = getOrdersService.execute(getOrdersRequest);
+
+        if (getOrdersResponse.hasErrors()) {
+            getOrdersResponse.getErrors().forEach(System.out::println);
+        }else {
+            getOrdersResponse.getOrders().forEach(System.out::println);
+        }
+    }
+}
