2169837f48dfca78929fe920d2acc6e4e3b3d7fb
ValerijaI <55547022+ValerijaI@users.noreply.github.com>
2020-12-09 18:59:37 +0200

    GetOrderResponse created

---
+++ team_no_name_no_problem/src/main/java/internet_store/core/response/order/GetOrdersResponse.java
@@ -0,0 +1,25 @@
+package internet_store.core.response.order;
+
+import internet_store.core.domain.Order;
+import internet_store.core.response.CoreError;
+import internet_store.core.response.CoreResponse;
+
+import java.util.List;
+
+public class GetOrdersResponse extends CoreResponse {
+
+    private List<Order> orders;
+
+    public GetOrdersResponse(List<Order> orders) {
+        this.orders = orders;
+    }
+
+    public GetOrdersResponse(List<CoreError> errors, List<Order> orders) {
+        super(errors);
+        this.orders = orders;
+    }
+
+    public List<Order> getOrders() {
+        return orders;
+    }
+}
