c4719e8a410a554025b9786162e8f8d55fb75bb3
ValerijaI <55547022+ValerijaI@users.noreply.github.com>
2020-12-09 22:50:58 +0200

    BuyProductUIAction refactoring

--- team_no_name_no_problem/src/main/java/internet_store/console_ui/product/BuyProductUIAction.java
+++ team_no_name_no_problem/src/main/java/internet_store/console_ui/product/BuyProductUIAction.java
@@ -28,44 +28,40 @@ public class BuyProductUIAction implements UIAction {
     @Override
     public void execute() {
 
-                Scanner in = new Scanner(System.in);
+        Scanner in = new Scanner(System.in);
 
-                Map<Product, Integer> shoppingCart = new HashMap<>();
+        Map<Product, Integer> shoppingCart = new HashMap<>();
 
-                while (true) {
-                    System.out.println("To add product in Shopping Cart, please enter ID of this product: ");
-                    Long id = in.nextLong();
+        while (true) {
+            System.out.println("To add product in Shopping Cart, please enter ID of this product: ");
+            Long id = in.nextLong();
 
-                    System.out.println("Please enter quantity of this product");
-                    Integer quantity = in.nextInt();
-                    String endOfShopping = in.nextLine();
+            System.out.println("Please enter quantity of this product");
+            Integer quantity = in.nextInt();
+            String endOfShopping = in.nextLine();
 
-                    System.out.println("Please enter CHECKOUT to make order");
-                    endOfShopping = in.nextLine();
+            System.out.println("Please enter CHECKOUT to make order");
+            endOfShopping = in.nextLine();
 
-                    BuyProductRequest buyProductRequest = new BuyProductRequest(id, quantity, endOfShopping);
-                    BuyProductResponse buyProductResponse = buyProductService.execute(buyProductRequest);
+            BuyProductRequest buyProductRequest = new BuyProductRequest(id, quantity, endOfShopping);
+            BuyProductResponse buyProductResponse = buyProductService.execute(buyProductRequest);
 
-                    if (buyProductResponse.hasErrors()) {
-                        buyProductResponse.getErrors().forEach(System.out::println);
-                    }else {
-                        Product product = buyProductResponse.getProduct();
-                        shoppingCart.put(product, buyProductRequest.getQuantity());
-                        }
+            if (buyProductResponse.hasErrors()) {
+                buyProductResponse.getErrors().forEach(System.out::println);
+            }else {
+                Product product = buyProductResponse.getProduct();
+                shoppingCart.put(product, buyProductRequest.getQuantity());
+            }
 
-                    if (endOfShopping.equalsIgnoreCase("CHECKOUT")) {
-                        Customer newCustomer = getNewCustomer();
-                        Order order = new Order(newCustomer, shoppingCart);
-                        orderDatabase.addOrder(order);
-                        System.out.println("Thank you for the order!\n" +
-                                "Order information:");
-                        System.out.println(order);
-
-                        break;
-                    }
-                }
-
-
+            if (endOfShopping.equalsIgnoreCase("CHECKOUT")) {
+                Customer newCustomer = getNewCustomer();
+                Order order = new Order(newCustomer, shoppingCart);
+                orderDatabase.addOrder(order);
+                System.out.println("Thank you for the order!\n");
+                System.out.println(order.toString());
+                break;
+            }
+        }
     }
 
     private Customer getNewCustomer () {
@@ -102,4 +98,5 @@ public class BuyProductUIAction implements UIAction {
         }
         return newCustomer;
     }
+
 }
