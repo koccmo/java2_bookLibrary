090d97f07aef3db4535acaf397fbe586c7415caf
vsavonins <viktors.savonins@proofit.lv>
2020-12-11 12:13:48 +0200

    Lesson 7 tasks

--- teacher/lesson_7_spring_basics/step_1_spring/src/main/java/lv/javaguru/java2/library/config/EmailServerConfiguration.java
+++ teacher/lesson_7_spring_basics/step_1_spring/src/main/java/lv/javaguru/java2/library/config/EmailServerConfiguration.java
@@ -1,6 +1,5 @@
 package lv.javaguru.java2.library.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
