b17c892c28fcae3ed142e9264a4d71e7d25b726d
Anvar331 <anvar.intezar@gmail.com>
2020-12-12 19:37:24 +0200

    Annotation Change for Product validators

--- team_no_name_no_problem/src/main/java/internet_store/core/services/product/validators/GetAllProductsValidator.java
+++ team_no_name_no_problem/src/main/java/internet_store/core/services/product/validators/GetAllProductsValidator.java
@@ -2,12 +2,12 @@ package internet_store.core.services.product.validators;
 
 import internet_store.core.requests.product.GetProductsRequest;
 import internet_store.core.response.CoreError;
-import internet_store.dependency_injection.DIComponent;
+import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
 import java.util.List;
 
-@DIComponent
+@Component
 public class GetAllProductsValidator {
 
     public List<CoreError> validate (GetProductsRequest getProductsRequest){
