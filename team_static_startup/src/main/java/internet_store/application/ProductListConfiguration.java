65e5137dd7cdcd5c19fc4bdb5541c922c421dd9d
ArturM <kpackalv@yahoo.com>
2020-12-12 16:57:01 +0200

    StaticStartup L7 ProductListConfiguration Creation

---
+++ team_static_startup/src/main/java/internet_store/application/ProductListConfiguration.java
@@ -0,0 +1,9 @@
+package internet_store.application;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "internet_store.application")
+public class ProductListConfiguration {
+}
