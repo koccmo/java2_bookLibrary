d93a2b38b72d657acfb10dbea246e44535c06b68
ArturM <kpackalv@yahoo.com>
2020-12-12 17:31:11 +0200

    StaticStartup L7 Spring in Acceptance Tests

--- team_static_startup/src/test/java/internet_store/application/acceptancetests/DeleteByProductNameAcceptanceTest.java
+++ team_static_startup/src/test/java/internet_store/application/acceptancetests/DeleteByProductNameAcceptanceTest.java
@@ -1,62 +1,63 @@
-//package internet_store.application.acceptancetests;
-//
-//import internet_store.application.core.requests.AddProductRequest;
-//import internet_store.application.core.requests.DeleteByProductNameRequest;
-//import internet_store.application.core.responses.DeleteByProductNameResponse;
-//import internet_store.application.core.services.AddProductService;
-//import internet_store.application.core.services.DeleteByProductNameService;
-//import internet_store.application.dependency_injection.ApplicationContext;
-//import internet_store.application.dependency_injection.DIApplicationContextBuilder;
-//import org.junit.Test;
-//
-//import java.math.BigDecimal;
-//
-//import static org.junit.Assert.*;
-//
-//public class DeleteByProductNameAcceptanceTest {
-//
-//    private final ApplicationContext appContext =
-//            new DIApplicationContextBuilder().build("internet_store.application");
-//
-//    @Test
-//    public void shouldDeleteProductCorrectly() {
-//        AddProductRequest request = new AddProductRequest(
-//                "TV", "SONY", new BigDecimal("1000"));
-//        getAddProductService().execute(request);
-//        DeleteByProductNameRequest requestToDelete = new DeleteByProductNameRequest("TV");
-//        DeleteByProductNameResponse response = getDeleteByProductNameService().execute(requestToDelete);
-//        assertTrue(response.isProductRemoved());
-//        assertFalse(response.hasErrors());
-//    }
-//
-//    @Test
-//    public void shouldNotDeleteProductIfNameNotCorrect() {
-//        AddProductRequest request = new AddProductRequest(
-//                "TV", "SONY", new BigDecimal("1000"));
-//        getAddProductService().execute(request);
-//        DeleteByProductNameRequest requestToDelete = new DeleteByProductNameRequest("qwe");
-//        DeleteByProductNameResponse response = getDeleteByProductNameService().execute(requestToDelete);
-//        assertFalse(response.isProductRemoved());
-//    }
-//
-//    @Test
-//    public void shouldNotDeleteProductIfNameNotEntered() {
-//        AddProductRequest request = new AddProductRequest(
-//                "TV", "SONY", new BigDecimal("1000"));
-//        getAddProductService().execute(request);
-//        DeleteByProductNameRequest requestToDelete = new DeleteByProductNameRequest("");
-//        DeleteByProductNameResponse response = getDeleteByProductNameService().execute(requestToDelete);
-//        assertFalse(response.isProductRemoved());
-//        assertTrue(response.hasErrors());
-//        assertEquals("Product name", response.getErrors().get(0).getField());
-//        assertEquals("must not be empty", response.getErrors().get(0).getMessage());
-//    }
-//
-//        private AddProductService getAddProductService () {
-//            return appContext.getBean(AddProductService.class);
-//        }
-//
-//        private DeleteByProductNameService getDeleteByProductNameService () {
-//            return appContext.getBean(DeleteByProductNameService.class);
-//        }
-//    }
+package internet_store.application.acceptancetests;
+
+import internet_store.application.ProductListConfiguration;
+import internet_store.application.core.requests.AddProductRequest;
+import internet_store.application.core.requests.DeleteByProductNameRequest;
+import internet_store.application.core.responses.DeleteByProductNameResponse;
+import internet_store.application.core.services.AddProductService;
+import internet_store.application.core.services.DeleteByProductNameService;
+import org.springframework.context.ApplicationContext;
+import org.junit.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.math.BigDecimal;
+
+import static org.junit.Assert.*;
+
+public class DeleteByProductNameAcceptanceTest {
+
+    private final ApplicationContext appContext =
+            new AnnotationConfigApplicationContext(ProductListConfiguration.class);
+
+    @Test
+    public void shouldDeleteProductCorrectly() {
+        AddProductRequest request = new AddProductRequest(
+                "TV", "SONY", new BigDecimal("1000"));
+        getAddProductService().execute(request);
+        DeleteByProductNameRequest requestToDelete = new DeleteByProductNameRequest("TV");
+        DeleteByProductNameResponse response = getDeleteByProductNameService().execute(requestToDelete);
+        assertTrue(response.isProductRemoved());
+        assertFalse(response.hasErrors());
+    }
+
+    @Test
+    public void shouldNotDeleteProductIfNameNotCorrect() {
+        AddProductRequest request = new AddProductRequest(
+                "TV", "SONY", new BigDecimal("1000"));
+        getAddProductService().execute(request);
+        DeleteByProductNameRequest requestToDelete = new DeleteByProductNameRequest("qwe");
+        DeleteByProductNameResponse response = getDeleteByProductNameService().execute(requestToDelete);
+        assertFalse(response.isProductRemoved());
+    }
+
+    @Test
+    public void shouldNotDeleteProductIfNameNotEntered() {
+        AddProductRequest request = new AddProductRequest(
+                "TV", "SONY", new BigDecimal("1000"));
+        getAddProductService().execute(request);
+        DeleteByProductNameRequest requestToDelete = new DeleteByProductNameRequest("");
+        DeleteByProductNameResponse response = getDeleteByProductNameService().execute(requestToDelete);
+        assertFalse(response.isProductRemoved());
+        assertTrue(response.hasErrors());
+        assertEquals("Product name", response.getErrors().get(0).getField());
+        assertEquals("must not be empty", response.getErrors().get(0).getMessage());
+    }
+
+        private AddProductService getAddProductService () {
+            return appContext.getBean(AddProductService.class);
+        }
+
+        private DeleteByProductNameService getDeleteByProductNameService () {
+            return appContext.getBean(DeleteByProductNameService.class);
+        }
+    }
