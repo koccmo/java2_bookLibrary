b616255f34e6553af9b052cfdd404f37f50c6000
Anvar331 <anvar.intezar@gmail.com>
2020-12-12 19:48:40 +0200

    Annotation Change for ProductDatabaseImpl

--- team_no_name_no_problem/src/main/java/internet_store/database/product/ProductDatabaseImpl.java
+++ team_no_name_no_problem/src/main/java/internet_store/database/product/ProductDatabaseImpl.java
@@ -1,14 +1,14 @@
 package internet_store.database.product;
 
 import internet_store.core.domain.Product;
-import internet_store.dependency_injection.DIComponent;
+import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
-@DIComponent
+@Component
 public class ProductDatabaseImpl implements ProductDatabase{
 
     private Long id= 1L;
