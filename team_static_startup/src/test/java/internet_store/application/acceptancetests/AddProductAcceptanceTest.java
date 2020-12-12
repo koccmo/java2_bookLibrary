df7b312d775d86830329ffdbf84b0e5a0c8b8de3
ArturM <kpackalv@yahoo.com>
2020-12-12 16:47:29 +0200

    StaticStartup L7 Acceptance Tests Comment

--- team_static_startup/src/test/java/internet_store/application/acceptancetests/AddProductAcceptanceTest.java
+++ team_static_startup/src/test/java/internet_store/application/acceptancetests/AddProductAcceptanceTest.java
@@ -1,83 +1,83 @@
-package internet_store.application.acceptancetests;
-
-import internet_store.application.dependency_injection.ApplicationContext;
-import internet_store.application.core.requests.AddProductRequest;
-import internet_store.application.core.requests.GetAllProductsRequest;
-import internet_store.application.core.responses.GetAllProductsResponse;
-import internet_store.application.core.services.AddProductService;
-import internet_store.application.core.services.GetAllProductsService;
-import internet_store.application.dependency_injection.DIApplicationContextBuilder;
-import org.junit.Before;
-import org.junit.Test;
-
-import java.math.BigDecimal;
-
-import static org.junit.Assert.assertEquals;
-
-public class AddProductAcceptanceTest {
-
-    private ApplicationContext applicationContext;
-
-    @Before
-    public void setUp(){
-        applicationContext =
-                new DIApplicationContextBuilder().build("internet_store.application");
-    }
-
-    @Test
-    public void shouldReturnNoErrorsWhenAddingBooks() {
-        AddProductRequest addProductRequest1 = new AddProductRequest("tv1",
-                "good tv1", new BigDecimal("399.99"));
-        getAddProductService().execute(addProductRequest1);
-
-        AddProductRequest addProductRequest2 = new AddProductRequest("tv2",
-                "good tv2", new BigDecimal("399.99"));
-        getAddProductService().execute(addProductRequest2);
-
-        GetAllProductsRequest request = new GetAllProductsRequest();
-        GetAllProductsResponse response = getAllProductsService().execute(request);
-        assertEquals(2, response.getProductList().size());
-    }
-
-    @Test
-    public void shouldNotAddBookWithWrongParameterName() {
-        AddProductRequest addProductRequest = new AddProductRequest(" ",
-                "good tv", new BigDecimal("399.99"));
-        getAddProductService().execute(addProductRequest);
-
-        GetAllProductsRequest request = new GetAllProductsRequest();
-        GetAllProductsResponse response = getAllProductsService().execute(request);
-        assertEquals(0, response.getProductList().size());
-    }
-
-    @Test
-    public void shouldNotAddBookWithWrongParameterDescription() {
-        AddProductRequest addProductRequest = new AddProductRequest("tv",
-                null, new BigDecimal("399.99"));
-        getAddProductService().execute(addProductRequest);
-
-        GetAllProductsRequest request = new GetAllProductsRequest();
-        GetAllProductsResponse response = getAllProductsService().execute(request);
-        assertEquals(0, response.getProductList().size());
-    }
-
-    @Test
-    public void shouldNotAddBookWithWrongParameterPrice() {
-        AddProductRequest addProductRequest = new AddProductRequest("tv",
-                "good tv", null);
-        getAddProductService().execute(addProductRequest);
-
-        GetAllProductsRequest request = new GetAllProductsRequest();
-        GetAllProductsResponse response = getAllProductsService().execute(request);
-        assertEquals(0, response.getProductList().size());
-    }
-
-    private AddProductService getAddProductService() {
-        return applicationContext.getBean(AddProductService.class);
-    }
-
-    private GetAllProductsService getAllProductsService() {
-        return applicationContext.getBean(GetAllProductsService.class);
-    }
-
-}
+//package internet_store.application.acceptancetests;
+//
+//import internet_store.application.dependency_injection.ApplicationContext;
+//import internet_store.application.core.requests.AddProductRequest;
+//import internet_store.application.core.requests.GetAllProductsRequest;
+//import internet_store.application.core.responses.GetAllProductsResponse;
+//import internet_store.application.core.services.AddProductService;
+//import internet_store.application.core.services.GetAllProductsService;
+//import internet_store.application.dependency_injection.DIApplicationContextBuilder;
+//import org.junit.Before;
+//import org.junit.Test;
+//
+//import java.math.BigDecimal;
+//
+//import static org.junit.Assert.assertEquals;
+//
+//public class AddProductAcceptanceTest {
+//
+//    private ApplicationContext applicationContext;
+//
+//    @Before
+//    public void setUp(){
+//        applicationContext =
+//                new DIApplicationContextBuilder().build("internet_store.application");
+//    }
+//
+//    @Test
+//    public void shouldReturnNoErrorsWhenAddingBooks() {
+//        AddProductRequest addProductRequest1 = new AddProductRequest("tv1",
+//                "good tv1", new BigDecimal("399.99"));
+//        getAddProductService().execute(addProductRequest1);
+//
+//        AddProductRequest addProductRequest2 = new AddProductRequest("tv2",
+//                "good tv2", new BigDecimal("399.99"));
+//        getAddProductService().execute(addProductRequest2);
+//
+//        GetAllProductsRequest request = new GetAllProductsRequest();
+//        GetAllProductsResponse response = getAllProductsService().execute(request);
+//        assertEquals(2, response.getProductList().size());
+//    }
+//
+//    @Test
+//    public void shouldNotAddBookWithWrongParameterName() {
+//        AddProductRequest addProductRequest = new AddProductRequest(" ",
+//                "good tv", new BigDecimal("399.99"));
+//        getAddProductService().execute(addProductRequest);
+//
+//        GetAllProductsRequest request = new GetAllProductsRequest();
+//        GetAllProductsResponse response = getAllProductsService().execute(request);
+//        assertEquals(0, response.getProductList().size());
+//    }
+//
+//    @Test
+//    public void shouldNotAddBookWithWrongParameterDescription() {
+//        AddProductRequest addProductRequest = new AddProductRequest("tv",
+//                null, new BigDecimal("399.99"));
+//        getAddProductService().execute(addProductRequest);
+//
+//        GetAllProductsRequest request = new GetAllProductsRequest();
+//        GetAllProductsResponse response = getAllProductsService().execute(request);
+//        assertEquals(0, response.getProductList().size());
+//    }
+//
+//    @Test
+//    public void shouldNotAddBookWithWrongParameterPrice() {
+//        AddProductRequest addProductRequest = new AddProductRequest("tv",
+//                "good tv", null);
+//        getAddProductService().execute(addProductRequest);
+//
+//        GetAllProductsRequest request = new GetAllProductsRequest();
+//        GetAllProductsResponse response = getAllProductsService().execute(request);
+//        assertEquals(0, response.getProductList().size());
+//    }
+//
+//    private AddProductService getAddProductService() {
+//        return applicationContext.getBean(AddProductService.class);
+//    }
+//
+//    private GetAllProductsService getAllProductsService() {
+//        return applicationContext.getBean(GetAllProductsService.class);
+//    }
+//
+//}
--- team_static_startup/src/test/java/internet_store/application/acceptancetests/ChangeProductNameAcceptanceTest.java
+++ team_static_startup/src/test/java/internet_store/application/acceptancetests/ChangeProductNameAcceptanceTest.java
@@ -1,68 +1,68 @@
-package internet_store.application.acceptancetests;
-
-import internet_store.application.core.database.Database;
-import internet_store.application.core.domain.Product;
-import internet_store.application.core.requests.ChangeProductNameRequest;
-import internet_store.application.core.responses.ChangeProductNameResponse;
-import internet_store.application.core.services.ChangeProductNameService;
-import internet_store.application.dependency_injection.ApplicationContext;
-import internet_store.application.dependency_injection.DIApplicationContextBuilder;
-import org.junit.Before;
-import org.junit.Test;
-
-import java.math.BigDecimal;
-
-import static org.junit.Assert.*;
-
-public class ChangeProductNameAcceptanceTest {
-
-    private ApplicationContext applicationContext;
-    private Database database;
-
-    @Before
-    public void setUp() {
-        applicationContext =
-                new DIApplicationContextBuilder().build("internet_store.application");
-        database = getDatabase();
-        database.add(new Product("iPhone", "phone", new BigDecimal("900")));
-        database.add(new Product("iMac", "pc", new BigDecimal("4000")));
-    }
-
-    @Test
-    public void shouldChangeProductName() {
-        ChangeProductNameRequest request = new ChangeProductNameRequest(1L, "iPhone12");
-        ChangeProductNameResponse response = getChangeProductNameService().execute(request);
-
-        assertTrue(response.isNameChanged());
-        assertEquals("iPhone12", database.getProductList().get(0).getName());
-        assertNull(response.getErrors());
-    }
-
-    @Test
-    public void shouldNotChangeNameWhenProductNotFound() {
-        ChangeProductNameRequest request = new ChangeProductNameRequest(3L, "iPhone12");
-        ChangeProductNameResponse response = getChangeProductNameService().execute(request);
-
-        assertFalse(response.isNameChanged());
-        assertNull(response.getErrors());
-    }
-
-    @Test
-    public void shouldReturnErrorWhenNewNameIsEmpty() {
-        ChangeProductNameRequest request = new ChangeProductNameRequest(1L, "");
-        ChangeProductNameResponse response = getChangeProductNameService().execute(request);
-
-        assertFalse(response.isNameChanged());
-        assertEquals(1, response.getErrors().size());
-        assertEquals("Product new name", response.getErrors().get(0).getField());
-        assertEquals("Should not be empty.", response.getErrors().get(0).getMessage());
-    }
-
-    private Database getDatabase() {
-        return applicationContext.getBean(Database.class);
-    }
-
-    private ChangeProductNameService getChangeProductNameService() {
-        return applicationContext.getBean(ChangeProductNameService.class);
-    }
-}
+//package internet_store.application.acceptancetests;
+//
+//import internet_store.application.core.database.Database;
+//import internet_store.application.core.domain.Product;
+//import internet_store.application.core.requests.ChangeProductNameRequest;
+//import internet_store.application.core.responses.ChangeProductNameResponse;
+//import internet_store.application.core.services.ChangeProductNameService;
+//import internet_store.application.dependency_injection.ApplicationContext;
+//import internet_store.application.dependency_injection.DIApplicationContextBuilder;
+//import org.junit.Before;
+//import org.junit.Test;
+//
+//import java.math.BigDecimal;
+//
+//import static org.junit.Assert.*;
+//
+//public class ChangeProductNameAcceptanceTest {
+//
+//    private ApplicationContext applicationContext;
+//    private Database database;
+//
+//    @Before
+//    public void setUp() {
+//        applicationContext =
+//                new DIApplicationContextBuilder().build("internet_store.application");
+//        database = getDatabase();
+//        database.add(new Product("iPhone", "phone", new BigDecimal("900")));
+//        database.add(new Product("iMac", "pc", new BigDecimal("4000")));
+//    }
+//
+//    @Test
+//    public void shouldChangeProductName() {
+//        ChangeProductNameRequest request = new ChangeProductNameRequest(1L, "iPhone12");
+//        ChangeProductNameResponse response = getChangeProductNameService().execute(request);
+//
+//        assertTrue(response.isNameChanged());
+//        assertEquals("iPhone12", database.getProductList().get(0).getName());
+//        assertNull(response.getErrors());
+//    }
+//
+//    @Test
+//    public void shouldNotChangeNameWhenProductNotFound() {
+//        ChangeProductNameRequest request = new ChangeProductNameRequest(3L, "iPhone12");
+//        ChangeProductNameResponse response = getChangeProductNameService().execute(request);
+//
+//        assertFalse(response.isNameChanged());
+//        assertNull(response.getErrors());
+//    }
+//
+//    @Test
+//    public void shouldReturnErrorWhenNewNameIsEmpty() {
+//        ChangeProductNameRequest request = new ChangeProductNameRequest(1L, "");
+//        ChangeProductNameResponse response = getChangeProductNameService().execute(request);
+//
+//        assertFalse(response.isNameChanged());
+//        assertEquals(1, response.getErrors().size());
+//        assertEquals("Product new name", response.getErrors().get(0).getField());
+//        assertEquals("Should not be empty.", response.getErrors().get(0).getMessage());
+//    }
+//
+//    private Database getDatabase() {
+//        return applicationContext.getBean(Database.class);
+//    }
+//
+//    private ChangeProductNameService getChangeProductNameService() {
+//        return applicationContext.getBean(ChangeProductNameService.class);
+//    }
+//}
