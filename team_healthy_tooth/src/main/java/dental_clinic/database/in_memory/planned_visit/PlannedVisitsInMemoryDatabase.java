a6dfb272ba6cb32d10e1d8558e291bf10f630784
ValerijaI <55547022+ValerijaI@users.noreply.github.com>
2021-01-02 16:03:36 +0200

    PlannedVisitsInMemoryDatabase created

---
+++ team_healthy_tooth/src/main/java/dental_clinic/database/in_memory/planned_visit/PlannedVisitsInMemoryDatabase.java
@@ -0,0 +1,19 @@
+package dental_clinic.database.in_memory.planned_visit;
+
+import dental_clinic.core.domain.PlannedVisit;
+
+import java.util.GregorianCalendar;
+import java.util.List;
+
+public interface PlannedVisitsInMemoryDatabase {
+
+    List<PlannedVisit> getPlannedVisits();
+
+    void addPlannedVisit(PlannedVisit plannedVisit);
+
+    void cancelPlannedVisit(Long id);
+
+    void changePlannedVisitTime(Long id, GregorianCalendar visitTime);
+
+    boolean containsId(Long id);
+}
