d9166db3d3f4d5a2b2c1673844a054e510ec270f
ValerijaI <55547022+ValerijaI@users.noreply.github.com>
2021-01-02 16:41:54 +0200

    PlannedVisit updated

--- team_healthy_tooth/src/main/java/dental_clinic/core/domain/PlannedVisit.java
+++ team_healthy_tooth/src/main/java/dental_clinic/core/domain/PlannedVisit.java
@@ -1,6 +1,8 @@
 package dental_clinic.core.domain;
 
+import java.util.Calendar;
 import java.util.GregorianCalendar;
+import java.util.Objects;
 
 public class PlannedVisit {
 
@@ -31,5 +33,27 @@ public class PlannedVisit {
 
     public PersonalData getPersonalData() {
         return personalData;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        PlannedVisit that = (PlannedVisit) o;
+        return Objects.equals(id, that.id) && Objects.equals(visitTime, that.visitTime) && Objects.equals(personalData, that.personalData);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, visitTime, personalData);
+    }
+
+    @Override
+    public String toString() {
+        return "Planned visit:\n" +
+                "visitTime: " + visitTime.get(Calendar.DAY_OF_MONTH) + " "
+                + visitTime.get(Calendar.MONTH) + " " + visitTime.get(Calendar.YEAR)+
+                visitTime.get(Calendar.HOUR_OF_DAY) + visitTime.get(Calendar.MINUTE) + "\n"
+                + personalData;
     }
 }
