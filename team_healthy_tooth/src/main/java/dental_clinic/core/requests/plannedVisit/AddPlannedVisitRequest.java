ce83856cce2e7fc9a229fa260bd6416c0a35315c
ValerijaI <55547022+ValerijaI@users.noreply.github.com>
2021-01-03 16:38:33 +0200

    AddPlannedVisitRequest updated

--- team_healthy_tooth/src/main/java/dental_clinic/core/requests/plannedVisit/AddPlannedVisitRequest.java
+++ team_healthy_tooth/src/main/java/dental_clinic/core/requests/plannedVisit/AddPlannedVisitRequest.java
@@ -4,12 +4,18 @@ import dental_clinic.core.domain.PersonalData;
 
 public class AddPlannedVisitRequest {
 
+    private boolean isNewPatient;
     private String visitDataText;
     private PersonalData personalData;
 
-    public AddPlannedVisitRequest(String visitDataText, PersonalData personalData) {
+    public AddPlannedVisitRequest(boolean isNewPatient, String visitDataText, PersonalData personalData) {
+        this.isNewPatient = isNewPatient;
         this.visitDataText = visitDataText;
         this.personalData = personalData;
+    }
+
+    public boolean getIsNewPatient() {
+        return isNewPatient;
     }
 
     public String getVisitDataText() {
