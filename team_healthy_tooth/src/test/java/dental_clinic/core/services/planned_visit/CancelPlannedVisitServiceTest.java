8a4951809ef00c2d5debc2ccdf2a32023641807f
ValerijaI <55547022+ValerijaI@users.noreply.github.com>
2021-01-03 15:09:54 +0200

    New tests

---
+++ team_healthy_tooth/src/test/java/dental_clinic/core/services/planned_visit/CancelPlannedVisitServiceTest.java
@@ -0,0 +1,79 @@
+package dental_clinic.core.services.planned_visit;
+
+import dental_clinic.core.requests.plannedVisit.CancelPlannedVisitRequest;
+import dental_clinic.core.responses.CoreError;
+import dental_clinic.core.responses.planned_visit.CancelPlannedVisitResponse;
+import dental_clinic.core.validators.planned_visit.CancelPlannedVisitValidator;
+import dental_clinic.database.in_memory.planned_visit.PlannedVisitsInMemoryDatabase;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+@RunWith(MockitoJUnitRunner.class)
+public class CancelPlannedVisitServiceTest {
+
+    @Mock
+    private PlannedVisitsInMemoryDatabase plannedVisitsInMemoryDatabase;
+    @Mock
+    private CancelPlannedVisitValidator cancelPlannedVisitValidator;
+    @InjectMocks
+    private CancelPlannedVisitService cancelPlannedVisitService;
+
+    @Test
+    public void validationErrors() {
+        CoreError coreError = new CoreError("id", "Not valid input for id");
+        List<CoreError> errorList = new ArrayList<>();
+        errorList.add(coreError);
+
+        CancelPlannedVisitRequest cancelPlannedVisitRequest = new CancelPlannedVisitRequest(-5L);
+
+        Mockito.when(cancelPlannedVisitValidator.validate(cancelPlannedVisitRequest)).thenReturn(errorList);
+
+        CancelPlannedVisitResponse cancelPlannedVisitResponse = cancelPlannedVisitService.execute(cancelPlannedVisitRequest);
+
+        assertTrue(cancelPlannedVisitResponse.hasErrors());
+        assertTrue(cancelPlannedVisitResponse.getErrors().size() == 1);
+        assertTrue(cancelPlannedVisitResponse.getErrors().contains(coreError));
+        Mockito.verifyNoInteractions(plannedVisitsInMemoryDatabase);
+    }
+
+    @Test
+    public void testDatabaseDoesNotContainId() {
+        CoreError coreError = new CoreError("database", "Database doesn't contain id 2");
+        List<CoreError> errorList = new ArrayList<>();
+        errorList.add(coreError);
+
+        CancelPlannedVisitRequest cancelPlannedVisitRequest = new CancelPlannedVisitRequest(2L);
+
+        Mockito.when(cancelPlannedVisitValidator.validate(cancelPlannedVisitRequest)).thenReturn(new ArrayList<>());
+        Mockito.when(plannedVisitsInMemoryDatabase.containsId(2L)).thenReturn(false);
+
+        CancelPlannedVisitResponse cancelPlannedVisitResponse = cancelPlannedVisitService.execute(cancelPlannedVisitRequest);
+
+        assertTrue(cancelPlannedVisitResponse.hasErrors());
+        assertTrue(cancelPlannedVisitResponse.getErrors().size() == 1);
+        assertTrue(cancelPlannedVisitResponse.getErrors().contains(coreError));
+    }
+
+    @Test
+    public void testSuccessfullyAdded() {
+        CancelPlannedVisitRequest cancelPlannedVisitRequest = new CancelPlannedVisitRequest(2L);
+
+        Mockito.when(cancelPlannedVisitValidator.validate(cancelPlannedVisitRequest)).thenReturn(new ArrayList<>());
+        Mockito.when(plannedVisitsInMemoryDatabase.containsId(2L)).thenReturn(true);
+
+        CancelPlannedVisitResponse cancelPlannedVisitResponse = cancelPlannedVisitService.execute(cancelPlannedVisitRequest);
+
+        assertFalse(cancelPlannedVisitResponse.hasErrors());
+        assertTrue(cancelPlannedVisitResponse.getId().equals(2L));
+    }
+
+}
