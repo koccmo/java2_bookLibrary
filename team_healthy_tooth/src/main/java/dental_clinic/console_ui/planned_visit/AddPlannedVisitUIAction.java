fa501efdf9a90c67937a20d17817abeb9e4ab5c3
ValerijaI <55547022+ValerijaI@users.noreply.github.com>
2021-01-03 16:38:14 +0200

    AddPlannedVisitUIAction changed logic for existing patients

--- team_healthy_tooth/src/main/java/dental_clinic/console_ui/planned_visit/AddPlannedVisitUIAction.java
+++ team_healthy_tooth/src/main/java/dental_clinic/console_ui/planned_visit/AddPlannedVisitUIAction.java
@@ -2,8 +2,11 @@ package dental_clinic.console_ui.planned_visit;
 
 import dental_clinic.console_ui.UIAction;
 import dental_clinic.core.domain.PersonalData;
+import dental_clinic.core.requests.patient.SearchPatientByPersonalCodeRequest;
 import dental_clinic.core.requests.plannedVisit.AddPlannedVisitRequest;
+import dental_clinic.core.responses.patient.SearchPatientByPersonalCodeResponse;
 import dental_clinic.core.responses.planned_visit.AddPlannedVisitResponse;
+import dental_clinic.core.services.patient.SearchPatientsByPersonalCodeService;
 import dental_clinic.core.services.planned_visit.AddPlannedVisitService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -15,6 +18,8 @@ public class AddPlannedVisitUIAction implements UIAction {
 
     @Autowired
     private AddPlannedVisitService addPlannedVisitService;
+    @Autowired
+    private SearchPatientsByPersonalCodeService searchPatientsByPersonalCodeService;
 
     @Override
     public void execute() {
@@ -24,18 +29,28 @@ public class AddPlannedVisitUIAction implements UIAction {
         System.out.println("Please enter visit date in format DD-MM-YYYY HH:MM");
         String visitDate = in.nextLine();
 
-        System.out.println("Please enter name");
-        String name = in.nextLine();
-        System.out.println("Please enter surname");
-        String surname = in.nextLine();
-        System.out.println("Please enter phone");
-        String phone = in.nextLine();
         System.out.println("Please enter personal code");
         String personalCode = in.nextLine();
 
-        PersonalData personalData = new PersonalData(name, surname, phone, personalCode);
+        SearchPatientByPersonalCodeRequest searchPatientByPersonalCodeRequest = new SearchPatientByPersonalCodeRequest(personalCode);
+        SearchPatientByPersonalCodeResponse searchPatientByPersonalCodeResponse = searchPatientsByPersonalCodeService.execute(searchPatientByPersonalCodeRequest);
 
-        AddPlannedVisitRequest addPlannedVisitRequest = new AddPlannedVisitRequest(visitDate, personalData);
+        PersonalData personalData = new PersonalData(null, null, null, personalCode);
+        Boolean isNewPatient = false;
+
+        if (searchPatientByPersonalCodeResponse.hasErrors()) {
+            System.out.println("Please enter name");
+            String name = in.nextLine();
+            System.out.println("Please enter surname");
+            String surname = in.nextLine();
+            System.out.println("Please enter phone");
+            String phone = in.nextLine();
+
+            personalData = new PersonalData(name, surname, phone, personalCode);
+            isNewPatient = true;
+        }
+
+        AddPlannedVisitRequest addPlannedVisitRequest = new AddPlannedVisitRequest(isNewPatient, visitDate, personalData);
         AddPlannedVisitResponse addPlannedVisitResponse = addPlannedVisitService.execute(addPlannedVisitRequest);
 
         if (addPlannedVisitResponse.hasErrors()) {
