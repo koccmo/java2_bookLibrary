5f3f45d288911a7b02457b4b2a026e8ef096fbea
Maris <maris.baduns@gmail.com>
2021-01-08 23:14:11 +0200

    Lesson_7 - step_2_strategy - Completed

--- team_marx/src/main/java/book_library/BookListApplication.java
+++ team_marx/src/main/java/book_library/BookListApplication.java
@@ -10,70 +10,17 @@ import java.util.Scanner;
 
 public class BookListApplication {
 
-    private static ApplicationContext applicationContext =
-            new AnnotationConfigApplicationContext(BookListConfiguration.class);
-
     public static void main(String[] args) {
-
-        while (true) {
-            printProgramMenu();
-
-            int userChoice = getMenuNumberFromUser();
-
-            executeSelectedMenuItem(userChoice);
+        ApplicationContext applicationContext = createApplicationContext();
+        ProgramMenu programMenu = applicationContext.getBean(ProgramMenu.class);
+        while (true){
+            programMenu.print();
+            int menuNumber = programMenu.getMenuNumberFromUser();
+            programMenu.executeSelectedMenuItem(menuNumber);
         }
     }
 
-    private static void executeSelectedMenuItem(int userChoice) {
-        switch (userChoice) {
-            case 1: {
-                AddBookUIAction uiAction = applicationContext.getBean(AddBookUIAction.class);
-                uiAction.execute();
-                break;
-            }
-            case 2: {
-                RemoveBookUIAction uiAction = applicationContext.getBean(RemoveBookUIAction.class);
-                uiAction.execute();
-                break;
-            }
-            case 3: {
-                GetAllBooksUIAction uiAction = applicationContext.getBean(GetAllBooksUIAction.class);
-                uiAction.execute();
-                break;
-            }
-            case 4: {
-                SearchBooksUIAction uiAction = applicationContext.getBean(SearchBooksUIAction.class);
-                uiAction.execute();
-                break;
-            }
-            case 5: {
-                ExitUIAction uiAction = applicationContext.getBean(ExitUIAction.class);
-                uiAction.execute();
-                break;
-            }
-        }
-
-        System.out.println("");
-    }
-
-    private static int getMenuNumberFromUser() {
-        System.out.println("Enter menu item to execute:");
-        Scanner scanner = new Scanner(System.in);
-        int userChoice = Integer.parseInt(scanner.nextLine());
-        return userChoice;
-    }
-
-    private static void printProgramMenu() {
-        System.out.println("=================================");
-        System.out.println("    MARX LIBRARY PROGRAM MENU: ");
-        System.out.println("=================================");
-        System.out.println("1. Add book to list");
-        System.out.println("2. Delete book from list");
-        System.out.println("3. Show all books in the list");
-        System.out.println("4. Search books");
-        System.out.println("5. Exit");
-        System.out.println("=================================");
-
-        System.out.println("");
+    private static ApplicationContext createApplicationContext() {
+        return new AnnotationConfigApplicationContext(BookListConfiguration.class);
     }
 }
---
+++ team_marx/src/main/java/book_library/console_ui/ProgramMenu.java
@@ -0,0 +1,57 @@
+package book_library.console_ui;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
+
+@Component
+public class ProgramMenu {
+
+    private Map<Integer, UIAction> menuNumberToUiActionMap;
+
+    @Autowired
+    public ProgramMenu(List<UIAction> uiActions) {
+        menuNumberToUiActionMap = new HashMap<>();
+        menuNumberToUiActionMap.put(1, findUIAction(uiActions, AddBookUIAction.class));
+        menuNumberToUiActionMap.put(2, findUIAction(uiActions, RemoveBookUIAction.class));
+        menuNumberToUiActionMap.put(3, findUIAction(uiActions, GetAllBooksUIAction.class));
+        menuNumberToUiActionMap.put(4, findUIAction(uiActions, SearchBooksUIAction.class));
+        menuNumberToUiActionMap.put(5, findUIAction(uiActions, ExitUIAction.class));
+    }
+
+    private UIAction findUIAction(List<UIAction> uiActions, Class uiActionClass) {
+        return uiActions.stream()
+                .filter(uiAction -> uiAction.getClass().equals(uiActionClass))
+                .findFirst()
+                .get();
+    }
+
+    public void print() {
+        System.out.println("=================================");
+        System.out.println("    MARX LIBRARY PROGRAM MENU: ");
+        System.out.println("=================================");
+        System.out.println("1. Add book to list");
+        System.out.println("2. Delete book from list");
+        System.out.println("3. Show all books in the list");
+        System.out.println("4. Search books");
+        System.out.println("5. Exit");
+        System.out.println("=================================");
+
+        System.out.println("");
+    }
+
+    public int getMenuNumberFromUser() {
+        System.out.println("Enter menu item to execute:");
+        Scanner scanner = new Scanner(System.in);
+        int userChoice = Integer.parseInt(scanner.nextLine());
+        return userChoice;
+    }
+
+    public void executeSelectedMenuItem(int userChoice) {
+        menuNumberToUiActionMap.get(userChoice).execute();
+    }
+}
