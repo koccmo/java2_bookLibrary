7556d9f14e572d291012b2603fb9f51cc486aeb5
Jaroslav <brutan.jaroslav@gmail.com>
2021-01-09 21:45:29 +0200

    database

--- team_no_name_no_problem/src/main/java/internet_store/config/MainMenuConfiguration.java
+++ team_no_name_no_problem/src/main/java/internet_store/config/MainMenuConfiguration.java
@@ -1,12 +1,83 @@
 package internet_store.config;
 
+import org.apache.commons.dbcp2.BasicDataSource;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.orm.hibernate5.HibernateTransactionManager;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+
+import javax.sql.DataSource;
+import java.util.Properties;
+
+
+
 
 @Configuration
 @ComponentScan(basePackages = "internet_store")
 @PropertySource(value = "classpath:application.properties")
+@PropertySource("classpath:database.properties")
+@EnableTransactionManagement
 public class MainMenuConfiguration {
 
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer(){
+        return new PropertySourcesPlaceholderConfigurer();
+    }
+
+    @Bean
+    public static DataSource dataSource(
+            @Value("${jdbc.url}") String jdbcUrl,
+            @Value("${driverClass}") String driverClass,
+            @Value("${database.user.name}") String userName,
+            @Value("${database.user.password}") String userPassword) {
+        BasicDataSource ds = new BasicDataSource();
+        ds.setUrl(jdbcUrl);
+        ds.setDriverClassName(driverClass);
+        ds.setUsername(userName);
+        ds.setPassword(userPassword);
+        return ds;
+    }
+
+    @Bean
+    public Properties hibernateProperties(
+            @Value("${hibernate.dialect}") String dialect,
+            @Value("${hibernate.show_sql}") boolean showSql,
+            @Value("${hibernate.format_sql}") boolean formatSql,
+            @Value("${hibernate.hbm2ddl.auto}") String hbm2ddl) {
+        Properties properties = new Properties();
+        properties.put("hibernate.dialect", dialect);
+        properties.put("hibernate.show_sql", showSql);
+        properties.put("hibernate.format_sql", formatSql);
+        properties.put("hibernate.hbm2ddl.auto", hbm2ddl);
+
+        return properties;
+    }
+
+    @Bean
+    public SessionFactory sessionFactory(DataSource dataSource,
+                                         @Value("${hibernate.packagesToScan}") String packagesToScan,
+                                         @Qualifier("hibernateProperties") Properties properties)
+                                            throws Exception{
+        LocalSessionFactoryBean sessionFactoryBean =
+                new LocalSessionFactoryBean();
+        sessionFactoryBean.setDataSource(dataSource);
+        sessionFactoryBean.setPackagesToScan(packagesToScan);
+        sessionFactoryBean.setHibernateProperties(properties);
+        sessionFactoryBean.afterPropertiesSet();
+        return sessionFactoryBean.getObject();
+    }
+
+    @Bean
+    public PlatformTransactionManager transactionManager(SessionFactory sessionFactory){
+        return new HibernateTransactionManager(sessionFactory);
+    }
 }
