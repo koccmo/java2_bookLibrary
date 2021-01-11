416e06a07a550828a1abcf1e2ae16f7db75a83d8
Anvar331 <anvar.intezar@gmail.com>
2021-01-11 22:11:23 +0200

    ProgramMenu + SearchProductService

--- team_no_name_no_problem/src/main/java/internet_store/core/services/product/SearchProductService.java
+++ team_no_name_no_problem/src/main/java/internet_store/core/services/product/SearchProductService.java
@@ -53,7 +53,27 @@ import java.util.stream.Collectors;
         if (isDescriptionFilled(searchProductRequest.getDescription())) {
             return searchByDescriptionIsProvided(searchProductRequest);
         }
-        return searchByPriceRangeIsProvided(searchProductRequest);
+        if (isPriceRangeFilled(searchProductRequest.getStartPrice(),searchProductRequest.getEndPrice())){
+            return searchByPriceRangeIsProvided(searchProductRequest);
+        }
+        if (!isTitleFilled(searchProductRequest.getTitle())) {
+            return searchByDescriptionIsProvided(searchProductRequest);
+        }
+        if (!isDescriptionFilled(searchProductRequest.getDescription())) {
+            return searchByPriceRangeIsProvided(searchProductRequest);
+        }
+        if (!isTitleFilled(searchProductRequest.getTitle()) && (!isDescriptionFilled(searchProductRequest.getDescription()))) {
+            return searchByPriceRangeIsProvided(searchProductRequest);
+        }
+        if (!isDescriptionFilled(searchProductRequest.getDescription()) &&
+           (!isPriceRangeFilled(searchProductRequest.getStartPrice(), searchProductRequest.getEndPrice()))) {
+            return searchByTitleIsProvided(searchProductRequest);
+        }
+        if (!isTitleFilled(searchProductRequest.getDescription()) &&
+                (!isPriceRangeFilled(searchProductRequest.getStartPrice(), searchProductRequest.getEndPrice()))) {
+            return searchByDescriptionIsProvided(searchProductRequest);
+        }
+        return searchByTitleAndDescriptionAndPriceIsProvided(searchProductRequest);
     }
 
     private boolean isTitleAndDescriptionAndPriceNotEmpty(String title, String description,
@@ -63,12 +83,12 @@ import java.util.stream.Collectors;
     }
 
     private SearchProductResponse searchByTitleAndDescriptionAndPriceIsProvided(SearchProductRequest searchProductRequest){
-        //TODO eto ne searchByTitleAndDescriptionAndPriceIsProvided
         List <CoreError>errors = new ArrayList<>();
         List <Product> products = productDatabase.findAllByTitleAndDescription(searchProductRequest.getTitle(), searchProductRequest.getDescription());
         if (products.isEmpty()){
             errors.add(new CoreError("database", "Database doesn't contain product with title: " +
-                    searchProductRequest.getTitle() + " and description: " + searchProductRequest.getDescription()));
+                    searchProductRequest.getTitle() + ", description: " + searchProductRequest.getDescription() +
+                    ", price range: from " + searchProductRequest.getStartPrice() + " till " + searchProductRequest.getEndPrice()));
             return new SearchProductResponse(errors, new ArrayList<>());
         }
         products = order(products, searchProductRequest.getOrdering());
@@ -82,6 +102,10 @@ import java.util.stream.Collectors;
 
     private boolean isDescriptionFilled(String description) {
         return description != null && !description.isEmpty();
+    }
+
+    private boolean isPriceRangeFilled(Integer startPrice, Integer endPrice) {
+        return startPrice != null && endPrice != null;
     }
 
     private SearchProductResponse searchByTitleIsProvided(SearchProductRequest searchProductRequest){
