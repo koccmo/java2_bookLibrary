531031cb8547f01bedd47ee6d9d16520a0de68ee
Anvar331 <anvar.intezar@gmail.com>
2021-01-11 22:24:59 +0200

    SearchProductService

--- team_no_name_no_problem/src/main/java/internet_store/core/services/product/SearchProductService.java
+++ team_no_name_no_problem/src/main/java/internet_store/core/services/product/SearchProductService.java
@@ -39,10 +39,7 @@ import java.util.stream.Collectors;
     }
 
     private SearchProductResponse provideSearchResultAccordingToRequest(SearchProductRequest searchProductRequest) {
-        //TODO Anvar, tut vsjo esjo net vseh variantov
-        //TODO Anvar, podumaj
-        //TODO I napishi vse, raz ho4esh delatj poisk po 3 poljam, variantov boljshe!!
-        //TODO 3 + 3 + 1 != 4
+
         if (isTitleAndDescriptionAndPriceNotEmpty(searchProductRequest.getTitle(), searchProductRequest.getDescription(),
                 searchProductRequest.getStartPrice(), searchProductRequest.getEndPrice())) {
             return searchByTitleAndDescriptionAndPriceIsProvided(searchProductRequest);
