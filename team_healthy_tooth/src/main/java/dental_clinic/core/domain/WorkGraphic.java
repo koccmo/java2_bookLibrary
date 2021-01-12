ec789857ccc226fe7a9a8f1ceb035274632c56f2
ValerijaI <55547022+ValerijaI@users.noreply.github.com>
2021-01-12 22:41:10 +0200

    WorkGraphic to array

--- team_healthy_tooth/src/main/java/dental_clinic/core/domain/WorkGraphic.java
+++ team_healthy_tooth/src/main/java/dental_clinic/core/domain/WorkGraphic.java
@@ -2,98 +2,34 @@ package dental_clinic.core.domain;
 
 public class WorkGraphic {
 
-    private String mondayStart = "";
-    private String mondayEnd = "";
-    private String tuesdayStart = "";
-    private String tuesdayEnd;
-    private String wednesdayStart;
-    private String wednesdayEnd;
-    private String thursdayStart;
-    private String thursdayEnd;
-    private String fridayStart;
-    private String fridayEnd;
-    private String saturdayStart;
-    private String saturdayEnd;
-    private String sundayStart;
-    private String sundayEnd;
 
-    public WorkGraphic(String mondayStart, String mondayEnd, String tuesdayStart, String tuesdayEnd,
-                       String wednesdayStart, String wednesdayEnd, String thursdayStart, String thursdayEnd,
-                       String fridayStart, String fridayEnd, String saturdayStart, String saturdayEnd,
-                       String sundayStart, String sundayEnd) {
-        this.mondayStart = mondayStart;
-        this.mondayEnd = mondayEnd;
-        this.tuesdayStart = tuesdayStart;
-        this.tuesdayEnd = tuesdayEnd;
-        this.wednesdayStart = wednesdayStart;
-        this.wednesdayEnd = wednesdayEnd;
-        this.thursdayStart = thursdayStart;
-        this.thursdayEnd = thursdayEnd;
-        this.fridayStart = fridayStart;
-        this.fridayEnd = fridayEnd;
-        this.saturdayStart = saturdayStart;
-        this.saturdayEnd = saturdayEnd;
-        this.sundayStart = sundayStart;
-        this.sundayEnd = sundayEnd;
+    String timesStart [] = new String [7];
+    String timesEnd [] = new String [7];
+
+    public WorkGraphic() {
+        for (int i = 0; i < timesStart.length; i++) {
+            timesStart[i] = "";
+            timesEnd[i] = "";
+        }
     }
 
-    public WorkGraphic () {};
-
-    public String getMondayEnd() {
-        return mondayEnd;
+    public String[] getTimesStart() {
+        return timesStart;
     }
 
-    public String getTuesdayStart() {
-        return tuesdayStart;
+    public String[] getTimesEnd() {
+        return timesEnd;
     }
 
-    public String getTuesdayEnd() {
-        return tuesdayEnd;
+    @Override
+    public String toString() {
+        return "WorkGraphic:\n" +
+                "monday: " + timesStart[0] + " - " + timesEnd[0] + "\n" +
+                "tuesday: " + timesStart[1] + " - " + timesEnd[1] + "\n" +
+                "wednesday: " + timesStart[2] + " - " + timesEnd[2] + "\n" +
+                "thursdayStart: " + timesStart[3] + " - " + timesEnd[3] + "\n" +
+                "fridayStart: " + timesStart[4] + " - " +  timesEnd[4] + "\n" +
+                "saturdayStart: " + timesStart[5] + " - " + timesEnd[5] + "\n" +
+                "sundayStart: " + timesStart[6] + " - " + timesEnd[6] + "\n";
     }
-
-    public String getWednesdayStart() {
-        return wednesdayStart;
-    }
-
-    public String getWednesdayEnd() {
-        return wednesdayEnd;
-    }
-
-    public String getThursdayStart() {
-        return thursdayStart;
-    }
-
-    public String getThursdayEnd() {
-        return thursdayEnd;
-    }
-
-    public String getFridayStart() {
-        return fridayStart;
-    }
-
-    public String getFridayEnd() {
-        return fridayEnd;
-    }
-
-    public String getSaturdayStart() {
-        return saturdayStart;
-    }
-
-    public String getSaturdayEnd() {
-        return saturdayEnd;
-    }
-
-    public String getSundayStart() {
-        return sundayStart;
-    }
-
-    public String getSundayEnd() {
-        return sundayEnd;
-    }
-
-    public String getMondayStart() {
-        return mondayStart;
-    }
-
-
 }
