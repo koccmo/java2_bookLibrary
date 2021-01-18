0ed55dd1c5dab7b47d7a67763c13432008d4256c
Anvar331 <anvar.intezar@gmail.com>
2021-01-18 20:45:35 +0200

    UIActions amendments

--- team_no_name_no_problem/src/main/java/internet_store/console_ui/product/DeleteByOtherUIAction.java
+++ team_no_name_no_problem/src/main/java/internet_store/console_ui/product/DeleteByOtherUIAction.java
@@ -23,6 +23,7 @@ public class DeleteByOtherUIAction implements UIAction {
         System.out.println("In order to delete the product by one of the criteria");
         System.out.println("it is not necessary to enter all of them. However, if one price type");
         System.out.println("is entered, another also should be entered. If one is skipped, other should be as well.");
+        System.out.println("Price range can be skipped only by entering zeros.");
         System.out.println();
 
         System.out.println("Please enter title to delete by title");
