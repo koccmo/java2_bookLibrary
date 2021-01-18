075cee3750c4feabf1e22abde185d81128b6d85a
m_projects@inbox.lv <Galatasaraj01>
2021-01-18 23:22:45 +0200

    Acceptance tests added

---
+++ team_VK/src/database/dbDataInsertScript.sql
@@ -0,0 +1,25 @@
+
+insert into books (title, author, bookingPeriod)
+values ("The Old Man and Sea", "Hemingway E.", 10);
+insert into books (title, author, bookingPeriod)
+values ("Captain's doter", "Pushkin A.", 10);
+insert into books (title, author, bookingPeriod)
+values ("Broken pines", "Rainis J.", 5);
+
+insert into books (id, title, author, bookingPeriod)
+values (4, "Calm Don", "Shelohov M.", 15);
+
+
+insert into clients (first_name, last_name, personalCode)
+values ("Andrew","Petroff", "123656-32145");
+insert into clients (first_name, last_name, personalCode)
+values ("Piter", "McAndrew", "211232-65446");
+insert into clients (first_name, last_name, personalCode)
+values ("Serjio", "Stecletto", "013212-65478");
+insert into clients (first_name, last_name, personalCode)
+values ("Mash", "Uralmasheva", "020196-45612");
+insert into clients (first_name, last_name, personalCode)
+values ("Pier", "Bezuhov", "151175-12365");
+insert into clients (first_name, last_name, personalCode)
+values ("Ivan", "Smirnov", "020202-65233");
+
