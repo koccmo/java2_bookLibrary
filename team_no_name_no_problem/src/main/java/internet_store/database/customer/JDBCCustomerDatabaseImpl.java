654d922532f01187e27f884c9a4e338b0d32978a
Jaroslav <brutan.jaroslav@gmail.com>
2021-01-18 22:40:02 +0200

    jdbc update

--- team_no_name_no_problem/src/main/java/internet_store/database/customer/JDBCCustomerDatabaseImpl.java
+++ team_no_name_no_problem/src/main/java/internet_store/database/customer/JDBCCustomerDatabaseImpl.java
@@ -2,9 +2,12 @@ package internet_store.database.customer;
 
 import internet_store.core.domain.Customer;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 
+import java.sql.PreparedStatement;
 import java.util.List;
 import java.util.Optional;
 
@@ -16,8 +19,7 @@ public class JDBCCustomerDatabaseImpl implements CustomerDatabase{
 
     @Override
     public List<Customer> getCustomers() {
-        jdbcTemplate.queryForObject("SELECT * FROM customers", Integer.class);
-        return null;
+        return jdbcTemplate.query("SELECT * FROM customers", new BeanPropertyRowMapper<>(Customer.class));
     }
 
     @Override
@@ -29,29 +31,52 @@ public class JDBCCustomerDatabaseImpl implements CustomerDatabase{
     }
 
     @Override
-    public void deleteCustomerById(Long id) {
-        jdbcTemplate.update("DELETE FROM customers WHERE id = ?" + "VALUES (?)",
-                id);
+    public boolean deleteCustomerById(Long id) {
+        return jdbcTemplate.update(connection -> {
+                    PreparedStatement statement = connection.prepareStatement(
+                            "DELETE FROM customers WHERE ID = ?");
+                    statement.setLong(1, id);
+                    return statement;
+                }) == 1;
     }
 
     @Override
     public Optional<Customer> findById(Long id) {
-        return Optional.empty();
+        try{
+            Customer customer = jdbcTemplate.queryForObject("SELECT * FROM customers WHERE id = ?",
+                    new Object[]{id}, new BeanPropertyRowMapper<>(Customer.class));
+            return Optional.ofNullable(customer);
+        } catch (EmptyResultDataAccessException e) {
+            return Optional.empty();
+        }
     }
 
     @Override
     public List<Customer> findCustomersByNameAndSurname(String name, String surname) {
-        return null;
+        return jdbcTemplate.query(connection -> {
+            PreparedStatement statement = connection.prepareStatement("SELECT * FROM customers WHERE name LIKE ? AND surname LIKE ?");
+            statement.setString(1, name);
+            statement.setString(2, surname);
+            return statement;
+        }, new BeanPropertyRowMapper<>(Customer.class));
     }
 
     @Override
     public List<Customer> findAllCustomersByName(String name) {
-        return null;
+        return jdbcTemplate.query(connection -> {
+            PreparedStatement statement = connection.prepareStatement("SELECT * FROM customers WHERE name LIKE ?");
+            statement.setString(1, name);
+            return statement;
+        }, new BeanPropertyRowMapper<>(Customer.class));
     }
 
     @Override
     public List<Customer> findAllCustomersBySurname(String surname) {
-        return null;
+        return jdbcTemplate.query(connection -> {
+            PreparedStatement statement = connection.prepareStatement("SELECT * FROM customers WHERE surname LIKE ?");
+            statement.setString(1, surname);
+            return statement;
+        }, new BeanPropertyRowMapper<>(Customer.class));
     }
 
     @Override
