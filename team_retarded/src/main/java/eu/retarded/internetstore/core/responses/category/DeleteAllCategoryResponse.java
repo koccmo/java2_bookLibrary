03be13158095306f77f4a97479c0daff174fb609
saturn7888 <saturn@lmr.lv>
2021-01-26 12:19:11 +0200

    версия магазина от 26012021

--- team_retarded/src/main/java/eu/retarded/internetstore/core/responses/category/DeleteAllCategoryResponse.java
+++
@@ -1,9 +0,0 @@
-package eu.retarded.internetstore.core.responses.category;
-
-import eu.retarded.internetstore.core.responses.CoreResponse;
-
-
-public class DeleteAllCategoryResponse extends CoreResponse {
-
-
-}
