5b1ef65d9bf82b57baf88dbdf66216db6da53bd2
Maris <maris.baduns@gmail.com>
2021-01-26 16:46:07 +0200

    Lesson_11 - task_18 - Completed

--- team_marx/src/main/java/book_library/core/database/OrmBookRepositoryImp.java
+++ team_marx/src/main/java/book_library/core/database/OrmBookRepositoryImp.java
@@ -5,16 +5,19 @@ import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import javax.transaction.Transactional;
 import java.util.List;
 
 @Component
-public class OrmBookRepositoryImp implements BookRepository{
+@Transactional
+public class OrmBookRepositoryImp implements BookRepository {
 
-    @Autowired private SessionFactory sessionFactory;
+    @Autowired
+    private SessionFactory sessionFactory;
 
     @Override
     public void save(Book book) {
-
+        sessionFactory.getCurrentSession().save(book);
     }
 
     @Override
