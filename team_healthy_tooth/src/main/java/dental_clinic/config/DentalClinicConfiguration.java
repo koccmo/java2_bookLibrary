244cfd4f9674dbc663c86bd8e616c8e3ea933cc9
ValerijaI <55547022+ValerijaI@users.noreply.github.com>
2021-01-27 22:04:50 +0200

    DentalClinicConfiguration + jdbc

--- team_healthy_tooth/src/main/java/dental_clinic/config/DentalClinicConfiguration.java
+++ team_healthy_tooth/src/main/java/dental_clinic/config/DentalClinicConfiguration.java
@@ -1,13 +1,47 @@
 package dental_clinic.config;
 
+import org.apache.commons.dbcp2.BasicDataSource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import javax.sql.DataSource;
 
 @Configuration
 @ComponentScan(basePackages = "dental_clinic")
 @PropertySource(value = "classpath:application.properties")
 public class DentalClinicConfiguration {
+
+    //JDBC
+    @Value("${jdbc.url}")
+    private String jdbcUrl;
+
+    @Value("${driverClass}")
+    private String driverClass;
+
+    @Value("${database.user.name}")
+    private String userName;
+
+    @Value("${database.user.password}")
+    private String password;
+
+    @Bean
+    public DataSource dataSource() {
+        BasicDataSource dataSource = new BasicDataSource();
+        dataSource.setUrl(jdbcUrl);
+        dataSource.setDriverClassName(driverClass);
+        dataSource.setUsername(userName);
+        dataSource.setPassword(password);
+        return dataSource;
+    }
+
+    @Bean
+	public JdbcTemplate jdbcTemplate(DataSource dataSource) {
+		return new JdbcTemplate(dataSource);
+	}
 }
 
 //Spring Framework поддерживает несколько способов хранения конфигурации:
