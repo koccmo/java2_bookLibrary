8167893d7c46888ca900ebf6b2bc77b0d468e1ce
Igors Mihejevs <mihejevs.igors@gmail.com>
2021-01-29 14:38:09 +0200

    ChangeProductNameAcceptanceTest refactoring to Hibernate (need to be corrected to real Database because of IDs)

--- team_static_startup/src/test/java/internet_store/application/acceptancetests/ChangeProductNameAcceptanceTest.java
+++ team_static_startup/src/test/java/internet_store/application/acceptancetests/ChangeProductNameAcceptanceTest.java
@@ -1,31 +1,47 @@
 package internet_store.application.acceptancetests;
 
+import internet_store.application.config.AppConfig;
+import internet_store.application.core.database.product.ProductRepository;
+import internet_store.application.core.domain.Product;
+import internet_store.application.core.requests.product.ChangeProductNameRequest;
+import internet_store.application.core.responses.product.ChangeProductNameResponse;
+import internet_store.application.core.services.product.ChangeProductNameService;
+import internet_store.application.database_cleaner.DatabaseCleaner;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Profile;
+import static org.junit.Assert.*;
 
-@Profile("inmemory")
+import java.math.BigDecimal;
+
+@Profile("hibernate")
 public class ChangeProductNameAcceptanceTest {
-/*
-    private ApplicationContext applicationContext;
-    private Database database;
+
+    private ApplicationContext appContext;
+    private ProductRepository productRepository;
 
     @Before
     public void setUp() {
-        applicationContext =
-                new AnnotationConfigApplicationContext(ProductListConfiguration.class);
-        database = getDatabase();
-        database.add(new Product("iPhone", "phone", new BigDecimal("900")));
-        database.add(new Product("iMac", "pc", new BigDecimal("4000")));
+        appContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        getDatabaseCleaner().clean();
+        productRepository = getRepository();
+        productRepository.add(new Product("iPhone", "phone", new BigDecimal("900")));
+        productRepository.add(new Product("iMac", "pc", new BigDecimal("4000")));
     }
 
+/*
     @Test
     public void shouldChangeProductName() {
         ChangeProductNameRequest request = new ChangeProductNameRequest(1L, "iPhone12");
         ChangeProductNameResponse response = getChangeProductNameService().execute(request);
 
         assertTrue(response.isNameChanged());
-        assertEquals("iPhone12", database.getProductList().get(0).getName());
+        assertEquals("iPhone12", productRepository.getProductList().get(0).getName());
         assertNull(response.getErrors());
     }
+*/
 
     @Test
     public void shouldNotChangeNameWhenProductNotFound() {
@@ -47,11 +63,16 @@ public class ChangeProductNameAcceptanceTest {
         assertEquals("Should not be empty.", response.getErrors().get(0).getMessage());
     }
 
-    private Database getDatabase() {
-        return applicationContext.getBean(Database.class);
+    private ProductRepository getRepository() {
+        return appContext.getBean(ProductRepository.class);
     }
 
     private ChangeProductNameService getChangeProductNameService() {
-        return applicationContext.getBean(ChangeProductNameService.class);
-    }*/
+        return appContext.getBean(ChangeProductNameService.class);
+    }
+
+    private DatabaseCleaner getDatabaseCleaner() {
+        return appContext.getBean(DatabaseCleaner.class);
+    }
+
 }
