d684bbfc8378bfdb1b495fc89d0f7355578ca31c
ValerijaI <55547022+ValerijaI@users.noreply.github.com>
2021-02-03 23:07:46 +0200

    OrmDoctorRepositoryImpl updated methods

--- team_healthy_tooth/src/main/java/dental_clinic/core/database/doctor/OrmDoctorRepositoryImpl.java
+++ team_healthy_tooth/src/main/java/dental_clinic/core/database/doctor/OrmDoctorRepositoryImpl.java
@@ -1,13 +1,12 @@
 package dental_clinic.core.database.doctor;
 
 import dental_clinic.core.domain.Doctor;
-import dental_clinic.core.domain.WorkGraphic;
+import dental_clinic.core.domain.DoctorsWorkGraphic;
 import org.hibernate.SessionFactory;
 import org.hibernate.criterion.Restrictions;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
-
 import javax.persistence.Query;
 import java.util.List;
 import java.util.Optional;
@@ -29,26 +28,30 @@ public class OrmDoctorRepositoryImpl implements DoctorRepository{
     @Override
     public void addDoctor(Doctor doctor) {
         sessionFactory.getCurrentSession().save(doctor);
+        Long id = getDoctorList().get(getDoctorList().size()-1).getId();
+        DoctorsWorkGraphic doctorsWorkGraphic = new DoctorsWorkGraphic(getDoctorById(id).get());
+        sessionFactory.getCurrentSession().save(doctorsWorkGraphic);
     }
 
     @Override
-    public boolean deleteDoctorById(Long id) {
-        Query query = sessionFactory.getCurrentSession().createQuery(
-                "delete Doctor WHERE id = :id");
+    public void deleteDoctorById(Long id) {
+        Query query = sessionFactory.getCurrentSession().createQuery("UPDATE Doctor SET isEmployed = false " +
+                "WHERE id = :id");
         query.setParameter("id", id);
-        int result = query.executeUpdate();
-        return result == 1;
+        query.executeUpdate();
     }
 
     @Override
     public boolean containsDoctor(Doctor doctor) {
-        return sessionFactory.getCurrentSession().contains(doctor);
+        return getDoctorList().stream()
+                .filter(doctor1 -> doctor1.equals(doctor))
+                .findAny().isPresent();
     }
 
     @Override
     public boolean containsId(Long id) {
         Query query = sessionFactory.getCurrentSession().createQuery(
-                "SELECT d FROM Doctor WHERE id = :id");
+                "SELECT d FROM Doctor d WHERE id = :id");
         query.setParameter("id", id);
         return !query.getResultList().isEmpty();
     }
@@ -71,12 +74,12 @@ public class OrmDoctorRepositoryImpl implements DoctorRepository{
 
     @Override
     public void updateWorkGraphicForSpecificDate(Long id, Integer day, String timeFrom, String timeTo) {
-        String sql = "UPDATE WorkGraphic SET " + getDayNameStart(day) + " = : timeFrom WHERE doctor_id = :id";
+        String sql = "UPDATE DoctorsWorkGraphic SET " + getDayNameStart(day) + " = : timeFrom WHERE doctor_id = :id";
         Query query = sessionFactory.getCurrentSession().createQuery(sql);
         query.setParameter("timeFrom", timeFrom);
         query.setParameter("id", id);
         query.executeUpdate();
-        String sqlTo = "UPDATE WorkGraphic SET " + getDayNameEnd(day) + " = : timeTo WHERE doctor_id = :id";
+        String sqlTo = "UPDATE DoctorsWorkGraphic SET " + getDayNameEnd(day) + " = : timeTo WHERE doctor_id = :id";
         Query queryTo = sessionFactory.getCurrentSession().createQuery(sqlTo);
         queryTo.setParameter("timeTo", timeTo);
         queryTo.setParameter("id", id);
@@ -84,10 +87,10 @@ public class OrmDoctorRepositoryImpl implements DoctorRepository{
     }
 
     @Override
-    public WorkGraphic getWorkGraphic(Doctor doctor) {
-        String sql = "SELECT w FROM WorkGraphic w WHERE doctor_id = " + doctor.getId();
+    public DoctorsWorkGraphic getWorkGraphic(Doctor doctor) {
+        String sql = "SELECT w FROM DoctorsWorkGraphic w WHERE doctor_id = " + doctor.getId();
         return sessionFactory.getCurrentSession()
-                .createQuery(sql, WorkGraphic.class)
+                .createQuery(sql, DoctorsWorkGraphic.class)
                 .getSingleResult();
     }
 
