0e42b4cd119940cc65708e025b65b4c2d9f25209
Jaroslav <brutan.jaroslav@gmail.com>
2021-02-03 22:33:25 +0200

    order item new class

---
+++ team_no_name_no_problem/src/main/java/internet_store/core/domain/OrderItem.java
@@ -0,0 +1,25 @@
+package internet_store.core.domain;
+
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "order_item")
+public class OrderItem {
+
+    @Id
+    @Column(name = "order_item_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    @JoinColumn(name = "order_id")
+    private Order order;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    @Column(name = "quantity")
+    private Integer quantity;
+}
