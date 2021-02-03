6d124ef61309ef961acad022be93a9186a3740aa
Jaroslav <brutan.jaroslav@gmail.com>
2021-02-03 21:34:11 +0200

    shopping cart update

--- team_no_name_no_problem/src/main/java/internet_store/core/domain/ShoppingCart.java
+++ team_no_name_no_problem/src/main/java/internet_store/core/domain/ShoppingCart.java
@@ -1,62 +1,13 @@
 package internet_store.core.domain;
 
-import javax.persistence.*;
-import java.util.Objects;
-
-@Entity
-@Table(name = "shopping_cart")
 public class ShoppingCart {
 
-    @Id
-    @Column(name = "cart_id")
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private Customer customer;
 
-    @ManyToOne
-    @JoinColumn(name = "order_id", nullable = false)
-    private Order order;
+    private Integer sumTotal;
 
-    @ManyToOne
-    @JoinColumn(name = "product_id", nullable = false)
-    private Product product;
-
-    @Column
-    private Integer quantity;
-
-    public ShoppingCart() {
+    public Integer getSumTotal() {
+      return getSumTotal();
     }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public Order getOrder() {
-        return order;
-    }
-
-    public void setOrder(Order order) {
-        this.order = order;
-    }
-
-    public Product getProduct() {
-        return product;
-    }
-
-    public void setProduct(Product product) {
-        this.product = product;
-    }
-
-    public Integer getQuantity() {
-        return quantity;
-    }
-
-    public void setQuantity(Integer quantity) {
-        this.quantity = quantity;
-    }
-
 
 }
