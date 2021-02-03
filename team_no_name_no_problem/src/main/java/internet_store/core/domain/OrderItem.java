3d0fc09c72ea3bada295745ab91f23be8aba4e06
Jaroslav <brutan.jaroslav@gmail.com>
2021-02-03 23:39:22 +0200

    order item class update

--- team_no_name_no_problem/src/main/java/internet_store/core/domain/OrderItem.java
+++ team_no_name_no_problem/src/main/java/internet_store/core/domain/OrderItem.java
@@ -2,6 +2,7 @@ package internet_store.core.domain;
 
 
 import javax.persistence.*;
+import java.util.Objects;
 
 @Entity
 @Table(name = "order_item")
@@ -21,5 +22,63 @@ public class OrderItem {
     private Product product;
 
     @Column(name = "quantity")
-    private Integer quantity;
+    private Integer itemQuantity;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Order getOrder() {
+        return order;
+    }
+
+    public void setOrder(Order order) {
+        this.order = order;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public Integer getItemQuantity() {
+        return itemQuantity;
+    }
+
+    public void setItemQuantity(Integer itemQuantity) {
+        this.itemQuantity = itemQuantity;
+    }
+
+    public OrderItem () { }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        OrderItem orderItem = (OrderItem) o;
+        return Objects.equals(id, orderItem.id) && Objects.equals(order, orderItem.order) && Objects.equals(product, orderItem.product) && Objects.equals(itemQuantity, orderItem.itemQuantity);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, order, product, itemQuantity);
+    }
+
+    @Override
+    public String toString() {
+        return "OrderItem{" +
+                "id=" + id +
+                ", order=" + order +
+                ", product=" + product +
+                ", itemQuantity=" + itemQuantity +
+                '}';
+    }
 }
+
