85aa3cdd8d7bc8c5f2b06db451216b961c4c3785
Jaroslav <brutan.jaroslav@gmail.com>
2021-02-03 23:39:33 +0200

    shopping cart class update

--- team_no_name_no_problem/src/main/java/internet_store/core/domain/ShoppingCart.java
+++ team_no_name_no_problem/src/main/java/internet_store/core/domain/ShoppingCart.java
@@ -1,13 +1,78 @@
 package internet_store.core.domain;
 
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity
+@Table(name = "shopping_cart")
 public class ShoppingCart {
 
+    @Id
+    @Column(name = "shopping_cart_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    @JoinColumn(name = "customer_id")
     private Customer customer;
 
+    @Column(name = "sum_total")
     private Integer sumTotal;
 
     public Integer getSumTotal() {
+         Integer sum = 0;
       return getSumTotal();
     }
 
+   /* public Integer getSumTotal() {
+        Integer sum = 0;
+        for (Product product : shoppingCart.keySet()) {
+            sum += shoppingCart.get(product) * product.getPrice();
+        }
+        return sum;
+    }*/
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+
+    public void setSumTotal(Integer sumTotal) {
+        this.sumTotal = sumTotal;
+    }
+
+    public ShoppingCart () { }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ShoppingCart that = (ShoppingCart) o;
+        return Objects.equals(id, that.id) && Objects.equals(customer, that.customer) && Objects.equals(sumTotal, that.sumTotal);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, customer, sumTotal);
+    }
+
+    @Override
+    public String toString() {
+        return "ShoppingCart{" +
+                "id=" + id +
+                ", customer=" + customer +
+                ", sumTotal=" + sumTotal +
+                '}';
+    }
 }
