9c49794603d7b40efdee811b861811f47a169594
ValerijaI <55547022+ValerijaI@users.noreply.github.com>
2021-02-05 00:35:30 +0200

    GetPatientCardService

--- team_healthy_tooth/src/main/java/dental_clinic/core/services/patient/GetPatientCardService.java
+++ team_healthy_tooth/src/main/java/dental_clinic/core/services/patient/GetPatientCardService.java
@@ -1,8 +1,5 @@
 package dental_clinic.core.services.patient;
 
-import dental_clinic.core.domain.Jowl;
-import dental_clinic.core.domain.Patient;
-import dental_clinic.core.domain.ToothStatus;
 import dental_clinic.core.requests.patient.GetPatientCardRequest;
 import dental_clinic.core.responses.CoreError;
 import dental_clinic.core.responses.patient.GetPatientCardResponse;
@@ -32,43 +29,8 @@ public class GetPatientCardService {
                     getPatientCardRequest.getId()));
             return new GetPatientCardResponse(errors);
         }
-        
-        Patient patientCard = createPatientCard(getPatientCardRequest);
-        return new GetPatientCardResponse(patientCard);
-    }
 
-    private Patient createPatientCard(GetPatientCardRequest getPatientCardRequest){
-        Patient newPatient = new Patient();
-        newPatient.setPersonalData(patientRepository.getPatientCard(getPatientCardRequest.getId()).get().getPersonalData());
-        Jowl specificPatientJowl = new Jowl();
-        specificPatientJowl = getSpecificPatientJowl(getPatientCardRequest);
-        Jowl currentStatusOfJowl = new Jowl();
-        currentStatusOfJowl = getCurrentPatientJowl(specificPatientJowl);
-        newPatient.setJowl(currentStatusOfJowl);
-        return newPatient;
-    }
-
-    private Jowl getCurrentPatientJowl(Jowl specificPatientJowl) {
-        Jowl currentStatusOfJowl = new Jowl();
-        for (Integer key : specificPatientJowl.getJowl().keySet()) {
-            currentStatusOfJowl.getJowl().put(key,
-                    new ArrayList<>(addLastStatus(specificPatientJowl, key)));
-        }
-        return currentStatusOfJowl;
-    }
-
-    private Jowl getSpecificPatientJowl(GetPatientCardRequest getPatientCardRequest) {
-        Jowl specificPatientJowl = new Jowl();
-
-        for (Integer key : specificPatientJowl.getJowl().keySet()) {
-            specificPatientJowl.getJowl().put(key,
-                    new ArrayList<>(addLastStatus(patientRepository.getPatientCard(getPatientCardRequest.getId()).get().getJowl(), key)));
-        }
-        return specificPatientJowl;
-    }
-
-    private List<ToothStatus> addLastStatus(Jowl specificPatientJowl, Integer key) {
-        return Arrays.asList(specificPatientJowl.getJowl().get(key).get(specificPatientJowl.getJowl().get(key).size() - 1));
+        return new GetPatientCardResponse(patientRepository.getPatientCard(getPatientCardRequest.getId()).get());
     }
 
 }
