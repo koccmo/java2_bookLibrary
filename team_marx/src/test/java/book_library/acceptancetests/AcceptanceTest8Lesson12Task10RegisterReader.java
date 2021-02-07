d41545ed389a73905added309d2a8b94dc8a5bb9
Maris <maris.baduns@gmail.com>
2021-02-07 23:15:41 +0200

    Lesson_12 - Task10_RegisterReaderService_acceptanceTest_shouldReturnErrorNotValidFirstName()_Complete

--- team_marx/src/test/java/book_library/acceptancetests/AcceptanceTest8Lesson12Task10RegisterReader.java
+++ team_marx/src/test/java/book_library/acceptancetests/AcceptanceTest8Lesson12Task10RegisterReader.java
@@ -17,7 +17,7 @@ public class AcceptanceTest8Lesson12Task10RegisterReader {
     private ApplicationContext appContext;
 
     @Before
-    public void setup(){
+    public void setup() {
 
         appContext = new AnnotationConfigApplicationContext(BookListConfiguration.class);
         getDatabaseCleaner().clean();
@@ -29,13 +29,23 @@ public class AcceptanceTest8Lesson12Task10RegisterReader {
 
     @Test
     public void shouldPasseValidation() {
-        RegisterReaderRequest request = new RegisterReaderRequest("FirstName","LastName");
+        RegisterReaderRequest request = new RegisterReaderRequest("FirstName", "LastName");
         RegisterReaderResponse response = getRegisterReaderService().execute(request);
 
         assertEquals(null, response.getErrors());
     }
 
-    private RegisterReaderService getRegisterReaderService(){
+    @Test
+    public void shouldReturnErrorNotValidFirstName() {
+        RegisterReaderRequest request = new RegisterReaderRequest(null,"LastName");
+        RegisterReaderResponse response = getRegisterReaderService().execute(request);
+
+        assertEquals(1, response.getErrors().size());
+        assertEquals("firstName", response.getErrors().get(0).getField());
+        assertEquals("Must not be empty!", response.getErrors().get(0).getMessage());
+    }
+
+    private RegisterReaderService getRegisterReaderService() {
         return appContext.getBean(RegisterReaderService.class);
     }
 }
