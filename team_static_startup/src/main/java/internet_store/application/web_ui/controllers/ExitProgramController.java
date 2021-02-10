2944c7bfea58924dcfc9b95626b1b65b8e82558e
Igors Mihejevs <mihejevs.igors@gmail.com>
2021-02-10 15:16:04 +0200

    exitProgram added

---
+++ team_static_startup/src/main/java/internet_store/application/web_ui/controllers/ExitProgramController.java
@@ -0,0 +1,20 @@
+package internet_store.application.web_ui.controllers;
+
+import internet_store.application.core.services.ExitProgramService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ExitProgramController {
+
+    @Autowired
+    private ExitProgramService exitProgramService;
+
+    @GetMapping(value = "/exitProgram")
+    public String exitProgram() {
+        exitProgramService.execute();
+        return "exitProgram";
+    }
+
+}
