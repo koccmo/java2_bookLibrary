ef621f285b3ee3ae030cbb2fbfab9e75e209cbc8
Jaroslav <brutan.jaroslav@gmail.com>
2021-02-10 23:45:44 +0200

    index controller update

--- team_no_name_no_problem/src/main/java/internet_store/web_ui/controllers/IndexController.java
+++ team_no_name_no_problem/src/main/java/internet_store/web_ui/controllers/IndexController.java
@@ -1,7 +1,9 @@
 package internet_store.web_ui.controllers;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
 public class IndexController {
 
     @GetMapping(value = "/")
