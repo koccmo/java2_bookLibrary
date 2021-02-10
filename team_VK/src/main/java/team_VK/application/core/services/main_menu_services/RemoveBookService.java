1e7492fbeae4d53b77181fd093dec41830a9e304
m_projects@inbox.lv <Galatasaraj01>
2021-02-10 21:02:47 +0200

    ORM implemented

--- team_VK/src/main/java/team_VK/application/core/services/main_menu_services/RemoveBookService.java
+++ team_VK/src/main/java/team_VK/application/core/services/main_menu_services/RemoveBookService.java
@@ -2,6 +2,7 @@ package team_VK.application.core.services.main_menu_services;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 import team_VK.application.core.domain.Book;
 import team_VK.application.core.requests.RemoveBookRequest;
 import team_VK.application.core.responses.CoreError;
@@ -13,6 +14,7 @@ import java.util.List;
 import java.util.Optional;
 
 @Component
+@Transactional
 public class RemoveBookService {
 
     @Autowired
