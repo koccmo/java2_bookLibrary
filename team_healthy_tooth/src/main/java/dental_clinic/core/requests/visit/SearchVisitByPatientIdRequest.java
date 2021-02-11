e0a81f9ba735a489f7eb6f04a61efbec8a1d8466
ValerijaI <55547022+ValerijaI@users.noreply.github.com>
2021-02-11 20:04:19 +0200

    SearchVisitByPatientIdRequest

--- team_healthy_tooth/src/main/java/dental_clinic/core/requests/visit/SearchVisitByPatientIdRequest.java
+++ team_healthy_tooth/src/main/java/dental_clinic/core/requests/visit/SearchVisitByPatientIdRequest.java
@@ -4,11 +4,17 @@ public class SearchVisitByPatientIdRequest {
 
     private Long id;
 
+    public SearchVisitByPatientIdRequest() { }
+
     public SearchVisitByPatientIdRequest(Long id) {
         this.id = id;
     }
 
     public Long getId() {
         return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 }
