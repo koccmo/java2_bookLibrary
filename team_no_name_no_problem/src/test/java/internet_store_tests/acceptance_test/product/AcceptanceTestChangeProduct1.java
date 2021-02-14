c93b3cdf18514e55d9d06b7b8ba077cdeb8b76aa
Jaroslav <brutan.jaroslav@gmail.com>
2021-02-14 09:07:44 +0200

    old update should check

--- team_no_name_no_problem/src/test/java/internet_store_tests/acceptance_test/product/AcceptanceTestChangeProduct1.java
+++ team_no_name_no_problem/src/test/java/internet_store_tests/acceptance_test/product/AcceptanceTestChangeProduct1.java
@@ -1,5 +1,20 @@
 package internet_store_tests.acceptance_test.product;
 
+import internet_store.WebInternetStoreApplication;
+import internet_store.core.domain.Product;
+import internet_store.core.requests.product.AddProductRequest;
+import internet_store.core.requests.product.ChangeProductRequest;
+import internet_store.core.requests.product.GetProductsRequest;
+import internet_store.core.response.product.ChangeProductResponse;
+import internet_store.core.response.product.GetProductsResponse;
+import internet_store.core.services.product.AddProductService;
+import internet_store.core.services.product.ChangeProductService;
+import internet_store.core.services.product.GetAllProductsService;
+import internet_store.dependency_injection.ApplicationContext;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
@@ -9,7 +24,7 @@ public class AcceptanceTestChangeProduct1 {
 
     @Before
     public void setup() {
-        appContext = new AnnotationConfigApplicationContext(MainMenuConfiguration.class);
+        appContext = new AnnotationConfigApplicationContext(WebInternetStoreApplication.class);
     }
     @Test
     public void test() {
@@ -43,5 +58,5 @@ public class AcceptanceTestChangeProduct1 {
         return appContext.getBean(GetAllProductsService.class);
     }
     
- */
+*/
 }
