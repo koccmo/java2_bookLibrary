967a23fef84057d5d2b24283847d2954073127f8
Maris <maris.baduns@gmail.com>
2021-02-14 13:35:09 +0200

    Added Unit Test shouldSearchByPersonalCodeWithOrderingAscendingByFirstName() to SearchReadersServiceTest

--- team_marx/src/test/java/book_library/core/services/Reader/SearchReadersServiceTest.java
+++ team_marx/src/test/java/book_library/core/services/Reader/SearchReadersServiceTest.java
@@ -286,6 +286,33 @@ public class SearchReadersServiceTest {
     }
 
     @Test
+    public void shouldSearchByPersonalCodeWithOrderingAscendingByFirstName() {
+        Ordering ordering = new Ordering("firstName", "ASCENDING");
+        SearchReaderRequest request = new SearchReaderRequest(null, null, 11111111111L, ordering);
+        List<CoreError> errors = new ArrayList<>();
+        Mockito.when(validator.validate(request)).thenReturn(errors);
+
+        List<Reader> readers = new ArrayList<>();
+        readers.add(new Reader("FirstName2", "LastName2", 11111111111L));
+        readers.add(new Reader("FirstName1", "LastName1", 11111111111L));
+        readers.add(new Reader("FirstName3", "LastName3", 11111111111L));
+        Mockito.when(readerRepository.findByPersonalCode(11111111111L)).thenReturn(readers);
+
+        SearchReadersResponse response = service.execute(request);
+        assertFalse(response.hasErrors());
+        assertEquals(3, response.getReaders().size());
+        assertEquals("FirstName1", response.getReaders().get(0).getFirstName());
+        assertEquals("LastName1", response.getReaders().get(0).getLastName());
+        assertEquals(Optional.of(11111111111L), java.util.Optional.ofNullable(response.getReaders().get(0).getPersonalCode()));
+        assertEquals("FirstName2", response.getReaders().get(1).getFirstName());
+        assertEquals("LastName2", response.getReaders().get(1).getLastName());
+        assertEquals(Optional.of(11111111111L), java.util.Optional.ofNullable(response.getReaders().get(1).getPersonalCode()));
+        assertEquals("FirstName3", response.getReaders().get(2).getFirstName());
+        assertEquals("LastName3", response.getReaders().get(2).getLastName());
+        assertEquals(Optional.of(11111111111L), java.util.Optional.ofNullable(response.getReaders().get(2).getPersonalCode()));
+    }
+
+    @Test
     public void shouldSearchByFirstNameWithOrderingAscendingByLastName_a() {
         Ordering ordering = new Ordering("lastName", "ASCENDING");
         SearchReaderRequest request = new SearchReaderRequest("FirstName", "LastName", 11111111111L, ordering);
