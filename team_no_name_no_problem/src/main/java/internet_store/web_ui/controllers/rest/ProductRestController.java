4e9ab279df71acf3d0fe220f50e122e258d2a26a
Anvar331 <anvar.intezar@gmail.com>
2021-02-15 21:57:32 +0200

    Product Rest Controller

---
+++ team_no_name_no_problem/src/main/java/internet_store/web_ui/controllers/rest/ProductRestController.java
@@ -0,0 +1,9 @@
+package internet_store.web_ui.controllers.rest;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/product")
+public class ProductRestController {
+}
