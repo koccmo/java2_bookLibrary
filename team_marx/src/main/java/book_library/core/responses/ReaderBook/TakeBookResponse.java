4603114c733dcc24ba0b48d4a48884f83b0009b3
Maris <maris.baduns@gmail.com>
2021-02-16 00:18:13 +0200

    Lesson_12 - Task_15 - Added TakeBookResponse - Completed

--- team_marx/src/main/java/book_library/core/responses/ReaderBook/TakeBookResponse.java
+++ team_marx/src/main/java/book_library/core/responses/ReaderBook/TakeBookResponse.java
@@ -1,4 +1,20 @@
 package book_library.core.responses.ReaderBook;
 
-public class TakeBookResponse {
+import book_library.core.domain.ReaderBook;
+import book_library.core.responses.CoreError;
+import book_library.core.responses.CoreResponse;
+
+import java.util.List;
+
+public class TakeBookResponse extends CoreResponse {
+
+    private ReaderBook newReaderBook;
+
+    public TakeBookResponse(List<CoreError> errors) {
+        super(errors);
+    }
+
+    public TakeBookResponse(ReaderBook newReaderBook) {
+        this.newReaderBook = newReaderBook;
+    }
 }
