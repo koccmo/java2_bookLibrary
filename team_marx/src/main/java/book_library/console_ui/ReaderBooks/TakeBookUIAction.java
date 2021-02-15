47c2d0f27f0f23c8f053906296cce3758997dca2
Maris <maris.baduns@gmail.com>
2021-02-15 10:26:11 +0200

    Lesson_12 - Task_15 - Added "Take a book" to ProgramMenu - Completed

---
+++ team_marx/src/main/java/book_library/console_ui/ReaderBooks/TakeBookUIAction.java
@@ -0,0 +1,13 @@
+package book_library.console_ui.ReaderBooks;
+
+import book_library.console_ui.UIAction;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TakeBookUIAction implements UIAction {
+
+    @Override
+    public void execute() {
+
+    }
+}
