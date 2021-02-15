a59f3fc2de0be1cc7e378b2cb810912a70d57c51
Anvar331 <anvar.intezar@gmail.com>
2021-02-15 22:24:00 +0200

    AddProduct in Rest Controller

--- team_no_name_no_problem/src/main/java/internet_store/web_ui/controllers/rest/ProductRestController.java
+++ team_no_name_no_problem/src/main/java/internet_store/web_ui/controllers/rest/ProductRestController.java
@@ -1,19 +1,20 @@
 package internet_store.web_ui.controllers.rest;
 
+import internet_store.core.requests.product.AddProductRequest;
 import internet_store.core.requests.product.FindProductByIdRequest;
+import internet_store.core.response.product.AddProductResponse;
 import internet_store.core.response.product.FindProductByIdResponse;
+import internet_store.core.services.product.AddProductService;
 import internet_store.core.services.product.FindProductByIdService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/product")
 public class ProductRestController {
 
-    @Autowired private FindProductByIdService findProductByIdService;
+    @Autowired
+    private FindProductByIdService findProductByIdService;
 
     @GetMapping(path = "/{id}", produces = "application/json")
     public FindProductByIdResponse findProductById(@PathVariable Long id) {
@@ -21,4 +22,14 @@ public class ProductRestController {
         return findProductByIdService.execute(request);
     }
 
+    @Autowired
+    private AddProductService addProductService;
+
+    @PostMapping(path = "/",
+            consumes = "application/json",
+            produces = "application/json")
+    public AddProductResponse addProduct(@RequestBody AddProductRequest request) {
+        return addProductService.execute(request);
+    }
 }
+
