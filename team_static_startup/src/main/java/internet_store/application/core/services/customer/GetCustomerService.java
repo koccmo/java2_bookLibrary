14ca8b02124899110f47b4bf8ab4a329d6e5f3de
Igors Mihejevs <mihejevs.igors@gmail.com>
2021-02-15 14:14:18 +0200

    lesson14 task1-task8 added for Customer

---
+++ team_static_startup/src/main/java/internet_store/application/core/services/customer/GetCustomerService.java
@@ -0,0 +1,34 @@
+package internet_store.application.core.services.customer;
+
+import internet_store.application.core.database.customer.CustomerRepository;
+import internet_store.application.core.requests.customer.GetCustomerRequest;
+import internet_store.application.core.responses.CoreError;
+import internet_store.application.core.responses.customer.GetCustomerResponse;
+import internet_store.application.core.services.customer.validators.GetCustomerValidator;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+@Component
+@Transactional
+public class GetCustomerService {
+
+    @Autowired
+    private CustomerRepository ormCustomerRepository;
+    @Autowired private GetCustomerValidator validator;
+
+    public GetCustomerResponse execute(GetCustomerRequest request) {
+        List<CoreError> errors = validator.validate(request);
+        if (!errors.isEmpty()) {
+            return new GetCustomerResponse(errors);
+        }
+        return ormCustomerRepository.getById(request.getId())
+                .map(GetCustomerResponse::new)
+                .orElseGet(() -> {
+                    errors.add(new CoreError("id", "Not found!"));
+                    return new GetCustomerResponse(errors);
+                });
+    }
+
+}
