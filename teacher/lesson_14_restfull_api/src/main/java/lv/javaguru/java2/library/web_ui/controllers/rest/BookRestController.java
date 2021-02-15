89d3db62e231dd13992b238a1302df11f164f858
vsavonins <viktors.savonins@proofit.lv>
2021-02-15 09:08:56 +0200

    Lesson 14 tasks

--- teacher/lesson_14_restfull_api/src/main/java/lv/javaguru/java2/library/web_ui/controllers/rest/BookRestController.java
+++ teacher/lesson_14_restfull_api/src/main/java/lv/javaguru/java2/library/web_ui/controllers/rest/BookRestController.java
@@ -4,16 +4,20 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import lv.javaguru.java2.library.core.requests.AddBookRequest;
 import lv.javaguru.java2.library.core.requests.GetBookRequest;
+import lv.javaguru.java2.library.core.requests.UpdateBookRequest;
 import lv.javaguru.java2.library.core.responses.AddBookResponse;
 import lv.javaguru.java2.library.core.responses.GetBookResponse;
+import lv.javaguru.java2.library.core.responses.UpdateBookResponse;
 import lv.javaguru.java2.library.core.services.AddBookService;
 import lv.javaguru.java2.library.core.services.GetBookService;
+import lv.javaguru.java2.library.core.services.UpdateBookService;
 
 @RestController
 @RequestMapping("/book")
@@ -21,6 +25,7 @@ public class BookRestController {
 
 	@Autowired private GetBookService getBookService;
 	@Autowired private AddBookService addBookService;
+	@Autowired private UpdateBookService updateBookService;
 
 	@GetMapping(path = "/{id}", produces = "application/json")
 	public GetBookResponse getBook(@PathVariable Long id) {
@@ -33,6 +38,13 @@ public class BookRestController {
 			produces = "application/json")
 	public AddBookResponse addBook(@RequestBody AddBookRequest request) {
 		return addBookService.execute(request);
+	}
+
+	@PutMapping(path = "/{id}",
+			consumes = "application/json",
+			produces = "application/json")
+	public UpdateBookResponse updateBook(@RequestBody UpdateBookRequest request) {
+		return updateBookService.execute(request);
 	}
 
 }
