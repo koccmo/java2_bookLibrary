804c755bf69e98aa6c7cd0eaf3983d005b37b904
Anvar331 <anvar.intezar@gmail.com>
2021-02-16 21:03:51 +0200

    ProductRestController update

--- team_no_name_no_problem/src/main/java/internet_store/web_ui/controllers/rest/ProductRestController.java
+++ team_no_name_no_problem/src/main/java/internet_store/web_ui/controllers/rest/ProductRestController.java
@@ -1,10 +1,13 @@
 package internet_store.web_ui.controllers.rest;
 
 import internet_store.core.requests.product.AddProductRequest;
+import internet_store.core.requests.product.ChangeProductRequest;
 import internet_store.core.requests.product.FindProductByIdRequest;
 import internet_store.core.response.product.AddProductResponse;
+import internet_store.core.response.product.ChangeProductResponse;
 import internet_store.core.response.product.FindProductByIdResponse;
 import internet_store.core.services.product.AddProductService;
+import internet_store.core.services.product.ChangeProductService;
 import internet_store.core.services.product.FindProductByIdService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -30,6 +33,16 @@ public class ProductRestController {
             produces = "application/json")
     public AddProductResponse addProduct(@RequestBody AddProductRequest request) {
         return addProductService.execute(request);
+    }
+
+    @Autowired
+    private ChangeProductService changeProductService;
+
+    @PutMapping(path = "/{id}",
+            consumes = "application/json",
+            produces = "application/json")
+    public ChangeProductResponse changeProduct(@RequestBody ChangeProductRequest request) {
+        return changeProductService.execute(request);
     }
 }
 
