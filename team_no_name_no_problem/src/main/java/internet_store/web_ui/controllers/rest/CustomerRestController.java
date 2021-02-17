7efacf85d62be263f6aa3f73dbb56b5f8867276f
Anvar331 <anvar.intezar@gmail.com>
2021-02-17 21:47:49 +0200

    CustomerRestController Delete

--- team_no_name_no_problem/src/main/java/internet_store/web_ui/controllers/rest/CustomerRestController.java
+++ team_no_name_no_problem/src/main/java/internet_store/web_ui/controllers/rest/CustomerRestController.java
@@ -1,13 +1,22 @@
 package internet_store.web_ui.controllers.rest;
 
 import internet_store.core.requests.customer.AddCustomerRequest;
+import internet_store.core.requests.customer.DeleteCustomerRequest;
 import internet_store.core.requests.customer.FindCustomerByIdRequest;
 import internet_store.core.requests.product.AddProductRequest;
+import internet_store.core.requests.product.ChangeProductRequest;
+import internet_store.core.requests.product.DeleteProductByIdRequest;
 import internet_store.core.response.customer.AddCustomerResponse;
+import internet_store.core.response.customer.DeleteCustomerResponse;
 import internet_store.core.response.customer.FindCustomerByIdResponse;
 import internet_store.core.response.product.AddProductResponse;
+import internet_store.core.response.product.ChangeProductResponse;
+import internet_store.core.response.product.DeleteProductByIdResponse;
 import internet_store.core.services.customer.AddCustomerService;
+import internet_store.core.services.customer.DeleteCustomerService;
 import internet_store.core.services.customer.FindCustomerByIdService;
+import internet_store.core.services.product.ChangeProductService;
+import internet_store.core.services.product.DeleteProductByIdService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -32,5 +41,14 @@ public class CustomerRestController {
             produces = "application/json")
     public AddCustomerResponse addCustomer(@RequestBody AddCustomerRequest request) {
         return addCustomerService.execute(request);
+    }
+
+    @Autowired
+    private DeleteCustomerService deleteCustomerService;
+
+    @DeleteMapping(path = "/{id}", produces = "application/json")
+    public DeleteCustomerResponse deleteCustomer(@PathVariable Long id) {
+        DeleteCustomerRequest request = new DeleteCustomerRequest(id);
+        return deleteCustomerService.execute(request);
     }
 }
