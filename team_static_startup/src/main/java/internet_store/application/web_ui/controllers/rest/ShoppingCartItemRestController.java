7f38c51420162e80cf13d52923e79b0bb98f0e97
ArturM <kpackalv@yahoo.com>
2021-02-17 22:51:54 +0200

    StaticStartup REST -> ShoppingCartItemRestController -> updateShoppingCartItem();

--- team_static_startup/src/main/java/internet_store/application/web_ui/controllers/rest/ShoppingCartItemRestController.java
+++ team_static_startup/src/main/java/internet_store/application/web_ui/controllers/rest/ShoppingCartItemRestController.java
@@ -2,10 +2,13 @@ package internet_store.application.web_ui.controllers.rest;
 
 import internet_store.application.core.requests.shopping_cart_item.AddShoppingCartItemRequest;
 import internet_store.application.core.requests.shopping_cart_item.GetShoppingCartItemRequest;
+import internet_store.application.core.requests.shopping_cart_item.UpdateShoppingCartItemRequest;
 import internet_store.application.core.responses.shopping_cart_item.AddShoppingCartItemResponse;
 import internet_store.application.core.responses.shopping_cart_item.GetShoppingCartItemResponse;
+import internet_store.application.core.responses.shopping_cart_item.UpdateShoppingCartItemResponse;
 import internet_store.application.core.services.shopping_cart_item.AddShoppingCartItemService;
 import internet_store.application.core.services.shopping_cart_item.GetShoppingCartItemService;
+import internet_store.application.core.services.shopping_cart_item.UpdateShoppingCartItemService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,7 +18,7 @@ public class ShoppingCartItemRestController {
 
     @Autowired private GetShoppingCartItemService getShoppingCartItemService;
     @Autowired private AddShoppingCartItemService addShoppingCartItemService;
-//    @Autowired private UpdateShoppingCartItemService updateShoppingCartItemService;
+    @Autowired private UpdateShoppingCartItemService updateShoppingCartItemService;
 //    @Autowired private DeleteShoppingCartItemService deleteShoppingCartItemService;
 
 
@@ -32,13 +35,13 @@ public class ShoppingCartItemRestController {
         return addShoppingCartItemService.execute(request);
     }
 
-//    @PutMapping(path = "/",
-//            produces = "application/json",
-//            consumes = "application/json")
-//    public UpdateShoppingCartItemResponse updateShoppingCartItem(@RequestBody UpdateShoppingCartItemRequest request){
-//        return updateShoppingCartItemService.execute(request);
-//    }
-//
+    @PutMapping(path = "/",
+            produces = "application/json",
+            consumes = "application/json")
+    public UpdateShoppingCartItemResponse updateShoppingCartItem(@RequestBody UpdateShoppingCartItemRequest request){
+        return updateShoppingCartItemService.execute(request);
+    }
+
 //    @DeleteMapping(path = "/{id}", produces = "application/json")
 //    public DeleteShoppingCartItemResponse deleteShoppingCartItem(@PathVariable Long id){
 //        DeleteShoppingCartItemRequest request = new DeleteShoppingCartItemRequest(id);
