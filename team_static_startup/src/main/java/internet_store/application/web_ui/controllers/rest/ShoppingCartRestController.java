bfbae4a413fb23703967f49203ccaaec36301038
AlexBogachenkov <aleksbogac@gmail.com>
2021-02-17 20:25:38 +0200

    ShoppingCartRestController implemented

--- team_static_startup/src/main/java/internet_store/application/web_ui/controllers/rest/ShoppingCartRestController.java
+++ team_static_startup/src/main/java/internet_store/application/web_ui/controllers/rest/ShoppingCartRestController.java
@@ -1,10 +1,41 @@
 package internet_store.application.web_ui.controllers.rest;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import internet_store.application.core.requests.shopping_cart.AddShoppingCartRequest;
+import internet_store.application.core.requests.shopping_cart.FindShoppingCartByIdRequest;
+import internet_store.application.core.responses.shopping_cart.AddShoppingCartResponse;
+import internet_store.application.core.responses.shopping_cart.FindAllShoppingCartsResponse;
+import internet_store.application.core.responses.shopping_cart.FindShoppingCartByIdResponse;
+import internet_store.application.core.services.shopping_cart.AddShoppingCartService;
+import internet_store.application.core.services.shopping_cart.FindAllShoppingCartsService;
+import internet_store.application.core.services.shopping_cart.FindShoppingCartByIdService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/shopping_cart")
 public class ShoppingCartRestController {
+
+    @Autowired
+    private AddShoppingCartService addShoppingCartService;
+    @Autowired
+    private FindAllShoppingCartsService findAllShoppingCartsService;
+    @Autowired
+    private FindShoppingCartByIdService findShoppingCartByIdService;
+
+    @PostMapping(path = "/", consumes = "application/json", produces = "application/json")
+    public AddShoppingCartResponse addShoppingCart(@RequestBody AddShoppingCartRequest request) {
+        return addShoppingCartService.execute(request);
+    }
+
+    @GetMapping(path = "/", produces = "application/json")
+    public FindAllShoppingCartsResponse findAllShoppingCarts() {
+        return findAllShoppingCartsService.execute();
+    }
+
+    @GetMapping(path = "/{id}", produces = "application/json")
+    public FindShoppingCartByIdResponse findShoppingCartById(@PathVariable Long id) {
+        FindShoppingCartByIdRequest request = new FindShoppingCartByIdRequest(id);
+        return findShoppingCartByIdService.execute(request);
+    }
 
 }
