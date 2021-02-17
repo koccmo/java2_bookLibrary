6751523c406eb19b2fc163b845fa61637ff4a858
Maris <maris.baduns@gmail.com>
2021-02-17 10:35:24 +0200

    Lesson_12 - Task_15 - Added TakeBookValidatorTest_case_success() - Completed

--- team_marx/src/test/java/book_library/core/validators/ReaderBook/TakeBookValidatorTest.java
+++ team_marx/src/test/java/book_library/core/validators/ReaderBook/TakeBookValidatorTest.java
@@ -1,7 +1,62 @@
 package book_library.core.validators.ReaderBook;
 
-import static org.junit.Assert.*;
+import book_library.core.database.Book.BookRepository;
+import book_library.core.database.Reader.ReaderRepository;
+import book_library.core.database.ReaderBook.ReaderBookRepository;
+import book_library.core.requests.ReaderBook.TakeBookRequest;
+import book_library.core.responses.CoreError;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+
+import static org.junit.Assert.*;
+import static org.mockito.ArgumentMatchers.any;
+
+@RunWith(MockitoJUnitRunner.class)
 public class TakeBookValidatorTest {
+
+    @Mock
+    private ReaderRepository readerRepository;
+
+    @Mock
+    private BookRepository bookRepository;
+
+    @Mock
+    private ReaderBookRepository readerBookRepository;
+
+    @InjectMocks
+    private TakeBookValidator validator;
+
+    @Test
+    public void success() throws ParseException {
+        SimpleDateFormat formatter1 = new SimpleDateFormat("yyyy/MM/dd HH:mm");
+        Date bookOutDate = formatter1.parse("2020/01/01 14:45");
+        TakeBookRequest request = new TakeBookRequest(1L,1L, bookOutDate);
+        Mockito.when(readerRepository.isSuchIdPresentsInDatabase(any())).thenReturn(true);
+        Mockito.when(bookRepository.isSuchIdPresentsInDatabase(any())).thenReturn(true);
+        Mockito.when(readerBookRepository.isBookInLibrary(any())).thenReturn(true);
+        List<CoreError> errors = validator.validate(request);
+        assertEquals(0, errors.size());
+    }
+
+    @Test
+    public void shouldReturnErrorWhenReaderIdIsNull() throws ParseException {
+        SimpleDateFormat formatter1 = new SimpleDateFormat("yyyy/MM/dd HH:mm");
+        Date bookOutDate = formatter1.parse("2020/01/01 14:45");
+        TakeBookRequest request = new TakeBookRequest(1L,1L, bookOutDate);
+        Mockito.when(readerRepository.isSuchIdPresentsInDatabase(any())).thenReturn(true);
+        Mockito.when(bookRepository.isSuchIdPresentsInDatabase(any())).thenReturn(true);
+        Mockito.when(readerBookRepository.isBookInLibrary(any())).thenReturn(true);
+        List<CoreError> errors = validator.validate(request);
+
+    }
 
 }
