5e6c8c66510b9ed2c0fac239ac1237c68175772b
Maris <maris.baduns@gmail.com>
2021-02-17 10:09:28 +0200

    Lesson_12 - Task_15 - Added TakeBookValidator - Completed

--- team_marx/src/main/java/book_library/core/database/ReaderBook/ReaderBookRepository.java
+++ team_marx/src/main/java/book_library/core/database/ReaderBook/ReaderBookRepository.java
@@ -2,6 +2,7 @@ package book_library.core.database.ReaderBook;
 
 import book_library.core.domain.ReaderBook;
 import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -20,5 +21,12 @@ public class ReaderBookRepository {
 
     public ReaderBook getById(Long id) {
         return sessionFactory.getCurrentSession().get(ReaderBook.class, id);
+    }
+
+    public boolean isBookInLibrary(Long id) {
+        Query query = sessionFactory.getCurrentSession().createQuery(
+                "SELECT b FROM ReaderBook b WHERE book_id = :book_id AND book_return_date IS NULL");
+        query.setParameter("book_id", id);
+        return !query.getResultList().isEmpty();
     }
 }
---
+++ team_marx/src/test/java/book_library/core/validators/ReaderBook/TakeBookValidatorTest.java
@@ -0,0 +1,7 @@
+package book_library.core.validators.ReaderBook;
+
+import static org.junit.Assert.*;
+
+public class TakeBookValidatorTest {
+
+}
