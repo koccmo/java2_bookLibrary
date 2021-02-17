3ea858c707750cbfe3f90a9f241c4a6fc63ddfe9
AlexBogachenkov <aleksbogac@gmail.com>
2021-02-17 20:48:45 +0200

    OrderRestController implemented

--- team_static_startup/src/main/java/internet_store/application/web_ui/controllers/rest/OrderRestController.java
+++ team_static_startup/src/main/java/internet_store/application/web_ui/controllers/rest/OrderRestController.java
@@ -1,10 +1,44 @@
 package internet_store.application.web_ui.controllers.rest;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import internet_store.application.core.requests.order.AddOrderRequest;
+import internet_store.application.core.requests.order.FindOrderByIdRequest;
+import internet_store.application.core.requests.shopping_cart.FindShoppingCartByIdRequest;
+import internet_store.application.core.responses.order.AddOrderResponse;
+import internet_store.application.core.responses.order.FindAllOrdersResponse;
+import internet_store.application.core.responses.order.FindOrderByIdResponse;
+import internet_store.application.core.responses.shopping_cart.FindAllShoppingCartsResponse;
+import internet_store.application.core.responses.shopping_cart.FindShoppingCartByIdResponse;
+import internet_store.application.core.services.order.AddOrderService;
+import internet_store.application.core.services.order.FindAllOrdersService;
+import internet_store.application.core.services.order.FindOrderByIdService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/order")
 public class OrderRestController {
+
+    @Autowired
+    private AddOrderService addOrderService;
+    @Autowired
+    private FindAllOrdersService findAllOrdersService;
+    @Autowired
+    private FindOrderByIdService findOrderByIdService;
+
+    @PostMapping(path = "/", consumes = "application/json", produces = "application/json")
+    public AddOrderResponse addOrder(@RequestBody AddOrderRequest request) {
+        return addOrderService.execute(request);
+    }
+
+    @GetMapping(path = "/", produces = "application/json")
+    public FindAllOrdersResponse findAllOrders() {
+        return findAllOrdersService.execute();
+    }
+
+    @GetMapping(path = "/{id}", produces = "application/json")
+    public FindOrderByIdResponse findOrderById(@PathVariable Long id) {
+        FindOrderByIdRequest request = new FindOrderByIdRequest(id);
+        return findOrderByIdService.execute(request);
+    }
 
 }
