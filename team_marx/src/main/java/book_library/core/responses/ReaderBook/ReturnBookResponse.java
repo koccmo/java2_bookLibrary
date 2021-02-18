dc3a552daf93e0fecd3981c0c547c7a77f5045f2
Maris <maris.baduns@gmail.com>
2021-02-18 23:13:38 +0200

    Lesson_12 - Task_16 - Added ReturnBookResponse - Completed

---
+++ team_marx/src/main/java/book_library/core/responses/ReaderBook/ReturnBookResponse.java
@@ -0,0 +1,20 @@
+package book_library.core.responses.ReaderBook;
+
+import book_library.core.domain.ReaderBook;
+import book_library.core.responses.CoreError;
+import book_library.core.responses.CoreResponse;
+
+import java.util.List;
+
+public class ReturnBookResponse extends CoreResponse {
+
+    private ReaderBook readerBook;
+
+    public ReturnBookResponse(List<CoreError> errors) {
+        super(errors);
+    }
+
+    public ReturnBookResponse(ReaderBook readerBook) {
+        this.readerBook = readerBook;
+    }
+}
