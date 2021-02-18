030aad988ca2245cef59a128e9eef44c585b1145
Maris <maris.baduns@gmail.com>
2021-02-18 23:09:27 +0200

    Lesson_12 - Task_16 - Added ReturnBookRequest - Completed

---
+++ team_marx/src/main/java/book_library/core/services/ReaderBooks/ReturnBookService.java
@@ -0,0 +1,10 @@
+package book_library.core.services.ReaderBooks;
+
+import org.springframework.stereotype.Component;
+
+import javax.transaction.Transactional;
+
+@Component
+@Transactional
+public class ReturnBookService {
+}
