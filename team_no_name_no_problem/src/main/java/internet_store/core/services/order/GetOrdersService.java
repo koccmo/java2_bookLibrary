e5df434a66893e213ef5351a48bb15788bdba34d
Jaroslav <brutan.jaroslav@gmail.com>
2021-02-21 19:00:02 +0200

    get orders service update

--- team_no_name_no_problem/src/main/java/internet_store/core/services/order/GetOrdersService.java
+++ team_no_name_no_problem/src/main/java/internet_store/core/services/order/GetOrdersService.java
@@ -1,19 +1,22 @@
 package internet_store.core.services.order;
 
+import internet_store.core.domain.Order;
 import internet_store.core.requests.order.GetOrdersRequest;
 import internet_store.core.response.CoreError;
-//import internet_store.core.response.order.GetOrdersResponse;
+import internet_store.core.response.order.GetOrdersResponse;
 import internet_store.core.services.order.validators.GetOrdersRequestValidator;
 import internet_store.database.order.OrderDatabase;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import javax.transaction.Transactional;
 import java.util.ArrayList;
 import java.util.List;
 
 @Component
+@Transactional
 public class GetOrdersService {
-/*
+
     @Autowired
     private OrderDatabase orderDatabase;
     @Autowired
@@ -22,15 +25,12 @@ public class GetOrdersService {
     public GetOrdersResponse execute (GetOrdersRequest getOrdersRequest) {
         List<CoreError> errors = getOrdersRequestValidator.validate(getOrdersRequest);
 
-        if (!errors.isEmpty()) {
-            return new GetOrdersResponse(errors, new ArrayList<>());
-        }
-
         if (orderDatabase.getOrders().isEmpty()) {
             errors.add(new CoreError("order database", "Orders database is empty"));
             return new GetOrdersResponse(errors, new ArrayList<>());
         }
-        return new GetOrdersResponse(orderDatabase.getOrders());
+        List<Order> orders = orderDatabase.getOrders();
+        return new GetOrdersResponse(orders);
     }
-    */
+
 }
