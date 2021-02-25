57fd34a9e0711100272e352e40085a0516014cd9
Igors Mihejevs <mihejevs.igors@gmail.com>
2021-02-25 14:08:48 +0200

    JpaCustomerRepository refactoring

--- team_static_startup/src/main/java/internet_store/application/core/database/jpa/JpaCustomerRepository.java
+++ team_static_startup/src/main/java/internet_store/application/core/database/jpa/JpaCustomerRepository.java
@@ -2,6 +2,8 @@ package internet_store.application.core.database.jpa;
 
 import internet_store.application.core.domain.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -9,10 +11,13 @@ import java.util.List;
 @Repository
 public interface JpaCustomerRepository extends JpaRepository<Customer, Long> {
 
-    boolean deleteByCustomerId(Long id);
+    @Query("delete from Customer c where c.customerId = :id")
+    boolean deleteByCustomerId(@Param("id")Long id);
 
-    List<Customer> findByFirstName(String customerName);
+    @Query("select c from Customer c where c.customerFirstName = :firstname")
+    List<Customer> findByFirstName(@Param("firstname") String customerFirstName);
 
-    boolean changeFirstName(Long id, String newFirstName);
+    @Query("update Customer c SET c.customerFirstName = :name where c.customerId = :id")
+    boolean changeFirstName(@Param("id") Long id, @Param("name")String newFirstName);
 
 }
