58e030820b736aa44d35d0e6abd9bffc9db95a31
Igors Mihejevs <mihejevs.igors@gmail.com>
2021-02-26 20:19:46 +0200

    updateCustomer added

--- team_static_startup/src/main/java/internet_store/application/core/requests/customer/UpdateCustomerRequest.java
+++ team_static_startup/src/main/java/internet_store/application/core/requests/customer/UpdateCustomerRequest.java
@@ -33,4 +33,28 @@ public class UpdateCustomerRequest {
     public String getNewAddress() {
         return newAddress;
     }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setNewFirstName(String newFirstName) {
+        this.newFirstName = newFirstName;
+    }
+
+    public void setNewSecondName(String newSecondName) {
+        this.newSecondName = newSecondName;
+    }
+
+    public void setNewPhone(String newPhone) {
+        this.newPhone = newPhone;
+    }
+
+    public void setNewEMail(String newEMail) {
+        this.newEMail = newEMail;
+    }
+
+    public void setNewAddress(String newAddress) {
+        this.newAddress = newAddress;
+    }
 }
