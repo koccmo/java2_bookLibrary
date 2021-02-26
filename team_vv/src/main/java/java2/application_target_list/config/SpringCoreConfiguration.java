41c5b479c580004036bc8df793a4e11c3a7d8f21
VadimsVladisevs <vadims.vladisevs@gmail.com>
2021-02-26 13:34:12 +0200

    liquibase added

--- team_vv/src/main/java/java2/application_target_list/config/SpringCoreConfiguration.java
+++ team_vv/src/main/java/java2/application_target_list/config/SpringCoreConfiguration.java
@@ -1,13 +1,17 @@
 package java2.application_target_list.config;
 
 
+import liquibase.integration.spring.SpringLiquibase;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
 
 
 @Configuration
@@ -19,4 +23,12 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EnableJpaRepositories(value = {"java2.application_target_list.core.database.jpa"})
 public class SpringCoreConfiguration {
 
+    @Bean
+    public SpringLiquibase springLiquibase(DataSource dataSource) {
+        SpringLiquibase liquibase = new SpringLiquibase();
+        liquibase.setChangeLog("classpath:/db/changelog/changelog-master.xml");
+        liquibase.setShouldRun(true);
+        liquibase.setDataSource(dataSource);
+        return liquibase;
+    }
 }
