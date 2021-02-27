88689d00bfc2881dba022fe77b351dd427054da7
ArturM <kpackalv@yahoo.com>
2021-02-27 13:03:47 +0200

    StaticStartup Liquibase SpringCoreConfiguration update

--- team_static_startup/src/main/java/internet_store/application/config/SpringCoreConfiguration.java
+++ team_static_startup/src/main/java/internet_store/application/config/SpringCoreConfiguration.java
@@ -1,25 +1,17 @@
 package internet_store.application.config;
 
-import org.apache.commons.dbcp2.BasicDataSource;
-import org.hibernate.SessionFactory;
-import org.springframework.beans.factory.annotation.Value;
+import liquibase.integration.spring.SpringLiquibase;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.orm.hibernate5.HibernateTransactionManager;
-import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
-import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.sql.DataSource;
-import java.io.IOException;
-import java.util.Properties;
 
 @Configuration
 @EnableAutoConfiguration
@@ -29,6 +21,16 @@ import java.util.Properties;
 @EntityScan(basePackages = "internet_store.application.core.domain")
 @EnableJpaRepositories(value = "internet_store.application.core.database.jpa")
 public class SpringCoreConfiguration {
+
+    @Bean
+    public SpringLiquibase springLiquibase(@Qualifier("dataSource") DataSource dataSource) {
+        SpringLiquibase liquibase = new SpringLiquibase();
+        liquibase.setChangeLog("classpath:/db/changelog/changelog-master.xml");
+        liquibase.setShouldRun(true);
+        liquibase.setDataSource(dataSource);
+        return liquibase;
+    }
+
 
 /*    @Value("${spring.database.username}")
     private String username;
