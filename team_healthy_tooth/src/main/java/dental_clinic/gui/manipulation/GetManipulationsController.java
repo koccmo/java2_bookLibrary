fb1b01ae797ff2f901769bfdfc4ef9092f8b07ee
ValerijaI <55547022+ValerijaI@users.noreply.github.com>
2021-03-06 23:05:11 +0200

    GUI Get manipulations

---
+++ team_healthy_tooth/src/main/java/dental_clinic/gui/manipulation/GetManipulationsController.java
@@ -0,0 +1,21 @@
+package dental_clinic.gui.manipulation;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class GetManipulationsController {
+
+    @FXML
+    private Button exitButton;
+
+    @FXML
+    private TextField time;
+
+    @FXML
+    private TextField personalCode;
+
+    @FXML
+    private TextField doctorsId;
+
+}
