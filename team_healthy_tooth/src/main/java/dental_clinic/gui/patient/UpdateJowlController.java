c1f28bc31a3850da8d8a3e6dc68477a3e2a0953f
ValerijaI <55547022+ValerijaI@users.noreply.github.com>
2021-03-06 23:08:35 +0200

    GUI Update jowl

---
+++ team_healthy_tooth/src/main/java/dental_clinic/gui/patient/UpdateJowlController.java
@@ -0,0 +1,25 @@
+package dental_clinic.gui.patient;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class UpdateJowlController {
+
+    @FXML
+    private TextField patientId;
+
+    @FXML
+    private Button enterButton;
+
+    @FXML
+    private Button exitButton;
+
+    @FXML
+    private PasswordField toothNumber;
+
+    @FXML
+    private TextField toothStatus;
+
+}
