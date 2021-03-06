99d5ea1244fa0a5654c341488a5a101df6d5d252
ValerijaI <55547022+ValerijaI@users.noreply.github.com>
2021-03-06 22:30:53 +0200

    GUI Search visit by patient id

---
+++ team_healthy_tooth/src/main/java/dental_clinic/gui/visit/SearchVisitByPatientIdController.java
@@ -0,0 +1,18 @@
+package dental_clinic.gui.visit;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class SearchVisitByPatientIdController {
+
+    @FXML
+    private TextField id;
+
+    @FXML
+    private Button blockButton;
+
+    @FXML
+    private Button exitButton;
+
+}
