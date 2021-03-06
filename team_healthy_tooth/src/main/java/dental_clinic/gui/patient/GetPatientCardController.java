220a4e0326813767821a78fd92807d0d018147ad
ValerijaI <55547022+ValerijaI@users.noreply.github.com>
2021-03-06 23:05:23 +0200

    GUI Get patient card

---
+++ team_healthy_tooth/src/main/java/dental_clinic/gui/patient/GetPatientCardController.java
@@ -0,0 +1,33 @@
+package dental_clinic.gui.patient;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class GetPatientCardController {
+
+    @FXML
+    private TextField id;
+
+    @FXML
+    private Button blockButton;
+
+    @FXML
+    private Button exitButton;
+
+    @FXML
+    private TextField time;
+
+    @FXML
+    private TextField personalCode;
+
+    @FXML
+    private TextField doctorsId;
+
+    @FXML
+    private TextField name;
+
+    @FXML
+    private TextField surname;
+
+}
