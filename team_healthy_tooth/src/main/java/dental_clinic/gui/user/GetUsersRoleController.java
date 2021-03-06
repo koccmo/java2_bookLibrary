20da6d001563b1a4c845f9ae131e69e98ffc4bc6
ValerijaI <55547022+ValerijaI@users.noreply.github.com>
2021-03-06 21:35:31 +0200

    GUI Get users role

---
+++ team_healthy_tooth/src/main/java/dental_clinic/gui/user/GetUsersRoleController.java
@@ -0,0 +1,21 @@
+package dental_clinic.gui.user;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class GetUsersRoleController {
+
+    @FXML
+    private TextField name;
+
+    @FXML
+    private Button enterButton;
+
+    @FXML
+    private Button exitButton;
+
+    @FXML
+    private TextField roleId;
+
+}
