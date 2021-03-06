0659739bcb15a46ae23755e12cc3b5e6ac90ee92
ValerijaI <55547022+ValerijaI@users.noreply.github.com>
2021-03-06 21:34:21 +0200

    GUI Get roles

---
+++ team_healthy_tooth/src/main/java/dental_clinic/gui/role/GetRolesController.java
@@ -0,0 +1,11 @@
+package dental_clinic.gui.role;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class GetRolesController {
+
+    @FXML
+    private Button exitButton;
+
+}
