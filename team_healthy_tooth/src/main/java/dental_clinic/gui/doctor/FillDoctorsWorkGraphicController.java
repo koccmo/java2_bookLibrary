4c9ef4b994acb8085c6663f3e8925926ea3b8886
ValerijaI <55547022+ValerijaI@users.noreply.github.com>
2021-03-06 23:04:35 +0200

    GUI Fill graphic

---
+++ team_healthy_tooth/src/main/java/dental_clinic/gui/doctor/FillDoctorsWorkGraphicController.java
@@ -0,0 +1,27 @@
+package dental_clinic.gui.doctor;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class FillDoctorsWorkGraphicController {
+
+    @FXML
+    private TextField id;
+
+    @FXML
+    private Button blockButton;
+
+    @FXML
+    private Button exitButton;
+
+    @FXML
+    private TextField day;
+
+    @FXML
+    private TextField from;
+
+    @FXML
+    private TextField to;
+
+}
