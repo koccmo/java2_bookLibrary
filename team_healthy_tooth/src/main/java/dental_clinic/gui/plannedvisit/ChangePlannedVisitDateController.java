e87f23b138f95fbd49298d2fbbc59335479bc378
ValerijaI <55547022+ValerijaI@users.noreply.github.com>
2021-03-06 22:04:20 +0200

    GUI Change planned visit

---
+++ team_healthy_tooth/src/main/java/dental_clinic/gui/plannedvisit/ChangePlannedVisitDateController.java
@@ -0,0 +1,21 @@
+package dental_clinic.gui.plannedvisit;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class ChangePlannedVisitDateController {
+
+    @FXML
+    private TextField id;
+
+    @FXML
+    private Button blockButton;
+
+    @FXML
+    private Button exitButton;
+
+    @FXML
+    private TextField visitDate;
+
+}
