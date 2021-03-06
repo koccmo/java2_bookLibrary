acb59e4f0a6f64a5c64bbd1ecdd9b1542c840d55
ValerijaI <55547022+ValerijaI@users.noreply.github.com>
2021-03-06 21:35:10 +0200

    Some changes

--- team_healthy_tooth/src/main/java/dental_clinic/core/validators/role/GetRolesRequestValidator.java
+++ team_healthy_tooth/src/main/java/dental_clinic/core/validators/role/GetRolesRequestValidator.java
@@ -2,10 +2,12 @@ package dental_clinic.core.validators.role;
 
 import dental_clinic.core.requests.role.GetRolesRequest;
 import dental_clinic.core.responses.CoreError;
+import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Component
 public class GetRolesRequestValidator {
 
     public List<CoreError> validate (GetRolesRequest getRolesRequest) {
