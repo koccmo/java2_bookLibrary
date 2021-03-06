119fe5bf5049626d4f06ce0f4775d94729e85d95
ValerijaI <55547022+ValerijaI@users.noreply.github.com>
2021-03-06 23:05:34 +0200

    GUI Get patient history

---
+++ team_healthy_tooth/src/main/java/dental_clinic/gui/patient/GetPatientHistoryController.java
@@ -0,0 +1,27 @@
+package dental_clinic.gui.patient;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class GetPatientHistoryController {
+
+    @FXML
+    private TextField id;
+
+    @FXML
+    private Button blockButton;
+
+    @FXML
+    private Button exitButton;
+
+    @FXML
+    private TextField time;
+
+    @FXML
+    private TextField personalCode;
+
+    @FXML
+    private TextField doctorsId;
+
+}
