abd0afa01660f0c71ae5023bc2f06b4911068426
ValerijaI <55547022+ValerijaI@users.noreply.github.com>
2021-03-06 22:30:42 +0200

    GUI Search visit by date

---
+++ team_healthy_tooth/src/main/java/dental_clinic/gui/visit/SearchVisitByDateController.java
@@ -0,0 +1,18 @@
+package dental_clinic.gui.visit;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class SearchVisitByDateController {
+
+    @FXML
+    private TextField date;
+
+    @FXML
+    private Button blockButton;
+
+    @FXML
+    private Button exitButton;
+
+}
