be4649f029a8c84725de8346db17820fb4f5f15e
ValerijaI <55547022+ValerijaI@users.noreply.github.com>
2021-03-06 23:04:21 +0200

    GUI Deactivate manipulation

---
+++ team_healthy_tooth/src/main/java/dental_clinic/gui/manipulation/DeactivateManipulationController.java
@@ -0,0 +1,18 @@
+package dental_clinic.gui.manipulation;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class DeactivateManipulationController {
+
+    @FXML
+    private TextField id;
+
+    @FXML
+    private Button blockButton;
+
+    @FXML
+    private Button exitButton;
+
+}
