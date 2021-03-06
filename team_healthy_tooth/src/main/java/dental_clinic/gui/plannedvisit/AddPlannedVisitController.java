d0f543ea6d6535f2122b7d1e9426a8e73f7a8f1d
ValerijaI <55547022+ValerijaI@users.noreply.github.com>
2021-03-06 22:03:54 +0200

    GUI Add planned visit

---
+++ team_healthy_tooth/src/main/java/dental_clinic/gui/plannedvisit/AddPlannedVisitController.java
@@ -0,0 +1,25 @@
+package dental_clinic.gui.plannedvisit;
+
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class AddPlannedVisitController {
+
+    @FXML
+    private TextField visitDate;
+
+    @FXML
+    private Button enterButton;
+
+    @FXML
+    private Button exitButton;
+
+    @FXML
+    private TextField doctorsId;
+
+    @FXML
+    private TextField personalCode;
+
+}
