2c2b7bc59402aef719845cdbc75ae61fcebb0e59
ValerijaI <55547022+ValerijaI@users.noreply.github.com>
2021-03-06 21:33:47 +0200

    GUI Block user

---
+++ team_healthy_tooth/src/main/java/dental_clinic/gui/user/BlockUserController.java
@@ -0,0 +1,18 @@
+package dental_clinic.gui.user;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class BlockUserController {
+
+    @FXML
+    private TextField name;
+
+    @FXML
+    private Button blockButton;
+
+    @FXML
+    private Button exitButton;
+
+}
