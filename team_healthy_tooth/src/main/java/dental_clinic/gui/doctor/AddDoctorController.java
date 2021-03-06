8e6113b7628607979ec9280a68ab8ce46604b6e0
ValerijaI <55547022+ValerijaI@users.noreply.github.com>
2021-03-06 23:03:24 +0200

    GUI Add doctor

---
+++ team_healthy_tooth/src/main/java/dental_clinic/gui/doctor/AddDoctorController.java
@@ -0,0 +1,25 @@
+package dental_clinic.gui.doctor;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class AddDoctorController {
+
+    @FXML
+    private TextField name;
+
+    @FXML
+    private Button enterButton;
+
+    @FXML
+    private Button exitButton;
+
+    @FXML
+    private PasswordField phone;
+
+    @FXML
+    private TextField surname;
+
+}
