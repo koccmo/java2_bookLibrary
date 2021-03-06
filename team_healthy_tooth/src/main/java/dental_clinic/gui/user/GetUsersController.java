33347fc0b47125270e4e1ae38d36931a54a21f93
ValerijaI <55547022+ValerijaI@users.noreply.github.com>
2021-03-06 21:35:22 +0200

    GUI Get users

---
+++ team_healthy_tooth/src/main/java/dental_clinic/gui/user/GetUsersController.java
@@ -0,0 +1,11 @@
+package dental_clinic.gui.user;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class GetUsersController {
+
+    @FXML
+    private Button exitButton;
+
+}
