25175d16310383c3f5a8e5630ce1ab3c939a2903
ValerijaI <55547022+ValerijaI@users.noreply.github.com>
2021-03-06 23:03:36 +0200

    GUI Add manipulation

---
+++ team_healthy_tooth/src/main/java/dental_clinic/gui/manipulation/AddManipulationController.java
@@ -0,0 +1,21 @@
+package dental_clinic.gui.manipulation;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class AddManipulationController {
+
+    @FXML
+    private TextField manipulationType;
+
+    @FXML
+    private Button enterButton;
+
+    @FXML
+    private Button exitButton;
+
+    @FXML
+    private TextField price;
+
+}
