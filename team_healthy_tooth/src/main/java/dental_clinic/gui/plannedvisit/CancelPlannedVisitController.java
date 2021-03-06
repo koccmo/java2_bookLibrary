af3bf17175d442f2efe095d6593543955b592ae1
ValerijaI <55547022+ValerijaI@users.noreply.github.com>
2021-03-06 22:04:08 +0200

    GUI Cancel planned visit

---
+++ team_healthy_tooth/src/main/java/dental_clinic/gui/plannedvisit/CancelPlannedVisitController.java
@@ -0,0 +1,18 @@
+package dental_clinic.gui.plannedvisit;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class CancelPlannedVisitController {
+
+    @FXML
+    private TextField name;
+
+    @FXML
+    private Button blockButton;
+
+    @FXML
+    private Button exitButton;
+
+}
