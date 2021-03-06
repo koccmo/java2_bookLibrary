c052303f7f2433ebdfd6039efc2602765ef6f9da
ValerijaI <55547022+ValerijaI@users.noreply.github.com>
2021-03-06 23:04:48 +0200

    GUI Fire Doctor

---
+++ team_healthy_tooth/src/main/java/dental_clinic/gui/doctor/FireDoctorController.java
@@ -0,0 +1,19 @@
+package dental_clinic.gui.doctor;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class FireDoctorController {
+
+    @FXML
+    private TextField id;
+
+    @FXML
+    private Button fireButton;
+
+    @FXML
+    private Button exitButton;
+
+}
+
