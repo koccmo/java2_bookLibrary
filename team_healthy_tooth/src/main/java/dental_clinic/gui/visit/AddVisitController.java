6cb53cd9886095484e4ff5d0867096599e12d6ad
ValerijaI <55547022+ValerijaI@users.noreply.github.com>
2021-03-06 22:30:20 +0200

    GUI Add visit

---
+++ team_healthy_tooth/src/main/java/dental_clinic/gui/visit/AddVisitController.java
@@ -0,0 +1,35 @@
+package dental_clinic.gui.visit;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class AddVisitController {
+
+    @FXML
+    private TextField patientId;
+
+    @FXML
+    private Button enterButton;
+
+    @FXML
+    private Button exitButton;
+
+    @FXML
+    private TextField doctorId;
+
+    @FXML
+    private PasswordField toothNumber;
+
+    @FXML
+    private TextField manipulationId;
+
+    @FXML
+    private TextField toothStatus;
+
+    @FXML
+    private PasswordField comment;
+
+}
+
