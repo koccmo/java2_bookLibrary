21ac03ef26552678e20fc3c1275a80d42507ccf0
ValerijaI <55547022+ValerijaI@users.noreply.github.com>
2021-03-06 23:08:23 +0200

    GUI Search patient

---
+++ team_healthy_tooth/src/main/java/dental_clinic/gui/patient/SearchPatientController.java
@@ -0,0 +1,18 @@
+package dental_clinic.gui.patient;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class SearchPatientController {
+
+    @FXML
+    private TextField searchField;
+
+    @FXML
+    private Button blockButton;
+
+    @FXML
+    private Button exitButton;
+
+}
