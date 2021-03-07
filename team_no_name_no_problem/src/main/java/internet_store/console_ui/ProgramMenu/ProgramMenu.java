d6ad587b249c972f9e3f303b7dbaf97645dc981c
Jaroslav <brutan.jaroslav@gmail.com>
2021-03-07 19:08:55 +0200

    program menu jpa update

--- team_no_name_no_problem/src/main/java/internet_store/console_ui/ProgramMenu/ProgramMenu.java
+++ team_no_name_no_problem/src/main/java/internet_store/console_ui/ProgramMenu/ProgramMenu.java
@@ -16,7 +16,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
 
-@Component
+//@Component
 public class ProgramMenu {
 
     private Map<Integer, UIAction> menuNumberToActionMap = new HashMap<>();
