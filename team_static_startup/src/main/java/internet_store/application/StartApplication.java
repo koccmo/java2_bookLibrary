717fa4d7e9b3035ec42d12493c168df3873a152d
Igors Mihejevs <mihejevs.igors@gmail.com>
2021-03-14 19:44:02 +0200

    test

--- team_static_startup/src/main/java/internet_store/application/StartApplication.java
+++ team_static_startup/src/main/java/internet_store/application/StartApplication.java
@@ -11,5 +11,5 @@ public class StartApplication {
     public static void main(String[] args) {
         ConfigurableApplicationContext context = SpringApplication.run(SpringWebConfiguration.class);
     }
-//test
+
 }
