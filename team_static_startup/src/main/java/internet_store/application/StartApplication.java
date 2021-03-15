6840393c70d6531f43905601827ba02391abd72f
Igors Mihejevs <mihejevs.igors@gmail.com>
2021-03-15 19:27:32 +0200

    StartApplication refactoring

--- team_static_startup/src/main/java/internet_store/application/StartApplication.java
+++ team_static_startup/src/main/java/internet_store/application/StartApplication.java
@@ -3,13 +3,12 @@ package internet_store.application;
 import internet_store.application.web_ui.config.SpringWebConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class StartApplication {
 
     public static void main(String[] args) {
-        ConfigurableApplicationContext context = SpringApplication.run(SpringWebConfiguration.class);
+        SpringApplication.run(SpringWebConfiguration.class, args);
     }
 
 }
